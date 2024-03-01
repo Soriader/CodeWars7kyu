0ae8443263fcaa51fac2034f5364b5c7b4580b87
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-01 12:45:16 +0100

    Add project files.

---
+++ CodeWarsTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using CodeWars7kyu;
+
+namespace CodeWarsTest
+{
+    public class Tests
+    {
+
+        [TestFixture]
+        public class KataTest
+        {
+            
+        }
+
+    }
+}
