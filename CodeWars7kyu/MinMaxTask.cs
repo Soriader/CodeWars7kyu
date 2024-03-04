c3fff43ba917fab6811b3fdf97cb39992bc61107
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-04 10:15:46 +0100

    [~] Task:  The highest profit wins!

---
+++ CodeWars7kyu/MinMaxTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class MinMaxTask
+    {
+        public static int[] minMax(int[] lst)
+        {
+            int min = lst.Min();
+            int max = lst.Max();
+            return new int[] { min, max };
+        }
+        //https://www.codewars.com/kata/559590633066759614000063/train/csharp
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -9,6 +9,15 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public static void MinMaxTest()
+            {
+                Assert.AreEqual(new int[] { -1, 20 }, MinMaxTask.minMax(new int[] { 1, 2, 5, -1, 12, 20 }));
+                Assert.AreEqual(new int[] { 1, 5 }, MinMaxTask.minMax(new int[] { 1, 2, 3, 4, 5 }));
+                Assert.AreEqual(new int[] { -3, 5 }, MinMaxTask.minMax(new int[] { 1, 2, -3, 4, 5 }));
+            }
+
             [Test]
             public static void PrinterErrorTest()
             {
