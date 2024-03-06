7d532edce7e07bce29250fce53ce49657efc7a95
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-06 12:40:12 +0100

    [~] Task: You're a square!

---
+++ CodeWars7kyu/IsSquareTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class IsSquareTask
+    {
+        public static bool IsSquare(int n)
+        {
+            double squareRoot = Math.Sqrt(n);
+            return squareRoot == (int)squareRoot;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,17 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public static void IsSquareTest()
+            {
+                Assert.AreEqual(false, IsSquareTask.IsSquare(-1), "negative numbers aren't square numbers");
+                Assert.AreEqual(false, IsSquareTask.IsSquare(3), "3 isn't a square number");
+                Assert.AreEqual(true, IsSquareTask.IsSquare(4), "4 is a square number");
+                Assert.AreEqual(true, IsSquareTask.IsSquare(25), "25 is a square number");
+                Assert.AreEqual(false, IsSquareTask.IsSquare(26), "26 isn't a square number");
+            }
+
             [Test]
             public void GetTheMiddleCharacterTest()
             {
