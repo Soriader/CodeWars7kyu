eed3af189cbf2817ac9deaa0f606256233df4299
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-06 12:23:41 +0100

    [~] Task: String ends with?

---
+++ CodeWars7kyu/StringEndsWithTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class StringEndsWithTask
+    {
+        public static bool Solution(string str, string ending)
+        {
+            bool findTheAnswer = str.EndsWith(ending);
+
+            if (findTheAnswer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,17 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public void StringEndsWithTest()
+            {
+                Assert.AreEqual(true, StringEndsWithTask.Solution("samurai", "ai"));
+                Assert.AreEqual(false, StringEndsWithTask.Solution("sumo", "omo"));
+                Assert.AreEqual(false, StringEndsWithTask.Solution("ails", "fails"));
+                Assert.AreEqual(true, StringEndsWithTask.Solution("ninja", "ja"));
+                Assert.AreEqual(true, StringEndsWithTask.Solution("!@#$%^&*() :-)", ":-)"));
+            }
+
             [Test]
             public void IsTriangleTest()
             {
