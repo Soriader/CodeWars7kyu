c9bdf96841d3dc89d31e18c2af7c64b7b1a5ac73
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-07 20:11:26 +0100

    [~] Task: Binary Addition

---
+++ CodeWars7kyu/BinaryAdditionTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class BinaryAdditionTask
+    {
+        public static string AddBinary(int a, int b)
+        {
+            int sum = a + b;
+            string binary = Convert.ToString(sum, 2);
+            return binary;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,11 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+            [Test]
+            public void BinaryAdditionTest()
+            {
+                Assert.AreEqual("11", BinaryAdditionTask.AddBinary(1, 2), "Should return \"11\" for 1 + 2");
+            }
 
             [Test]
             public void DescendingOrderTest()
