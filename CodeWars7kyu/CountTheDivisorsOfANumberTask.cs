eca9c581043880d2626f447bba76cb68848df627
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-07 12:15:27 +0100

    [~] Task: Count the divisors of a number

---
+++ CodeWars7kyu/CountTheDivisorsOfANumberTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CountTheDivisorsOfANumberTask
+    {
+        public static int Divisors(int n)
+        {
+            int divisors = 1;
+            int divisor = 1;
+
+
+            while (divisor < n)
+            {
+                int action = n % divisor;
+
+                if (n % divisor == 0)
+                {
+                    divisors++;
+                }
+
+                divisor++;
+
+                if (divisor > n)
+                {
+                    break;
+                }
+            }
+
+            return divisors;
+        }
+    }
+}
+//https://www.codewars.com/kata/542c0f198e077084c0000c2e/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,14 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+            [Test]
+            public void CountTheDivisorsOfANumberTest()
+            {
+                Assert.AreEqual(1, CountTheDivisorsOfANumberTask.Divisors(1));
+                Assert.AreEqual(4, CountTheDivisorsOfANumberTask.Divisors(10));
+                Assert.AreEqual(2, CountTheDivisorsOfANumberTask.Divisors(11));
+                Assert.AreEqual(8, CountTheDivisorsOfANumberTask.Divisors(54));
+            }
 
             [Test]
             public void ValidatePinTest()
