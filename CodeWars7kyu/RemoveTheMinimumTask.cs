dcb45c1ec7da0171cbe3456a0e0229f2758407c2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-08 13:02:49 +0100

    [~] Task: Remove the minimum

---
+++ CodeWars7kyu/RemoveTheMinimumTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class RemoveTheMinimumTask
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                return new List<int>();
+            }
+
+            int min = numbers.Min(); 
+
+            List<int> result = new List<int>(numbers); 
+
+            result.Remove(min);
+
+            return result;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,14 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+            [Test]
+            public static void RemoveTheMinimumTest()
+            {
+                Assert.AreEqual(new List<int> { 2, 3, 4, 5 }, RemoveTheMinimumTask.RemoveSmallest(new List<int> { 1, 2, 3, 4, 5 }));
+                Assert.AreEqual(new List<int> { 5, 3, 2, 4 }, RemoveTheMinimumTask.RemoveSmallest(new List<int> { 5, 3, 2, 1, 4 }));
+                Assert.AreEqual(new List<int> { 2, 3, 1, 1 }, RemoveTheMinimumTask.RemoveSmallest(new List<int> {1, 2, 3, 1, 1 }));
+                Assert.AreEqual(new List<int>(), RemoveTheMinimumTask.RemoveSmallest(new List<int>()));
+            }
 
             [Test]
             public void SumTwoSmallestNumbersTest()
