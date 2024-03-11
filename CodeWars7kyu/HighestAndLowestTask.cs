994a82511a4cdf06056808b1b788422bf445acaa
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-11 21:45:06 +0100

    [~] Task: Highest and Lowest

---
+++ CodeWars7kyu/HighestAndLowestTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class HighestAndLowestTask
+    {
+        public static string HighAndLow(string numbers)
+        {
+            var maxAdnMin = numbers.Split(' ').Select(int.Parse).ToList();
+            var max = maxAdnMin.Max();
+            var min = maxAdnMin.Min();
+
+            return $"{max} {min}";
+        }
+    }
+}
+//https://www.codewars.com/kata/554b4ac871d6813a03000035/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,15 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public void HighestAndLowestTest()
+            {
+                Assert.AreEqual("42 -9", HighestAndLowestTask.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+                Assert.AreEqual("3 1", HighestAndLowestTask.HighAndLow("1 2 3"));
+
+            }
+
             [Test]
             public static void RemoveTheMinimumTest()
             {
