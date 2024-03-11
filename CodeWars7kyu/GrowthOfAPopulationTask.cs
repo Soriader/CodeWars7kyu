e3a2608096f3a39138064825c0af1a6f1118f7e8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-11 21:59:48 +0100

    [~}Task: Growth of a Population

---
+++ CodeWars7kyu/GrowthOfAPopulationTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class GrowthOfAPopulationTask
+    {
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            int currentPopulation = p0;
+            int years = 0;
+
+            while (currentPopulation < p)
+            {
+                currentPopulation += (int)(currentPopulation * percent / 100) + aug;
+                years++;
+            }
+
+            return years;
+        }
+    }
+}
+//https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,13 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+            [Test]
+            public static void GrowthOfAPopulationTest()
+            {
+                Assert.AreEqual(15, GrowthOfAPopulationTask.NbYear(1500, 5, 100, 5000));
+                Assert.AreEqual(10, GrowthOfAPopulationTask.NbYear(1500000, 2.5, 10000, 2000000));
+                Assert.AreEqual(94, GrowthOfAPopulationTask.NbYear(1500000, 0.25, 1000, 2000000));
+            }
 
             [Test]
             public void HighestAndLowestTest()
