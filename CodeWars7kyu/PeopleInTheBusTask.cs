0f120a72cc687e62d9c9672df6aec56a6bca429c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-16 18:35:40 +0100

    [~]Task: Number of People in the Bus

---
+++ CodeWars7kyu/PeopleInTheBusTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class PeopleInTheBusTask
+    {
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            int remainingPassengers = 0;
+
+            foreach (int[] stop in peopleListInOut)
+            {
+                remainingPassengers += stop[0];
+                remainingPassengers -= stop[1]; 
+
+
+                if (remainingPassengers < 0)
+                {
+                    remainingPassengers = 0;
+                }
+            }
+
+            return remainingPassengers;
+        }
+    }
+}
+//https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,16 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public void PeopleInTheBusTest()
+            {
+
+                Assert.AreEqual(5, PeopleInTheBusTask.Number(new List<int[]>() { new[] { 10, 0 }, new[] { 3, 5 }, new[] { 5, 8 } }));
+                Assert.AreEqual(17, PeopleInTheBusTask.Number(new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 10 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 10 } }));
+                Assert.AreEqual(21, PeopleInTheBusTask.Number(new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 8 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 8 } }));
+            }
+
             [Test]
             public static void test1()
             {
