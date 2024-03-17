14f5882589270e6d3cc507147116f9ec3226bf32
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-17 16:59:52 +0100

    [~] Task: Exes and Ohs

---
+++ CodeWars7kyu/ExesAndOhsTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ExesAndOhsTask
+    {
+        public static bool XO(string input)
+        {
+            var exes = input.ToLower().Count(c => c == 'x');
+            var ohs = input.ToLower().Count(c => c == 'o');
+
+            if( exes == ohs ) 
+            { 
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -10,6 +10,15 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+            [Test]
+            public void ExesAndOhsTest()
+            {
+                Assert.AreEqual(true, ExesAndOhsTask.XO("xo"));
+                Assert.AreEqual(true, ExesAndOhsTask.XO("xxOo"));
+                Assert.AreEqual(false, ExesAndOhsTask.XO("xxxm"));
+                Assert.AreEqual(false, ExesAndOhsTask.XO("Oo"));
+                Assert.AreEqual(false, ExesAndOhsTask.XO("ooom"));
+            }
 
             [Test]
             public void PeopleInTheBusTest()
