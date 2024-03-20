20450802a7207880dec3cb0a0e59b62aef428c0a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-20 18:22:59 +0100

    [~] Task: Testing 1-2-3

---
+++ CodeWars7kyu/TestingNumerationTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class TestingNumerationTask
+    {
+        public static List<string> Number(List<string> lines)
+        {
+            int start = 1;
+            List<string> result = new List<string>();
+
+            foreach (string line in lines)
+            {
+                result.Add((start + ": " + line));
+                start++;
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -12,6 +12,15 @@ namespace CodeWarsTest
         public class KataTest
         {
             [Test]
+            public void TestingNumerationTest()
+            {
+                Assert.AreEqual(new List<string>(), TestingNumerationTask.Number(new List<string>()));
+                Assert.AreEqual(new List<string> { "1: a", "2: b", "3: c" }, TestingNumerationTask.Number(new List<string> { "a", "b", "c" }));
+                Assert.AreEqual(new List<string> { "1: ", "2: ", "3: ", "4: ", "5: " }, TestingNumerationTask.Number(new List<string> { "", "", "", "", "" }));
+            }
+
+
+            [Test]
             public void SquareEveryDigitTest()
             {
                 Assert.AreEqual(811181, SquareEveryDigitTask.SquareDigits(9119));
