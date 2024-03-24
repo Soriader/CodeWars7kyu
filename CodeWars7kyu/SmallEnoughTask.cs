d3bb4b7ab80b2f3da724a007f14df609b2397035
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-24 20:00:07 +0100

    [~] Task: Small enough? - Beginner

---
+++ CodeWars7kyu/SmallEnoughTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SmallEnoughTask
+    {
+        public static bool SmallEnough(int[] a, int limit)
+        {
+            foreach (int i in a)
+            {
+                if(i > limit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+//https://www.codewars.com/kata/57cc981a58da9e302a000214/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -12,6 +12,15 @@ namespace CodeWarsTest
         public class KataTest
         {
             [Test]
+            public void SmallEnoughTest()
+            {
+                Assert.AreEqual(true, SmallEnoughTask.SmallEnough(new int[] { 66, 101 }, 200));
+                Assert.AreEqual(false, SmallEnoughTask.SmallEnough(new int[] { 78, 117, 110, 99, 104, 117, 107, 115 }, 100));
+                Assert.AreEqual(true, SmallEnoughTask.SmallEnough(new int[] { 101, 45, 75, 105, 99, 107 }, 107));
+                Assert.AreEqual(true, SmallEnoughTask.SmallEnough(new int[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 120));
+            }
+
+            [Test]
             public void TestingNumerationTest()
             {
                 Assert.AreEqual(new List<string>(), TestingNumerationTask.Number(new List<string>()));
