1d36bb0434d95458c6f47f49f1af9df32fff24d1
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-27 21:19:57 +0100

    [~] Task: Remove anchor from URL

---
+++ CodeWars7kyu/RemoveAnchorFromURLTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CodeWars7kyu
+{
+    public class RemoveAnchorFromURLTask
+    {
+        public static string RemoveUrlAnchor(string url)
+        {
+            string result = string.Empty;
+            int index = url.IndexOf('#');
+
+            if (index != -1)
+            {
+                result = url.Substring(0, index);
+                return result;
+
+            }
+
+            return url;
+        }
+    }
+}
+//https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -11,6 +11,16 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public void RemoveAnchorFromURLTest()
+            {
+                Assert.AreEqual("www.codewars.com", RemoveAnchorFromURLTask.RemoveUrlAnchor("www.codewars.com#about"));
+                Assert.AreEqual("www.codewars.com/katas/?page=1", RemoveAnchorFromURLTask.RemoveUrlAnchor("www.codewars.com/katas/?page=1#about"));
+                Assert.AreEqual("www.codewars.com/katas/", RemoveAnchorFromURLTask.RemoveUrlAnchor("www.codewars.com/katas/"));
+            }
+
+
             [Test]
             public void JadenCasingStringTest()
             {
