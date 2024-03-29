9996912d9cc806ed56681766207420a7ea316950
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-29 12:37:35 +0100

    [~] Task: Check the exam

---
+++ CodeWars7kyu/CheckTheExamTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CheckTheExamTask
+    {
+        public static int CheckExam(string[] arr1, string[] arr2)
+        {
+            int score = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr2[i] == "")
+                {
+                    continue;
+                }
+
+                if (arr1[i] == arr2[i])
+                {
+                    score += 4;
+                }
+                else
+                {
+                    score -= 1; 
+                }
+
+            }
+
+            if(score < 0)
+            {
+                return 0;
+            }
+
+            return score;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -13,6 +13,13 @@ namespace CodeWarsTest
         {
 
             [Test]
+            public void CheckTheExamTest()
+            {
+                Assert.AreEqual(7, CheckTheExamTask.CheckExam(new string[] { "a", "a", "c", "b" }, new string[] { "a", "a", "b", "" }));
+                Assert.AreEqual(6, CheckTheExamTask.CheckExam(new string[] { "a", "a", "b", "b" }, new string[] { "a", "c", "b", "d" }));
+                Assert.AreEqual(0, CheckTheExamTask.CheckExam(new string[] { "b", "c", "b", "a" }, new string[] { "", "a", "a", "c" }));
+            }
+
             public void RemoveAnchorFromURLTest()
             {
                 Assert.AreEqual("www.codewars.com", RemoveAnchorFromURLTask.RemoveUrlAnchor("www.codewars.com#about"));
