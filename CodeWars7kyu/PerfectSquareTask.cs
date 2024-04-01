d8d1532deb18ad2a888dc3d613ff8226df5da84b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-01 11:32:03 +0200

    [~] Task: Find the next perfect square!

---
+++ CodeWars7kyu/PerfectSquareTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class PerfectSquareTask
+    {
+        public static long FindNextSquare(long num)
+        {
+            double squareRoot = Math.Sqrt(num);
+
+            long roundedSquareRoot = (long)Math.Floor(squareRoot);
+
+            if (roundedSquareRoot * roundedSquareRoot == num)
+            {
+                long nextSquare = roundedSquareRoot + 1;
+                return nextSquare * nextSquare;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -11,9 +11,18 @@ namespace CodeWarsTest
         [TestFixture]
         public class KataTest
         {
-
+            
             public class Tests
             {
+                [Test]
+                public static void PerfectSquareTest()
+                {
+                    Assert.AreEqual(-1, PerfectSquareTask.FindNextSquare(155));
+                    Assert.AreEqual(144, PerfectSquareTask.FindNextSquare(121));
+                    Assert.AreEqual(676, PerfectSquareTask.FindNextSquare(625));
+                    Assert.AreEqual(320356, PerfectSquareTask.FindNextSquare(319225));
+                }
+
                 [Test]
                 public void SumDigitsTest()
                 {
