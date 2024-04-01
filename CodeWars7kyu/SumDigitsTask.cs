135a8a209f63c91fdb510fb844939311b07427ff
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-01 10:24:16 +0200

    [~] Task: Summing a number's digits

---
+++ CodeWars7kyu/SumDigitsTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SumDigitsTask
+    {
+        public static int SumDigits(int number)
+        {
+            int absoluteNumber = Math.Abs(number);
+            char[] digits = absoluteNumber.ToString().ToCharArray();
+            int sum = 0;
+            foreach (char digit in digits)
+            {
+                sum += int.Parse(digit.ToString());
+            }
+
+            return sum;
+        }
+    }
+}
+//https://www.codewars.com/kata/52f3149496de55aded000410/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void SumDigitsTest()
+                {
+                    Assert.AreEqual(1, SumDigitsTask.SumDigits(10));
+                    Assert.AreEqual(18, SumDigitsTask.SumDigits(99));
+                    Assert.AreEqual(5, SumDigitsTask.SumDigits(-32));
+
+                }
+
+                [Test]
                 public void FixStringCaseTest()
                 {
                     Assert.AreEqual("code", FixStringCaseTask.Solve("code"));
