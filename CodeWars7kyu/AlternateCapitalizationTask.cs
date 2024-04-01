72b8eafd14b72cd233569d11191873e27edac3db
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-01 16:23:08 +0200

    [~] Task: Alternate capitalization

---
+++ CodeWars7kyu/AlternateCapitalizationTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class AlternateCapitalizationTask
+    {
+        public static string[] Capitalize(string s)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    list.Add(char.ToUpper(s[i]).ToString());
+                }
+                else
+                {
+                    list.Add(char.ToLower(s[i]).ToString());
+                }
+            }
+
+            string[] result = new string[2];
+            result[0] = string.Join("", list);
+            list.Clear();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    list.Add(char.ToLower(s[i]).ToString());
+                }
+                else
+                {
+                    list.Add(char.ToUpper(s[i]).ToString());
+                }
+            }
+            result[1] = string.Join("", list);
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/59cfc000aeb2844d16000075/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -14,6 +14,16 @@ namespace CodeWarsTest
             
             public class Tests
             {
+
+                [Test]
+                public void AlternateCapitalizationTest()
+                {
+                    Assert.AreEqual(new string[2] { "AbCdEf", "aBcDeF" }, AlternateCapitalizationTask.Capitalize("abcdef"));
+                    Assert.AreEqual(new string[2] { "CoDeWaRs", "cOdEwArS" }, AlternateCapitalizationTask.Capitalize("codewars"));
+                    Assert.AreEqual(new string[2] { "AbRaCaDaBrA", "aBrAcAdAbRa" }, AlternateCapitalizationTask.Capitalize("abracadabra"));
+                    Assert.AreEqual(new string[2] { "CoDeWaRrIoRs", "cOdEwArRiOrS" }, AlternateCapitalizationTask.Capitalize("codewarriors"));
+                }
+
                 [Test]
                 public void TwoFightersOneWinnerTest()
                 {
