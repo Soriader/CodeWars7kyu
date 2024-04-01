c6cbc9d84a659668b91cf3d23024b13c7c252cd4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-01 12:02:12 +0200

    [~] Task: Two fighters, one winner.

---
+++ CodeWars7kyu/TwoFightersOneWinnerTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class TwoFightersOneWinnerTask
+    {
+        public class Fighter
+        {
+            public string Name;
+            public int Health, DamagePerAttack;
+            public Fighter(string name, int health, int damagePerAttack)
+            {
+                this.Name = name;
+                this.Health = health;
+                this.DamagePerAttack = damagePerAttack;
+            }
+        }
+        public static string DeclareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+        {
+            while (fighter1.Health > 0 && fighter2.Health > 0)
+            {
+                if (firstAttacker == fighter1.Name)
+                {
+                    fighter2.Health -= fighter1.DamagePerAttack;
+                    if (fighter2.Health <= 0)
+                    {
+                        return fighter1.Name;
+                    }
+                    fighter1.Health -= fighter2.DamagePerAttack;
+                    if (fighter1.Health <= 0)
+                    {
+                        return fighter2.Name;
+                    }
+                }
+                else if (firstAttacker == fighter2.Name)
+                {
+                    fighter1.Health -= fighter2.DamagePerAttack;
+                    if (fighter1.Health <= 0)
+                    {
+                        return fighter2.Name;
+                    }
+                    fighter2.Health -= fighter1.DamagePerAttack;
+                    if (fighter2.Health <= 0)
+                    {
+                        return fighter1.Name;
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
+//https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,18 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void TwoFightersOneWinnerTest()
+                {
+                    Assert.AreEqual("Lew", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Lew", 10, 2), new TwoFightersOneWinnerTask.Fighter("Harry", 5, 4), "Lew"));
+                    Assert.AreEqual("Harry", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Lew", 10, 2), new TwoFightersOneWinnerTask.Fighter("Harry", 5, 4), "Harry"));
+                    Assert.AreEqual("Harald", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Harald", 20, 5), new TwoFightersOneWinnerTask.Fighter("Harry", 5, 4), "Harry"));
+                    Assert.AreEqual("Harald", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Harald", 20, 5), new TwoFightersOneWinnerTask.Fighter("Harry", 5, 4), "Harald"));
+                    Assert.AreEqual("Harald", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Jerry", 30, 3), new TwoFightersOneWinnerTask.Fighter("Harald", 20, 5), "Jerry"));
+                    Assert.AreEqual("Harald", TwoFightersOneWinnerTask.DeclareWinner(new TwoFightersOneWinnerTask.Fighter("Jerry", 30, 3), new TwoFightersOneWinnerTask.Fighter("Harald", 20, 5), "Harald"));
+
+                }
+
+                [Test]
                 public static void PerfectSquareTest()
                 {
                     Assert.AreEqual(-1, PerfectSquareTask.FindNextSquare(155));
