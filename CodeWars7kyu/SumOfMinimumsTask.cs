2fa3b8ccf5b890b1de00d8b249bfe8ffcf88f2ac
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-04 18:56:36 +0200

    [~] Task: Sum of Minimums!

---
+++ CodeWars7kyu/SumOfMinimumsTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SumOfMinimumsTask
+    {
+        public static int SumOfMinimums(int[,] numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                int min = numbers[i, 0];
+
+                for (int j = 1; j < numbers.GetLength(1); j++)
+                {
+                    if (numbers[i, j] < min)
+                    {
+                        min = numbers[i, j];
+                    }
+                }
+
+                sum += min;
+            }
+
+            return sum;
+        }
+    }
+}
