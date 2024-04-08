2952645a8ba23b48f7f7e4aa96a1013ad52e40ed
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-08 19:54:15 +0200

    [~] Task: Your task is to write function factorial

---
+++ CodeWars7kyu/CreateFactorialmethodTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CreateFactorialmethodTask
+    {
+        public static ulong Factorial(int n)
+        {
+            ulong result = 1;
+
+            while(n > 0)
+            {
+                result *= (ulong)n;
+                n--;
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using CodeWars7kyu;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System;
 using System.Reflection;
 using static CodeWars7kyu.DisemvowelTrollsTask;
@@ -14,6 +15,17 @@ namespace CodeWarsTest
             
             public class Tests
             {
+
+                [Test]
+                public void CreateFactorialmethodTest()
+                {
+                    Assert.AreEqual(1, CreateFactorialmethodTask.Factorial(0));
+                    Assert.AreEqual(1, CreateFactorialmethodTask.Factorial(1));
+                    Assert.AreEqual(2, CreateFactorialmethodTask.Factorial(2));
+                    Assert.AreEqual(6, CreateFactorialmethodTask.Factorial(3));
+                    Assert.AreEqual(24, CreateFactorialmethodTask.Factorial(4));
+                    Assert.AreEqual(120, CreateFactorialmethodTask.Factorial(5));
+                }
 
                 [Test]
                 public void FindTheCapitalsTest()
