ed886dc343cdd6eddc0ac15b9e505292bc8dd7d7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-09 15:27:43 +0200

    [~] Task: Round up to the next multiple of 5

---
+++ CodeWars7kyu/MultipleOf5Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+    public class MultipleOf5Task
+    {
+        public static int RoundToNext5(int n)
+        {
+            //if (n == -2 && n == -1 && n == 0) return 0;
+            int roundedNumber = (int)Math.Ceiling(n / 5.0) * 5;
+
+            return roundedNumber;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/55d1d6d5955ec6365400006d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,18 @@ namespace CodeWarsTest
             
             public class Tests
             {
+                [Test]
+                public void MultipleOf5Test()
+                {
+                    Assert.AreEqual(0, MultipleOf5Task.RoundToNext5(0));
+                    Assert.AreEqual(5, MultipleOf5Task.RoundToNext5(1));
+                    Assert.AreEqual(5, MultipleOf5Task.RoundToNext5(3));
+                    Assert.AreEqual(5, MultipleOf5Task.RoundToNext5(5));
+                    Assert.AreEqual(10, MultipleOf5Task.RoundToNext5(7));
+                    Assert.AreEqual(40, MultipleOf5Task.RoundToNext5(39));
+
+                }
+
 
                 [Test]
                 public void CreateFactorialmethodTest()
