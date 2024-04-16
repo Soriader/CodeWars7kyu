cb3599e503d50945a1f394d52c4b8bb88791f45c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-16 12:10:49 +0200

    [~] Task: Alan Partridge III - London

---
+++ CodeWars7kyu/AlanPartridgeIIILondonTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class AlanPartridgeIIILondonTask
+    {
+        public static string Alan(string[] x)
+        {
+            string[] stations = { "Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway" };
+
+            foreach (string station in stations)
+            {
+                if (!x.Contains(station))
+                {
+                    return "No, seriously, run. You will miss it.";
+                }
+            }
+
+            return "Smell my cheese you mother!";
+        }
+    }
+}
+//https://www.codewars.com/kata/580a41b6d6df740d6100030c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void AlanPartridgeIIILondonTest()
+                {
+                    Assert.AreEqual("Smell my cheese you mother!", AlanPartridgeIIILondonTask.Alan(new string[] { "Norwich", "Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway", "London" }));
+                    Assert.AreEqual("No, seriously, run. You will miss it.", AlanPartridgeIIILondonTask.Alan(new string[] { "London", "Norwich" }));
+                    Assert.AreEqual("Smell my cheese you mother!", AlanPartridgeIIILondonTask.Alan(new string[] { "Norwich", "Tooting", "Bank", "Rejection", "Disappointment", "Backstabbing Central", "Exeter", "Shattered Dreams Parkway", "Belgium", "London" }));
+                }
+
+                [Test]
                 public void AnagramDetectionTest()
                 {
                     Assert.AreEqual(true, AnagramDetectionTask.IsAnagram("foefet", "toffee"));
