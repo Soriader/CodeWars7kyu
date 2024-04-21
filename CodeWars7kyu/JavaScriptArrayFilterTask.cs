cb01f4d31281cdb64a7e39aae948d27bf1655066
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-21 15:56:46 +0200

    [~] Task: JavaScript Array Filter

---
+++ CodeWars7kyu/JavaScriptArrayFilterTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class JavaScriptArrayFilterTask
+    {
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            int[] result = numbers.Where(w => w % 2 == 0).ToArray();
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/514a6336889283a3d2000001/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void JavaScriptArrayFilterTest()
+                {
+                    Assert.AreEqual(new int[] { 2 }, JavaScriptArrayFilterTask.GetEvenNumbers(new int[] { 1, 2 }));
+                    Assert.AreEqual(new int[] { 2, 6, 8, 10 }, JavaScriptArrayFilterTask.GetEvenNumbers(new int[] { 2, 6, 8, 10 }));
+                    Assert.AreEqual(new int[] { 12, 14 }, JavaScriptArrayFilterTask.GetEvenNumbers(new int[] { 12, 14, 15 }));
+                    Assert.AreEqual(new int[] { }, JavaScriptArrayFilterTask.GetEvenNumbers(new int[] { 1, 3, 9 }));
+
+                }
+
+                [Test]
                 public void AlanPartridgeIIILondonTest()
                 {
                     Assert.AreEqual("Smell my cheese you mother!", AlanPartridgeIIILondonTask.Alan(new string[] { "Norwich", "Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway", "London" }));
