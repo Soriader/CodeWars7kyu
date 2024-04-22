abd12204865aa40a36759acaf76a840bb69aaf7f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-22 18:05:22 +0200

    [~] Task: Sum of numbers from 0 to N

---
+++ CodeWars7kyu/SumofNumbersFrom0ToNTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SumofNumbersFrom0ToNTask
+    {
+        public static string ShowSequence(int n)
+        {
+            if(n == 0)
+            {
+                return "0=0";
+            }
+
+            if(n < 0)
+            {
+                return $"{n}<0";
+            }
+
+            int start = 0;
+            int sum = 0;
+            string result = "";
+
+            while (start < n)
+            {
+                if(start == n) 
+                {
+                    break;
+                }
+                result += $"{start}+";
+                start++;
+                sum += start;
+            }
+
+            if (start == n)
+            {
+                result += $"{start} = {sum}";
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,14 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void SumofNumbersFrom0ToNTest()
+                {
+                    Assert.AreEqual("0+1+2+3+4+5+6 = 21", SumofNumbersFrom0ToNTask.ShowSequence(6));
+                    Assert.AreEqual("-5<0", SumofNumbersFrom0ToNTask.ShowSequence(-5));
+                    Assert.AreEqual("0=0", SumofNumbersFrom0ToNTask.ShowSequence(0));
+                }
+
+                [Test]
                 public void MaximumProductTest()
                 {
                     Assert.AreEqual(200, MaximumProductTask.AdjacentElementsProduct(new int[] { 40, 5, 8 }));
