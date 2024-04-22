39dba2c198d1147492c1271d6784e4c406159209
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-22 17:44:20 +0200

    [~] Task: Maximum Product

---
+++ CodeWars7kyu/MaximumProductTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class MaximumProductTask
+    {
+        public static int AdjacentElementsProduct(int[] array)
+        {
+            int iterator = 0;
+            List<int> result = new List<int> ();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                iterator = array[i] * array[i + 1];
+                result.Add(iterator);
+            }
+        
+            var maxValue = result.Max (x => x);
+
+            return maxValue;
+        }
+    }
+}
+//https://www.codewars.com/kata/5a4138acf28b82aa43000117/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void MaximumProductTest()
+                {
+                    Assert.AreEqual(200, MaximumProductTask.AdjacentElementsProduct(new int[] { 40, 5, 8 }));
+                    Assert.AreEqual(6, MaximumProductTask.AdjacentElementsProduct(new int[] { 6, 1, 2, 3, 0 }));
+                    Assert.AreEqual(-14, MaximumProductTask.AdjacentElementsProduct(new int[] { -23, 4, -5, 99, -27, 329, -2, 7, -921 }));
+
+                }
+
+                [Test]
                 public void SortArrayByStringLengthTest()
                 {
                     Assert.AreEqual(new string[] { "I", "To", "Beg", "Life" }, SortArrayByStringLengthTask.SortByLength(new string[] { "Beg", "Life", "I", "To" }));
