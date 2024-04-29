d986a2385d4da80708fba012739380fcf386e730
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-29 17:39:17 +0200

    [~] Task: Most digits

---
+++ CodeWars7kyu/MostDigitsTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MostDigitsTask
+	{
+		public static int FindLongest(int[] number)
+		{
+			int longestNumberIndex = 0;
+			int maxDigits = 0;
+
+			for (int i = 0; i < number.Length; i++)
+			{
+				int currentDigits = Math.Abs(number[i]).ToString().Length;
+				if (currentDigits > maxDigits)
+				{
+					longestNumberIndex = i;
+					maxDigits = currentDigits;
+				}
+			}
+
+			return number[longestNumberIndex];
+		}
+	}
+}
+//https://www.codewars.com/kata/58daa7617332e59593000006/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,14 @@ namespace CodeWarsTest
 			public class Tests
             {
 				[Test]
+				public void MostDigitsTest()
+				{
+					Assert.AreEqual(100, MostDigitsTask.FindLongest(new int[] { 1, 10, 100 }));
+					Assert.AreEqual(9000, MostDigitsTask.FindLongest(new int[] { 9000, 8, 800 }));
+					Assert.AreEqual(900, MostDigitsTask.FindLongest(new int[] { 8, 900, 500 }));
+				}
+
+				[Test]
 				public void AlphabetWarTest()
 				{
 					Assert.AreEqual("Right side wins!", AlphabetWarTask.AlphabetWar("z"));
