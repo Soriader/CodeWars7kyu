f55dbb49a89cf983b68769e1dbcb9c625a2153b6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-29 17:56:44 +0200

    [~] Task: Build a square

---
+++ CodeWars7kyu/BuildASquareTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class BuildASquareTask
+	{
+		public static string GenerateShape(int n)
+		{
+			if (n == 0) return "";
+			int iterator = 0;
+			string block = "";
+			string result = "";
+
+			while (n > 0)
+			{
+				iterator++;
+				block += "+";
+
+				if(iterator == n)
+				{
+					iterator = 1;
+					result += block;
+					break;
+				}
+			}
+
+			while (n > iterator)
+			{
+				iterator++;
+				result += "\n" + block;
+
+				if (iterator == n)
+				{
+					break;
+				}
+			}
+
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
 			public class Tests
             {
 				[Test]
+				public void BuildASquareTest()
+				{
+					Assert.AreEqual("", BuildASquareTask.GenerateShape(0));
+					Assert.AreEqual("+", BuildASquareTask.GenerateShape(1));
+					Assert.AreEqual("++\n++", BuildASquareTask.GenerateShape(2));
+					Assert.AreEqual("+++\n+++\n+++", BuildASquareTask.GenerateShape(3));
+				}
+
+				[Test]
 				public void MostDigitsTest()
 				{
 					Assert.AreEqual(100, MostDigitsTask.FindLongest(new int[] { 1, 10, 100 }));
