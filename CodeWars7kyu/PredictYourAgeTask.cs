2a261d1c4ec375f4cd3ded8ca8df66f8b34392f2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-04 22:34:33 +0200

    [~] Task: Predict your age!

---
+++ CodeWars7kyu/PredictYourAgeTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class PredictYourAgeTask
+    {
+        public static int PredictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8)
+        {
+            List<int> list = new List<int> { age1, age2, age3, age4, age5, age6, age7,  age8 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                list[i] = list[i] * list[i];
+            }
+
+            int result = list.Aggregate((x, y) => x + y);
+
+            double nextStep = Math.Sqrt(result);
+
+            return (int)(nextStep / 2);
+        }
+
+    }
+}
+//https://www.codewars.com/kata/5aff237c578a14752d0035ae/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,13 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void PredictYourAgeTest()
+                {
+                    Assert.AreEqual(86, PredictYourAgeTask.PredictAge(65, 60, 75, 55, 60, 63, 64, 45));
+                    Assert.AreEqual(79, PredictYourAgeTask.PredictAge(32, 54, 76, 65, 34, 63, 64, 45));
+                }
+
+                [Test]
                 public void ConvertAnArrayOfStringsToArrayOfNumbersTest()
                 {
                     Assert.AreEqual(new double[] { 1.1, 2.2, 3.3 }, ConvertAnArrayOfStringsToArrayOfNumbersTask.ToDoubleArray(new string[] { "1.1", "2.2", "3.3" }));
