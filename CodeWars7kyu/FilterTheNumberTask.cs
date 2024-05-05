921e9fb37a8ee19e3eb634a0e2a499ea899ea35f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-05 13:13:13 +0200

    [~] Task: Filter the number

---
+++ CodeWars7kyu/FilterTheNumberTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class FilterTheNumberTask
+    {
+        public static int FilterString(string s)
+        {
+            List<int> listOfNumbers = new List<int> ();
+            var result = s.ToCharArray();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (char.IsDigit(result[i]))
+                {
+                    listOfNumbers.Add(int.Parse(result[i].ToString()));                
+                }
+            }
+
+            string concatenatedDigits = string.Concat(listOfNumbers);
+            return int.Parse(concatenatedDigits);
+        }
+    }
+}
+//https://www.codewars.com/kata/55b051fac50a3292a9000025/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public static void FilterTheNumberTest()
+                {
+                    Assert.AreEqual(123, FilterTheNumberTask.FilterString("123"), "Just return the numbers");
+                    Assert.AreEqual(123, FilterTheNumberTask.FilterString("a1b2c3"), "Just return the numbers");
+                    Assert.AreEqual(123, FilterTheNumberTask.FilterString("aa1bb2cc3dd"), "Just return the numbers");
+                }
+
+                [Test]
                 public void NumberOfDecimalDigitsTest()
                 {
                     Assert.AreEqual(1, NumberOfDecimalDigitsTask.Digits(5ul));
