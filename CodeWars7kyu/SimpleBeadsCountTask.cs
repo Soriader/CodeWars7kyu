58c0d7ee0ec6cb18ec3091aa068795be48bb4f88
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-05 12:47:17 +0200

    [~] Task: Simple beads count

---
+++ CodeWars7kyu/SimpleBeadsCountTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SimpleBeadsCountTask
+    {
+        public static int CountRedBeads(int n)
+        {
+            if(n < 2) return 0;
+
+            return (n - 1) * 2;
+
+
+        }
+    }
+}
+//https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/csharp
+
+//2 - 2
+//    3 - 4 
+//    4 - 6
+//    5 - 8
+//    6 - 10 
+//    7 - 12 
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void SimpleBeadsCountTest()
+                {
+                    Assert.That(SimpleBeadsCountTask.CountRedBeads(0), Is.EqualTo(0));
+                    Assert.That(SimpleBeadsCountTask.CountRedBeads(1), Is.EqualTo(0));
+                    Assert.That(SimpleBeadsCountTask.CountRedBeads(3), Is.EqualTo(4));
+                    Assert.That(SimpleBeadsCountTask.CountRedBeads(5), Is.EqualTo(8));
+                }
+
+                [Test]
                 public void PredictYourAgeTest()
                 {
                     Assert.AreEqual(86, PredictYourAgeTask.PredictAge(65, 60, 75, 55, 60, 63, 64, 45));
