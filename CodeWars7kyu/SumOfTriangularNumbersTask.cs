4f186b181d25ea6417d139411f8a46bfb1238c4d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-06 12:02:52 +0200

    [~] Task: Sum of Triangular Numbers

---
+++ CodeWars7kyu/SumOfTriangularNumbersTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SumOfTriangularNumbersTask
+    {
+        public static int SumTriangularNumbers(int n)
+        {
+            int iterator = 0;
+            int result = 0;
+
+            while (iterator < n)
+            {
+                iterator++;
+                result += (iterator + 1) * iterator / 2;
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/580878d5d27b84b64c000b51/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+                [Test]
+                public void SumOfTriangularNumbersTest()
+                {
+                    Assert.AreEqual(56, SumOfTriangularNumbersTask.SumTriangularNumbers(6));
+                    Assert.AreEqual(7140, SumOfTriangularNumbersTask.SumTriangularNumbers(34));
+                    Assert.AreEqual(0, SumOfTriangularNumbersTask.SumTriangularNumbers(-291));
+                    Assert.AreEqual(140205240, SumOfTriangularNumbersTask.SumTriangularNumbers(943));
+                    Assert.AreEqual(0, SumOfTriangularNumbersTask.SumTriangularNumbers(-971));
+                }
 
                 [Test]
                 public void GreetMeTest()
