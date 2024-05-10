0d65f3dcceeaa2dbe16eb36d7a34d4109242a6ed
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-10 22:33:09 +0200

    [~] Task: Divide and Conquer

---
+++ CodeWars7kyu/DivideAndConquerTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class DivideAndConquerTask
+    {
+        public static int DivCon(Object[] objArray)
+        {
+
+            List<int> digitsList = new List<int>();
+            List<int> stringsList = new List<int>();
+
+            foreach (object obj in objArray)
+            {
+                if (obj is int)
+                {
+                    digitsList.Add((int)obj);
+                }
+                else if (obj is string && int.TryParse((string)obj, out int parsedInt))
+                {
+                    stringsList.Add(parsedInt);
+                }
+            }
+
+            int sumOfDigits = digitsList.Sum();
+            int sumOfStrings = stringsList.Sum();
+
+            return sumOfDigits - sumOfStrings;
+        }
+    }
+}
+//https://www.codewars.com/kata/57eaec5608fed543d6000021/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,24 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void DivideAndConquerTest()
+                {
+                    Assert.AreEqual(2, DivideAndConquerTask.DivCon(new object[] { 9, 3, "7", "3" }));
+                    Assert.AreEqual(14, DivideAndConquerTask.DivCon(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 }));
+                    Assert.AreEqual(13, DivideAndConquerTask.DivCon(new object[] { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" }));
+                    Assert.AreEqual(11, DivideAndConquerTask.DivCon(new object[] { "1", "5", "8", 8, 9, 9, 2, "3" }));
+                    Assert.AreEqual(61, DivideAndConquerTask.DivCon(new object[] { 8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7 }));
+                    Assert.AreEqual(-6, DivideAndConquerTask.DivCon(new object[] { "0", "1", "2", "3" }));
+                    Assert.AreEqual(6, DivideAndConquerTask.DivCon(new object[] { 0, 1, 2, 3 }));
+                    Assert.AreEqual(0, DivideAndConquerTask.DivCon(new object[] { 1, "1" }));
+                    Assert.AreEqual(-2, DivideAndConquerTask.DivCon(new object[] { -1, "1" }));
+                    Assert.AreEqual(2, DivideAndConquerTask.DivCon(new object[] { 1, "-1" }));
+                    Assert.AreEqual(1, DivideAndConquerTask.DivCon(new object[] { 1 }));
+                    Assert.AreEqual(-1, DivideAndConquerTask.DivCon(new object[] { "1" }));
+                    Assert.AreEqual(0, DivideAndConquerTask.DivCon(new object[] { }));
+                }
+
+                [Test]
                 public void MakeAFunctionThatDoesArithmeticTest()
                 {
                     Assert.AreEqual(3, MakeAFunctionThatDoesArithmeticTask.Arithmetic(1, 2, "add"));
