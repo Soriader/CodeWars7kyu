e6930fb614456c66637e2f3303ae8fe12f354fd8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-11 13:06:29 +0200

    [~] Task: Over The Road

---
+++ CodeWars7kyu/OverTheRoadTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class OverTheRoadTask
+    {
+        public static long OverTheRoad(long address, long n)
+        {
+            return 2 * n + 1 - address;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void OverTheRoadTest()
+                {
+                    Assert.AreEqual(6, OverTheRoadTask.OverTheRoad(1, 3));
+                    Assert.AreEqual(4, OverTheRoadTask.OverTheRoad(3, 3));
+                    Assert.AreEqual(5, OverTheRoadTask.OverTheRoad(2, 3));
+                    Assert.AreEqual(8, OverTheRoadTask.OverTheRoad(3, 5));
+                    Assert.AreEqual(16, OverTheRoadTask.OverTheRoad(7, 11));
+                }
+
+                [Test]
                 public void DivideAndConquerTest()
                 {
                     Assert.AreEqual(2, DivideAndConquerTask.DivCon(new object[] { 9, 3, "7", "3" }));
