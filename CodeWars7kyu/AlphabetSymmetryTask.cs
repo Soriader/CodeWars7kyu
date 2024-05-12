ae613d48e559ad5c2bfae52f88032912c410173d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-12 18:50:22 +0200

    [~] Task: Alphabet symmetry

---
+++ CodeWars7kyu/AlphabetSymmetryTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class AlphabetSymmetryTask
+    {
+        public static List<int> Solve(List<string> arr)
+        {
+            List<int> result = new List<int>();
+
+            foreach (string word in arr)
+            {
+                int count = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char letter = char.ToUpper(word[i]);
+
+                    if (letter - 'A' + 1 == i + 1)
+                    {
+                        count++;
+                    }
+                }
+                result.Add(count);
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void AlphabetSymmetryTest()
+                {
+                    Assert.That(AlphabetSymmetryTask.Solve(new List<string> { "abode", "ABc", "xyzD" }), Is.EqualTo(new List<int> { 4, 3, 1 }));
+                    Assert.That(AlphabetSymmetryTask.Solve(new List<string> { "abide", "ABc", "xyz" }), Is.EqualTo(new List<int> { 4, 3, 0 }));
+                    Assert.That(AlphabetSymmetryTask.Solve(new List<string> { "IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc" }), Is.EqualTo(new List<int> { 6, 5, 7 }));
+                    Assert.That(AlphabetSymmetryTask.Solve(new List<string> { "encode", "abc", "xyzD", "ABmD" }), Is.EqualTo(new List<int> { 1, 3, 1, 3 }));
+                }
+
+                [Test]
                 public void OverTheRoadTest()
                 {
                     Assert.AreEqual(6, OverTheRoadTask.OverTheRoad(1, 3));
