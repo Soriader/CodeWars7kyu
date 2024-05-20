708f43c2aa2069dad5adfd830cf4c0c64ff61f4e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-20 16:59:28 +0200

    [~] Task: Balanced Number (Special Numbers Series #1 )

---
+++ CodeWars7kyu/BalancedNumberTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class BalancedNumberTask
+    {
+        public static string BalancedNumber(int number)
+        {
+            var digits = number.ToString().ToCharArray();
+            int length = digits.Length;
+            int middle = length / 2;
+
+            int start = 0;
+            int end = 0;
+
+            if (length % 2 == 0)
+            {
+                for (int i = 0; i < middle - 1; i++)
+                {
+                    start += digits[i] - '0';
+                }
+                for (int i = middle + 1; i < length; i++)
+                {
+                    end += digits[i] - '0';
+                }
+            }
+            else
+            {
+                for (int i = 0; i < middle; i++)
+                {
+                    start += digits[i] - '0';
+                }
+                for (int i = middle + 1; i < length; i++)
+                {
+                    end += digits[i] - '0';
+                }
+            }
+
+            if (start == end)
+            {
+                return "Balanced";
+            }
+            else
+            {
+                return "Not Balanced";
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/5a4e3782880385ba68000018/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,20 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void NBalancedNumberTest()
+                {
+                    Assert.AreEqual("Not Balanced", BalancedNumberTask.BalancedNumber(1024));
+                    Assert.AreEqual("Not Balanced", BalancedNumberTask.BalancedNumber(66545));
+                    Assert.AreEqual("Not Balanced", BalancedNumberTask.BalancedNumber(295591));
+                    Assert.AreEqual("Not Balanced", BalancedNumberTask.BalancedNumber(1230987));
+                    Assert.AreEqual("Balanced", BalancedNumberTask.BalancedNumber(7));
+                    Assert.AreEqual("Balanced", BalancedNumberTask.BalancedNumber(959));
+                    Assert.AreEqual("Balanced", BalancedNumberTask.BalancedNumber(13));
+                    Assert.AreEqual("Balanced", BalancedNumberTask.BalancedNumber(424));
+                    Assert.AreEqual("Balanced", BalancedNumberTask.BalancedNumber(56239814));
+                }
+
+                [Test]
                 public void SumOfCubesTest()
                 {
                     Assert.AreEqual(1, SumOfCubesTask.SumCubes(1));
