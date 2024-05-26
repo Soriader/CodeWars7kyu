7d935e5a814484a6d3684573b48750ab53e2897d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-26 17:54:55 +0200

    [~] Task: Simple string characters

---
+++ CodeWars7kyu/SimpleStringCharactersTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SimpleStringCharactersTask
+    {
+        public static int[] Solve(String s)
+        {
+            int uppercaseLetters = 0;
+            int lowercase = 0;
+            int numbers = 0;
+            int specialCharacters = 0;
+
+            char[] check = s.ToCharArray();
+
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (char.IsUpper(check[i]))
+                {
+                    uppercaseLetters++;
+                }
+
+                if (char.IsLower(check[i]))
+                {
+                    lowercase++;
+                }
+
+                if (char.IsDigit(check[i]))
+                {
+                    numbers++;
+                }
+
+                if (!char.IsLetterOrDigit(check[i]))
+                {
+                    specialCharacters++;
+                }
+            }
+
+            return new int[] { uppercaseLetters, lowercase, numbers, specialCharacters };
+        }
+    }
+}
+//https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void SimpleStringCharactersTest()
+                {
+                    Assert.AreEqual(new int[] { 1, 18, 3, 2 }, SimpleStringCharactersTask.Solve("Codewars@codewars123.com"));
+                    Assert.AreEqual(new int[] { 7, 6, 3, 2 }, SimpleStringCharactersTask.Solve("bgA5<1d-tOwUZTS8yQ"));
+                    Assert.AreEqual(new int[] { 9, 9, 6, 9 }, SimpleStringCharactersTask.Solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"));
+                    Assert.AreEqual(new int[] { 15, 8, 6, 9 }, SimpleStringCharactersTask.Solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"));
+                }
+
+                [Test]
                 public void SumOfOddCubedNumbersTest()
                 {
                     Assert.AreEqual(28, SumOfOddCubedNumbersTask.CubeOdd(new int[] { 1, 2, 3, 4 }));
