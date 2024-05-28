87e9a9c6367515888cdf28fa483c58d986878320
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-28 12:13:00 +0200

    [~] Task: Maximum Triplet Sum (Array Series #7)

---
+++ CodeWars7kyu/MaximumTripletSumTask.cs
@@ -0,0 +1,32 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeWars7kyu.DisemvowelTrollsTask;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+    public class MaximumTripletSumTask
+    {
+        public static int MaxTriSum(int[] a)
+        {
+            int[] set = a.Distinct().ToArray();
+
+            List<int> list = set.ToList();
+            list.Sort((x, y) => y.CompareTo(x));
+
+            if (list.Count < 3)
+            {
+                return 0;
+            }
+
+            int result = list[0] + list[1] + list[2];
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/5aa1bcda373c2eb596000112/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void MaximumTripletSumTest()
+                {
+                    Assert.AreEqual(0, MaximumTripletSumTask.MaxTriSum(new int[] { -2, 0, 2 }));
+                    Assert.AreEqual(17, MaximumTripletSumTask.MaxTriSum(new int[] { 3, 2, 6, 8, 2, 3 }));
+                    Assert.AreEqual(-9, MaximumTripletSumTask.MaxTriSum(new int[] { -3, -27, -4, -2, -27, -2 }));
+                    Assert.AreEqual(232, MaximumTripletSumTask.MaxTriSum(new int[] { -13, -50, 57, 13, 67, -13, 57, 108, 67 }));
+
+                }
+
+                [Test]
                 public void SortOutTheMenFromBoysTest()
                 {
                     Assert.AreEqual(new int[] { 14, 17, 7, 3 }, SortOutTheMenFromBoysTask.MenFromBoys(new int[] { 7, 3, 14, 17 }));
