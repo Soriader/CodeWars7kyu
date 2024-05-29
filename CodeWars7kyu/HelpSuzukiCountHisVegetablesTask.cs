43156a335f4c33c99c15c934de9686b79490c2a2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-29 11:32:53 +0200

    [~] Task: Help Suzuki count his vegetables....

---
+++ CodeWars7kyu/HelpSuzukiCountHisVegetablesTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class HelpSuzukiCountHisVegetablesTask
+    {
+        public static List<Tuple<int, string>> CountVegetables(string s)
+        {
+
+            var validVegetables = new HashSet<string>
+            {
+            "cabbage", "carrot", "celery", "cucumber", "mushroom",
+            "onion", "pepper", "potato", "tofu", "turnip"
+            };
+
+            var vegetableCounts = s.Split(' ')
+                .Where(word => validVegetables.Contains(word))
+                .GroupBy(word => word)
+                .Select(group => new Tuple<int, string>(group.Count(), group.Key))
+                .OrderByDescending(tuple => tuple.Item1)
+                .ThenByDescending(tuple => tuple.Item2)
+                .ToList();
+
+            return vegetableCounts;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/56ff1667cc08cacf4b00171b/train/csharp
