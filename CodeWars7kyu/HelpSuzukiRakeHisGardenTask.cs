eb37bc95edc889462e955b0ba25f7ac82c704ee3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-29 12:04:07 +0200

    [~] Task: Help Suzuki rake his garden!

---
+++ CodeWars7kyu/HelpSuzukiRakeHisGardenTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class HelpSuzukiRakeHisGardenTask
+    {
+        public static string RakeGarden(string garden)
+        {
+            var cleanGarden = garden.Split(' ');
+            StringBuilder resultBuilder = new StringBuilder();
+
+            foreach (var item in cleanGarden)
+            {
+                if (item == "gravel" || item == "rock")
+                {
+                    resultBuilder.Append(item).Append(" ");
+                }
+                else
+                {
+                    resultBuilder.Append("gravel").Append(" ");
+                }
+            }
+
+            return resultBuilder.ToString().Trim();
+        }
+    }
+}
+//https://www.codewars.com/kata/571c1e847beb0a8f8900153d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,19 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+
+                [Test]
+                public void HelpSuzukiRakeHisGardenTest()
+                {
+                    var garden1 = "slug spider rock gravel gravel gravel gravel gravel gravel gravel";
+                    var expectedGarden1 = "gravel gravel rock gravel gravel gravel gravel gravel gravel gravel";
+                    Assert.AreEqual(expectedGarden1, HelpSuzukiRakeHisGardenTask.RakeGarden(garden1));
+
+                    var garden2 = "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel";
+                    var expectedGarden2 = "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel";
+                    Assert.AreEqual(expectedGarden2, HelpSuzukiRakeHisGardenTask.RakeGarden(garden2));
+                }
+
                 [Test]
                 public void HelpSuzukiCountHisVegetablesTest()
                 {
