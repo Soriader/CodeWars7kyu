9841067b19f8070b8d796c04a74ec563b85ec6e9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-05-31 12:49:45 +0200

    [~] Task: Incrementer

---
+++ CodeWars7kyu/IncrementerTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class IncrementerTask
+    {
+        public static int[] Incrementer(int[] numbers)
+        {
+            var result = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = (numbers[i] + (i + 1)) % 10;
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/590e03aef55cab099a0002e8/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,16 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+
+                [Test]
+                public void IncrementerTest()
+                {
+                    Assert.AreEqual(new int[] { 2, 4, 6 }, IncrementerTask.Incrementer(new int[] { 1, 2, 3 }));
+                    Assert.AreEqual(new int[0], IncrementerTask.Incrementer(new int[0]));
+                    Assert.AreEqual(new int[] { 5, 8, 0, 5, 8 }, IncrementerTask.Incrementer(new int[] { 4, 6, 7, 1, 3 }));
+                    Assert.AreEqual(new int[] { 2, 4, 6, 8, 0, 2, 4, 6, 8, 9, 0, 1, 2, 2 }, IncrementerTask.Incrementer(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 8 }));
+                }
+
                 [Test]
                 public void SortByLastCharTest()
                 {
