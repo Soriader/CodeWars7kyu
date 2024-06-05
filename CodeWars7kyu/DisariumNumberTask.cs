0af84d28d9555b9326afbbad1038d43950646ae9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-05 17:57:52 +0200

    [~] Task: Disarium Number (Special Numbers Series #3)

---
+++ CodeWars7kyu/DisariumNumberTask.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class DisariumNumberTask
+    {
+        public static string DisariumNumber(int number)
+        {
+            var checkTheNumber = number.ToString().ToCharArray();
+            double resultOfSum = 0;
+
+            for (int i = 0; i < checkTheNumber.Length; i++)
+            {
+                resultOfSum += Math.Pow(char.GetNumericValue(checkTheNumber[i]), i + 1);
+            }
+
+            if (number == resultOfSum) return "Disarium !!";
+
+            return "Not !!";
+        }
+    }
+}
+//https://www.codewars.com/kata/5a53a17bfd56cb9c14000003/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+                [Test]
+                public void DisariumNumberTest()
+                {
+                    Assert.AreEqual("Disarium !!", DisariumNumberTask.DisariumNumber(89));
+                    Assert.AreEqual("Not !!", DisariumNumberTask.DisariumNumber(564));
+                    Assert.AreEqual("Disarium !!", DisariumNumberTask.DisariumNumber(135));
+                    Assert.AreEqual("Not !!", DisariumNumberTask.DisariumNumber(136586));
+
+                }
 
                 [Test]
                 public void CurryingFunctionsMultiplyAllElementsInAnArrayTest()
