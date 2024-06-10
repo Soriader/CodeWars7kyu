7cbd9115f49088d1e233a45e84ccac6f56167e6d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-10 17:35:16 +0200

    [~] Task: Digital cypher

---
+++ CodeWars7kyu/DigitalCypherTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class DigitalCypherTask
+    {
+        public static int[] Encode(string str, int n)
+        {
+            var checkTheLetter = str.ToLower().ToCharArray();
+            var checkTheNumber = n.ToString().ToCharArray();
+
+            int[] result = new int[str.Length];
+
+            for (int i = 0; i < checkTheLetter.Length; i++)
+            {
+                if (checkTheLetter[i] < 'a' || checkTheLetter[i] > 'z')
+                {
+                    throw new ArgumentException("Not a letter!");
+                }
+
+                var numberOfLetter = checkTheLetter[i] - 'a' + 1;
+
+                int keyDigit = checkTheNumber[i % checkTheNumber.Length] - '0'; 
+
+                result[i] = numberOfLetter + keyDigit;
+
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/592e830e043b99888600002d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -17,6 +17,14 @@ namespace CodeWarsTest
             {
 
                 [Test]
+                public void DigitalCypherTest()
+                {
+                    Assert.AreEqual(new int[] { 20, 12, 18, 30, 21 }, DigitalCypherTask.Encode("scout", 1939));
+                    Assert.AreEqual(new int[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 }, DigitalCypherTask.Encode("masterpiece", 1939));
+
+                }
+
+                [Test]
                 public static void GoingToTheCinemaTest()
                 {
                     Assert.AreEqual(43, GoingToTheCinemaTask.Movie(500, 15, 0.9));
