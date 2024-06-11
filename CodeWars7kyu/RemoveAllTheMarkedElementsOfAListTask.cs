60afc5e4f97f49899f9cb01f9a58bd420e927a6f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-11 20:32:05 +0200

    [~] Task: Remove All The Marked Elements of a List

---
+++ CodeWars7kyu/RemoveAllTheMarkedElementsOfAListTask.cs
@@ -0,0 +1,30 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeWars7kyu.DisemvowelTrollsTask;
+
+namespace CodeWars7kyu
+{
+    public class RemoveAllTheMarkedElementsOfAListTask
+    {
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int num in integerList)
+            {
+                if (!valuesList.Contains(num))
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+//https://www.codewars.com/kata/563089b9b7be03472d00002b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,14 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+                [Test]
+                public void RemoveAllTheMarkedElementsOfAListTest()
+                {
+
+                    CollectionAssert.AreEqual(new int[] { 2, 2, 4 }, RemoveAllTheMarkedElementsOfAListTask.Remove(new int[] { 1, 1, 2, 3, 1, 2, 3, 4 }, new int[] { 1, 3 }));
+                    CollectionAssert.AreEqual(new int[] { 5, 6, 7, 8 }, RemoveAllTheMarkedElementsOfAListTask.Remove(new int[] { 1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8 }, new int[] { 1, 3, 4, 2 }));
+
+                }
 
                 [Test]
                 public void TheOfficeIIBoredomScoreTest()
