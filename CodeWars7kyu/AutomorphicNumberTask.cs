4d2bc3afe00b6f7176da2dc98f63014765500aa7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-12 13:52:31 +0200

    [~] Task: Automorphic Number (Special Numbers Series #6)

---
+++ CodeWars7kyu/AutomorphicNumberTask.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeWars7kyu.DisemvowelTrollsTask;
+
+namespace CodeWars7kyu
+{
+    public class AutomorphicNumberTask
+    {
+        public static string Automorphic(int n)
+        {
+            double square = Math.Pow(n, 2);
+
+            string nStr = n.ToString();
+            string squareStr = square.ToString();
+
+            if (squareStr.EndsWith(nStr))
+            {
+                return "Automorphic";
+            }
+
+            return "Not!!";
+        }
+    }
+}
+//https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,18 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void AutomorphicNumberTest()
+                {
+                    Assert.AreEqual("Automorphic", AutomorphicNumberTask.Automorphic(1));
+                    Assert.AreEqual("Automorphic", AutomorphicNumberTask.Automorphic(6));
+                    Assert.AreEqual("Automorphic", AutomorphicNumberTask.Automorphic(625));
+                    Assert.AreEqual("Not!!", AutomorphicNumberTask.Automorphic(3));
+                    Assert.AreEqual("Not!!", AutomorphicNumberTask.Automorphic(95));
+                    Assert.AreEqual("Not!!", AutomorphicNumberTask.Automorphic(225));
+                }
+
+
+                [Test]
                 public void RemoveAllTheMarkedElementsOfAListTest()
                 {
 
