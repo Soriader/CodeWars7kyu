c34385611d06605c611a1d250d9b81e6192ef1d5
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-12 14:05:33 +0200

    [~] Task: Unlucky Days

---
+++ CodeWars7kyu/UnluckyDaysTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class UnluckyDaysTask
+    {
+        public static int UnluckyDays(int year)
+        {
+            int count = 0;
+
+            for (int month = 1; month <= 12; month++)
+            {
+                DateTime date = new DateTime(year, month, 13);
+
+                if (date.DayOfWeek == DayOfWeek.Friday)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,21 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void UnluckyDaysTest()
+                {
+                    Assert.AreEqual(1, UnluckyDaysTask.UnluckyDays(1586));
+                    Assert.AreEqual(3, UnluckyDaysTask.UnluckyDays(1001));
+                    Assert.AreEqual(2, UnluckyDaysTask.UnluckyDays(2819));
+                    Assert.AreEqual(2, UnluckyDaysTask.UnluckyDays(2792));
+                    Assert.AreEqual(2, UnluckyDaysTask.UnluckyDays(2723));
+                    Assert.AreEqual(1, UnluckyDaysTask.UnluckyDays(1909));
+                    Assert.AreEqual(2, UnluckyDaysTask.UnluckyDays(1812));
+                    Assert.AreEqual(2, UnluckyDaysTask.UnluckyDays(1618));
+                    Assert.AreEqual(1, UnluckyDaysTask.UnluckyDays(2132));
+                    Assert.AreEqual(3, UnluckyDaysTask.UnluckyDays(2065));
+                }
+
+                [Test]
                 public void AutomorphicNumberTest()
                 {
                     Assert.AreEqual("Automorphic", AutomorphicNumberTask.Automorphic(1));
