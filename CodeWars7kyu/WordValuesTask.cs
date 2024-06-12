3dbd675f534c62d2a5559551a2f91db2e437feb2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-12 14:54:13 +0200

    [~] Task: Word values

---
+++ CodeWars7kyu/WordValuesTask.cs
@@ -0,0 +1,39 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeWars7kyu.DisemvowelTrollsTask;
+
+namespace CodeWars7kyu
+{
+    public class WordValuesTask
+    {
+        public static int[] WordValue(string[] a)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                string word = a[i];
+                int wordValue = 0;
+
+                foreach (char c in word)
+                {
+                    if (char.IsLetter(c)) 
+                    {
+                        wordValue += (int)c - 96; 
+                    }
+                }
+
+                wordValue *= (i + 1); 
+                result.Add(wordValue);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+//https://www.codewars.com/kata/598d91785d4ce3ec4f000018/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void WordValuesTest()
+                {
+                    Assert.AreEqual(new int[] { 88, 12, 225 }, WordValuesTask.WordValue(new string[] { "codewars", "abc", "xyz" }));
+                    Assert.AreEqual(new int[] { 12, 24, 18, 24 }, WordValuesTask.WordValue(new string[] { "abc abc", "abc abc", "abc", "abc" }));
+
+                }
+
+                [Test]
                 public void UnluckyDaysTest()
                 {
                     Assert.AreEqual(1, UnluckyDaysTask.UnluckyDays(1586));
