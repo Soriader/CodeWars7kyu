768856a33f3cbbd47f8b987b673deb8b981d474b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-13 16:29:46 +0200

    [~] Task: Love vs friendship

---
+++ CodeWars7kyu/LoveVsFriendshipTask.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class LoveVsFriendshipTask
+    {
+        public static int WordsToMarks(string str)
+        {
+            var sumOfNumer = str.ToLower().Where(char.IsLetter).Sum(c => c - 'a' + 1);
+
+            return sumOfNumer;
+        }
+    }
+}
+//https://www.codewars.com/kata/59706036f6e5d1e22d000016/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void LoveVsFriendshipTest()
+                {
+                    Assert.AreEqual(100, LoveVsFriendshipTask.WordsToMarks("attitude"));
+                    Assert.AreEqual(75, LoveVsFriendshipTask.WordsToMarks("friends"));
+                    Assert.AreEqual(66, LoveVsFriendshipTask.WordsToMarks("family"));
+                    Assert.AreEqual(96, LoveVsFriendshipTask.WordsToMarks("knowledge"));
+
+                }
+
+                [Test]
                 public void FindTheNthDigitOfANumberTest()
                 {
                     Assert.AreEqual(5, FindTheNthDigitOfANumberTask.FindDigit(5673, 4));
