fa4a2e9f86bf269e0df75e3bfe0d0e61048eb490
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-13 17:32:00 +0200

    [~] Task: The old switcheroo

---
+++ CodeWars7kyu/TheOldSwitcheroTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class TheOldSwitcheroTask
+    {
+        public static string Vowel2Index(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            string result = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (vowels.Contains(str[i]))
+                {
+                    result += (i + 1).ToString();
+                }
+                else
+                {
+                    result += str[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/55d410c492e6ed767000004f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void TheOldSwitcheroTest()
+                {
+                    Assert.AreEqual("th3s 6s my str15ng", TheOldSwitcheroTask.Vowel2Index("this is my string"));
+                    Assert.AreEqual("C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld", TheOldSwitcheroTask.Vowel2Index("Codewars is the best site in the world"));
+                    Assert.AreEqual("T2m4rr7w 10s g1415ng t20 b23 r2627n29ng", TheOldSwitcheroTask.Vowel2Index("Tomorrow is going to be raining"));
+                }
+
+                [Test]
                 public void LoveVsFriendshipTest()
                 {
                     Assert.AreEqual(100, LoveVsFriendshipTask.WordsToMarks("attitude"));
