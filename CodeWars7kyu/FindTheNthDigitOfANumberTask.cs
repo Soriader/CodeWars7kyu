e7b405b4a43aa0edced9bfe668343edcae28e447
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-13 16:12:46 +0200

    [~] Task: Find the nth Digit of a Number

---
+++ CodeWars7kyu/FindTheNthDigitOfANumberTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class FindTheNthDigitOfANumberTask
+    {
+        public static int FindDigit(int num, int nth)
+        {
+            if (nth <= 0) return -1;
+
+            num = Math.Abs(num);
+
+            var digits = num.ToString().ToCharArray();
+
+            if (nth > digits.Length) return 0;
+
+            return digits[digits.Length - nth] - '0';
+        }
+    }
+}
+//https://www.codewars.com/kata/577b9960df78c19bca00007e/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,18 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void FindTheNthDigitOfANumberTest()
+                {
+                    Assert.AreEqual(5, FindTheNthDigitOfANumberTask.FindDigit(5673, 4));
+                    Assert.AreEqual(2, FindTheNthDigitOfANumberTask.FindDigit(129, 2));
+                    Assert.AreEqual(8, FindTheNthDigitOfANumberTask.FindDigit(-2825, 3));
+                    Assert.AreEqual(0, FindTheNthDigitOfANumberTask.FindDigit(-456, 4));
+                    Assert.AreEqual(0, FindTheNthDigitOfANumberTask.FindDigit(0, 20));
+                    Assert.AreEqual(-1, FindTheNthDigitOfANumberTask.FindDigit(65, 0));
+                    Assert.AreEqual(-1, FindTheNthDigitOfANumberTask.FindDigit(24, -8));
+                }
+
+                [Test]
                 public void WordValuesTest()
                 {
                     Assert.AreEqual(new int[] { 88, 12, 225 }, WordValuesTask.WordValue(new string[] { "codewars", "abc", "xyz" }));
