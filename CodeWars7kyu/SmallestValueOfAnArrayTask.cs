d7a24a8741a5c8f31ed8ad1541316eef7338d940
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-14 16:40:13 +0200

    [~] task: Smallest value of an array

---
+++ CodeWars7kyu/SmallestValueOfAnArrayTask.cs
@@ -0,0 +1,36 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SmallestValueOfAnArrayTask
+    {
+        public static int FindSmallest(int[] numbers, string toReturn)
+        {
+            int minValue = numbers.Min();
+
+            switch (toReturn)
+            {
+                case "value":
+                    return minValue;
+
+                case "index":
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] == minValue)
+                        {
+                            return i;
+                        }
+                    }
+                    break; 
+            }
+
+            return -1; 
+        }
+    }
+}
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,12 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+                [Test]
+                public void SmallestValueOfAnArrayTest()
+                {
+                    Assert.AreEqual(1, SmallestValueOfAnArrayTask.FindSmallest(new int[] { 5, 4, 3, 2, 1 }, "value"));
+                    Assert.AreEqual(4, SmallestValueOfAnArrayTask.FindSmallest(new int[] { 5, 4, 3, 2, 1 }, "index"));
+                }
 
                 [Test]
                 public void LargestElementsTest()
