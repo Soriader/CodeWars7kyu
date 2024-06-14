af4e7a699864b351fdec5b5a788d2f50593f9183
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-14 14:27:40 +0200

    [~] Task: Nth Smallest Element (Array Series #4)

---
+++ CodeWars7kyu/NthSmallestElementTask.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class NthSmallestElementTask
+    {
+        public static int NthSmallest(int[] arr, int pos)
+        {
+            int result = 0;
+            arr = arr.OrderBy(x => x).ToArray();
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(i+1 == pos)
+                {
+                    result = arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/5a512f6a80eba857280000fc/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void NthSmallestElementTest()
+                {
+                    Assert.AreEqual(2, NthSmallestElementTask.NthSmallest(new int[] { 3, 1, 2 }, 2));
+                    Assert.AreEqual(7, NthSmallestElementTask.NthSmallest(new int[] { 15, 20, 7, 10, 4, 3 }, 3));
+                    Assert.AreEqual(2, NthSmallestElementTask.NthSmallest(new int[] { 2, 169, 13, -5, 0, -1 }, 4));
+                    Assert.AreEqual(2, NthSmallestElementTask.NthSmallest(new int[] { 2, 1, 3, 3, 1, 2 }, 3));
+                }
+
+                [Test]
                 public void TheOldSwitcheroTest()
                 {
                     Assert.AreEqual("th3s 6s my str15ng", TheOldSwitcheroTask.Vowel2Index("this is my string"));
