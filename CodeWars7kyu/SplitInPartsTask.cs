e0b9ad9b7928d72dc25255df577587138e96b679
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-16 23:11:51 +0200

    [~] Task: Split In Parts

---
+++ CodeWars7kyu/SplitInPartsTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SplitInPartsTask
+    {
+        public static string SplitInParts(string s, int partLength)
+        {
+            var letter = s.ToCharArray();
+            int iterator = 0;
+            string result = "";
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                result += letter[i];
+                iterator ++;    
+                if(iterator == partLength)
+                {
+                    result += " ";
+                    iterator = 0;
+                }
+            }
+
+            return result.Trim();
+        }
+    }
+}
+//https://www.codewars.com/kata/5650ab06d11d675371000003/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,15 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+                [Test]
+                public void SplitInPartsTest()
+                {
+                    string str = "HelloKata";
+                    Assert.AreEqual("HelloKata", SplitInPartsTask.SplitInParts(str, 9));
+
+                    string str1 = "HelloKata";
+                    Assert.AreEqual("H e l l o K a t a", SplitInPartsTask.SplitInParts(str1, 1));
+                }
 
                 [Test]
                 public void LongestVowelChainTest()
