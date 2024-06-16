9da5239e7d762e38e2e49a829513ab0385968a08
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-16 23:39:36 +0200

    [~] Task: Last Survivor

---
+++ CodeWars7kyu/LastSurvivorTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class LastSurvivorTask
+    {
+        public static string LastSurvivor(string letters, int[] coords)
+        {
+            List<char> letterList = new List<char>(letters);
+
+            foreach (int index in coords)
+            {
+                letterList.RemoveAt(index);
+            }
+
+            return letterList[0].ToString();
+
+        }
+    }
+}
+//https://www.codewars.com/kata/609eee71109f860006c377d1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -17,6 +17,56 @@ namespace CodeWarsTest
             {
 
                 [Test]
+                public void LastSurvivorTest()
+                {
+                    // Test 1: letters: "abc" coords: [1, 1] -> expected: "a"
+                    {
+                        string letters = "abc";
+                        int[] coords = { 1, 1 };
+                        string expected = "a";
+                        string actual = LastSurvivorTask.LastSurvivor(letters, coords);
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                    // Test 2: letters: "kbc" coords: [0, 1] -> expected: "b"
+                    {
+                        string letters = "kbc";
+                        int[] coords = { 0, 1 };
+                        string expected = "b";
+                        string actual = LastSurvivorTask.LastSurvivor(letters, coords);
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                    // Test 3: letters: "zbk" coords: [2, 1] -> expected: "z"
+                    {
+                        string letters = "zbk";
+                        int[] coords = { 2, 1 };
+                        string expected = "z";
+                        string actual = LastSurvivorTask.LastSurvivor(letters, coords);
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                    // Test 4: letters: "c" coords: [] -> expected: "c"
+                    {
+                        string letters = "c";
+                        int[] coords = { };
+                        string expected = "c";
+                        string actual = LastSurvivorTask.LastSurvivor(letters, coords);
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                    // Test 5: long example
+                    {
+                        string letters = "foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr";
+                        int[] coords = { 8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1 };
+                        string expected = "d";
+                        string actual = LastSurvivorTask.LastSurvivor(letters, coords);
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+
+
+                [Test]
                 public void FindTheVowelsTest()
                 {
                     Assert.AreEqual(new int[] { }, FindTheVowelsTask.VowelIndices("mmm"));
