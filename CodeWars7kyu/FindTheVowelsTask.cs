b14ca9633f33c0edc492d223ed864eca53d5ab12
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-16 23:23:31 +0200

    [~] Task: Find the vowels

---
+++ CodeWars7kyu/FindTheVowelsTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class FindTheVowelsTask
+    {
+        public static int[] VowelIndices(string word)
+        {
+            var letter = word.ToLower().ToCharArray();
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (letter[i].ToString().Any(c => c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y'))
+                {
+                    result.Add(i+1);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+//https://www.codewars.com/kata/5680781b6b7c2be860000036/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,17 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
+
+                [Test]
+                public void FindTheVowelsTest()
+                {
+                    Assert.AreEqual(new int[] { }, FindTheVowelsTask.VowelIndices("mmm"));
+                    Assert.AreEqual(new int[] { 1, 5 }, FindTheVowelsTask.VowelIndices("apple"));
+                    Assert.AreEqual(new int[] { 2, 4 }, FindTheVowelsTask.VowelIndices("super"));
+                    Assert.AreEqual(new int[] { 1, 3, 6 }, FindTheVowelsTask.VowelIndices("orange"));
+                    Assert.AreEqual(new int[] { 2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 25, 27, 29, 31, 32, 33 }, FindTheVowelsTask.VowelIndices("supercalifragilisticexpialidocious"));
+                }
+
                 [Test]
                 public void SplitInPartsTest()
                 {
