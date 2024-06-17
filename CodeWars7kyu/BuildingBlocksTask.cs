6ee947cbc9d20cc76b1cfb2fc45a33069354d40c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-17 20:45:50 +0200

    [~] Task: Building blocks

---
+++ CodeWars7kyu/BuildingBlocksTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class BuildingBlocksTask
+    {
+        public int Width { get; private set; }
+        public int Length { get; private set; }
+        public int Height { get; private set; }
+
+        public BuildingBlocksTask(int[] properties)
+        {
+            this.Width = properties[0];
+            this.Length = properties[1];
+            this.Height = properties[2];
+        }
+
+        public int GetWidth() { return Width; }
+
+        public int GetLength() { return Length; }
+
+        public int GetHeight() { return Height; }
+
+        public int GetVolume() { return GetWidth() * GetHeight() * GetLength(); }
+
+        public int GetSurfaceArea()
+        {
+            return (2 * GetLength() * GetWidth())
+                   + (2 * GetLength() * GetHeight())
+                   + (2 * GetWidth() * GetHeight());
+        }
+    }
+}
+//https://www.codewars.com/kata/55b75fcf67e558d3750000a3/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,17 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void BuildingBlocksTest()
+                {
+                    BuildingBlocksTask b = new BuildingBlocksTask(new int[] { 2, 2, 2 });
+                    Assert.AreEqual(2, b.GetWidth());
+                    Assert.AreEqual(2, b.GetLength());
+                    Assert.AreEqual(2, b.GetHeight());
+                    Assert.AreEqual(8, b.GetVolume());
+                    Assert.AreEqual(24, b.GetSurfaceArea());
+                }
+
+                [Test]
                 public void DifferenceOfSquaresTest()
                 {
                     Assert.AreEqual(170, DifferenceOfSquaresTask.DifferenceOfSquares(5));
