bc1df43c18c1d4ea58b5f8cce030b85abb545452
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-18 10:55:57 +0200

    [~] Task: ToLeetSpeak

---
+++ CodeWars7kyu/ToLeetSpeakTask.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ToLeetSpeakTask
+    {
+        private static readonly Dictionary<char, char> LeetDictionary = new Dictionary<char, char>
+    {
+        {'A', '@'},
+        {'B', '8'},
+        {'C', '('},
+        {'D', 'D'},
+        {'E', '3'},
+        {'F', 'F'},
+        {'G', '6'},
+        {'H', '#'},
+        {'I', '!'},
+        {'J', 'J'},
+        {'K', 'K'},
+        {'L', '1'},
+        {'M', 'M'},
+        {'N', 'N'},
+        {'O', '0'},
+        {'P', 'P'},
+        {'Q', 'Q'},
+        {'R', 'R'},
+        {'S', '$'},
+        {'T', '7'},
+        {'U', 'U'},
+        {'V', 'V'},
+        {'W', 'W'},
+        {'X', 'X'},
+        {'Y', 'Y'},
+        {'Z', '2'}
+    };
+
+        public static string ToLeetSpeak(string str)
+        {
+            char[] leetChars = new char[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char currentChar = str[i];
+                if (LeetDictionary.ContainsKey(currentChar))
+                {
+                    leetChars[i] = LeetDictionary[currentChar];
+                }
+                else
+                {
+                    leetChars[i] = currentChar; 
+                }
+            }
+
+            return new string(leetChars);
+
+        }
+    }
+}
+//https://www.codewars.com/kata/57c1ab3949324c321600013f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ToLeetSpeakTest()
+                {
+                    Assert.AreEqual("1337", ToLeetSpeakTask.ToLeetSpeak("LEET"));
+                    Assert.AreEqual("(0D3W@R$", ToLeetSpeakTask.ToLeetSpeak("CODEWARS"));
+                    Assert.AreEqual("#3110 W0R1D", ToLeetSpeakTask.ToLeetSpeak("HELLO WORLD"));
+                    Assert.AreEqual("10R3M !P$UM D010R $!7 @M37", ToLeetSpeakTask.ToLeetSpeak("LOREM IPSUM DOLOR SIT AMET"));
+                    Assert.AreEqual("7#3 QU!(K 8R0WN F0X JUMP$ 0V3R 7#3 1@2Y D06", ToLeetSpeakTask.ToLeetSpeak("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+                }
+
+                [Test]
                 public void BuildingBlocksTest()
                 {
                     BuildingBlocksTask b = new BuildingBlocksTask(new int[] { 2, 2, 2 });
