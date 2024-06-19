91bfbc1e2045f88c81bac306fc764fdcf22e1900
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-19 23:07:10 +0200

    [~] Task: Special Number (Special Numbers Series #5)

---
+++ CodeWars7kyu/SpecialNumberFiveTask.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SpecialNumberFiveTask
+    {
+        public static string SpecialNumber(int number)
+        {
+            var check = number.ToString().ToCharArray();
+
+
+            foreach (var item in check)
+            {
+                var condition = item.ToString().Any(x => x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5');
+
+                if (condition == false)
+                {
+                    return "NOT!!";
+                }
+            }
+
+            return "Special!!";
+        }
+    }
+}
+//https://www.codewars.com/kata/5a55f04be6be383a50000187/train/csharp
