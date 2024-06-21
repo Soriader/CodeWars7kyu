f74f2f57d584309f87dd5fe1a8020f28f53003a9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-21 12:32:58 +0200

    [~] Task: Number Of Occurrences

---
+++ CodeWars7kyu/NumberOfOccurrencesTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class NumberOfOccurrencesTask
+    {
+        public static int NumberOfOccurrences(int x, int[] xs)
+        {
+            int result = 0;
+
+            for (int i = 0; i < xs.Length; i++)
+            {
+                if (xs[i] == x)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/52829c5fe08baf7edc00122b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void NumberOfOccurrencesTest()
+                {
+                    Assert.AreEqual(2, NumberOfOccurrencesTask.NumberOfOccurrences(4, new int[] { 4, 0, 4 }));
+                    Assert.AreEqual(0, NumberOfOccurrencesTask.NumberOfOccurrences(1, new int[] { 4, 0, 4 }));
+                    Assert.AreEqual(1, NumberOfOccurrencesTask.NumberOfOccurrences(0, new int[] { 4, 0, 4 }));
+                }
+
+                [Test]
                 public void SpecialNumberFiveTest()
                 {
                     Assert.That(SpecialNumberFiveTask.SpecialNumber(00002), Is.EqualTo("Special!!"));
