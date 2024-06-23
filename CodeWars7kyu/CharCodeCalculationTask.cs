859d16f8076a8f396126e573f224b02794f8a85d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-23 17:12:09 +0200

    [~] Task : Char Code Calculation

---
+++ CodeWars7kyu/CharCodeCalculationTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CharCodeCalculationTask
+    {
+        public static Int32 Calc(String s)
+        {
+            var letter = s.ToCharArray();
+            string total1 = "";
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                total1 += (int)letter[i];
+            }
+
+            var total2 = total1.Replace("7", "1");
+
+            var checkTotal1 = total1.ToCharArray();
+            var checkTotal2 = total2.ToCharArray();
+            int sumOfTotal1 = 0;
+            int sumOfTotal2 = 0;
+
+            for (int i = 0; i < checkTotal1.Length; i++)
+            {
+                sumOfTotal1 += (int)checkTotal1[i];
+            }
+
+            for (int i = 0; i < checkTotal2.Length; i++)
+            {
+                sumOfTotal2 += (int)checkTotal2[i];
+            }
+
+            return sumOfTotal1 - sumOfTotal2;
+        }
+    }
+}
+//https://www.codewars.com/kata/57f75cc397d62fc93d000059/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,30 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void CharCodeCalculationTest()
+                {
+                    Assert.AreEqual(0, CharCodeCalculationTask.Calc(""));
+                    Assert.AreEqual(6, CharCodeCalculationTask.Calc("abc"));
+                    Assert.AreEqual(6, CharCodeCalculationTask.Calc("ABC"));
+                    Assert.AreEqual(6, CharCodeCalculationTask.Calc("abcdef"));
+                    Assert.AreEqual(6, CharCodeCalculationTask.Calc("ifkhchlhfd"));
+                    Assert.AreEqual(6, CharCodeCalculationTask.Calc("jfmgklf8hglbe"));
+                    Assert.AreEqual(12, CharCodeCalculationTask.Calc("jaam"));
+                    Assert.AreEqual(18, CharCodeCalculationTask.Calc("AFHJD"));
+                    Assert.AreEqual(18, CharCodeCalculationTask.Calc("CodeWars"));
+                    Assert.AreEqual(24, CharCodeCalculationTask.Calc("FVJFVDF"));
+                    Assert.AreEqual(24, CharCodeCalculationTask.Calc("AOUCUAOF"));
+                    Assert.AreEqual(30, CharCodeCalculationTask.Calc("aaaaaddddr"));
+                    Assert.AreEqual(36, CharCodeCalculationTask.Calc("XLdNDcAbUkMnkV"));
+                    Assert.AreEqual(42, CharCodeCalculationTask.Calc("cSuLifxDQkOiypJsxOJSE"));
+                    Assert.AreEqual(48, CharCodeCalculationTask.Calc("ZHAUnSGoIbgPSezhjePIbHFrHUOv"));
+                    Assert.AreEqual(54, CharCodeCalculationTask.Calc("VFhSMbZETZVHxYiiYsBMrWuecDN"));
+                    Assert.AreEqual(60, CharCodeCalculationTask.Calc("sphPoGbicTCLbiuUcwFMEGaFmy"));
+                    Assert.AreEqual(72, CharCodeCalculationTask.Calc("oXoQKiCflHIHFyGizCYCuaHhX"));
+                    Assert.AreEqual(84, CharCodeCalculationTask.Calc("sUuPmNnnJOOCAGOuyzmcHQGJhXHYgZLY"));
+                }
+
+                [Test]
                 public void FindCountofMostFrequentItemInAnArrayTest()
                 {
                     Assert.AreEqual(5, FindCountofMostFrequentItemInAnArrayTask.MostFrequentItemCount(new int[] { 3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3 }));
