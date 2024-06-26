b55eb18a0c01a0b6d7914546408e412582104244
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-26 19:05:20 +0200

    [~] Task: Holiday III - Fire on the boat

---
+++ CodeWars7kyu/FireonTheBoatTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class FireonTheBoatTask
+    {
+        public static string FireFight(string s)
+        {
+            return s.Replace("Fire", "~~");
+        }
+    }
+}
+//https://www.codewars.com/kata/57e8fba2f11c647abc000944/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void FireonTheBoatTest()
+                {
+                    Assert.AreEqual("Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast", FireonTheBoatTask.FireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"));
+                    Assert.AreEqual("Mast Deck Engine Water ~~", FireonTheBoatTask.FireFight("Mast Deck Engine Water Fire"));
+                    Assert.AreEqual("~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain", FireonTheBoatTask.FireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"));
+                }
+
+                [Test]
                 public void ConsecutiveItemsTest()
                 {
                     Assert.AreEqual(false, ConsecutiveItemsTask.Consecutive(new int[] { 1, 3, 5, 7 }, 3, 7));
