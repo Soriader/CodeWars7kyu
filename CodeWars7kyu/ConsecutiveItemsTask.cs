1c65d39a607f90cc7b622c63be77c490362fb2ee
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-26 18:51:04 +0200

    [~] Task: Consecutive items

---
+++ CodeWars7kyu/ConsecutiveItemsTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ConsecutiveItemsTask
+    {
+        public static bool Consecutive(int[] arr, int a, int b)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == a && arr[i+1] == b || arr[i] == b && arr[i + 1] == a)
+                {
+                   return true;
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/5f6d533e1475f30001e47514/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,13 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ConsecutiveItemsTest()
+                {
+                    Assert.AreEqual(false, ConsecutiveItemsTask.Consecutive(new int[] { 1, 3, 5, 7 }, 3, 7));
+                    Assert.AreEqual(true, ConsecutiveItemsTask.Consecutive(new int[] { 1, 3, 5, 7 }, 3, 1));
+                    Assert.AreEqual(true, ConsecutiveItemsTask.Consecutive(new int[] { 1, 6, 9, -3, 4, -78, 0 }, -3, 4));
+                }
+                [Test]
                 public void SevenAteNineTest()
                 {
                     Assert.AreEqual("77", SevenAteNineTask.SevenAteNine("797"));
