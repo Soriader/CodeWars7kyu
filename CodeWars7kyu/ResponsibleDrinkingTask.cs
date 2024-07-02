9ee8c1afeee4c337801f0ba9a7ea8f1c56db7b19
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-02 19:07:15 +0200

    [~] Task: Responsible Drinking

---
+++ CodeWars7kyu/ResponsibleDrinkingTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ResponsibleDrinkingTask
+    {
+        public string Hydrate(string drinkString)
+        {
+            var check = drinkString.ToCharArray();
+            int howManyGlassOfWater = 0;
+
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (char.IsNumber(check[i]))
+                {
+                    howManyGlassOfWater += check[i] - '0';
+                }
+            }
+            if (howManyGlassOfWater == 1) return "1 glass of water";
+
+            return $"{howManyGlassOfWater} glasses of water";
+        }
+    }
+}
+//https://www.codewars.com/kata/5aee86c5783bb432cd000018/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,19 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ResponsibleDrinkingTest()
+                {
+                    ResponsibleDrinkingTask drinkin = new ResponsibleDrinkingTask();
+                    string expected = "10 glasses of water";
+                    string actual = drinkin.Hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer");
+                    Assert.AreEqual(expected, actual);
+
+                    string expected2 = "1 glass of water";
+                    string actual2 = drinkin.Hydrate("1 beer");
+                    Assert.AreEqual(expected, actual);
+                }
+
+                [Test]
                 public void ShorterConcatTest()
                 {
                     try
