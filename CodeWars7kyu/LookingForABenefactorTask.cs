6c8093f5cfd655b2efe970c89449752d7b83dce2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-03 13:18:21 +0200

    [~] Task: Looking for a benefactor

---
+++ CodeWars7kyu/LookingForABenefactorTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class LookingForABenefactorTask
+    {
+        public static long NewAvg(double[] arr, double navg)
+        {
+            double currentSum = arr.Sum();
+
+            int n = arr.Length;
+
+            double requiredDonation = navg * (n + 1) - currentSum;
+
+            if (requiredDonation <= 0)
+            {
+                throw new ArgumentException("The required donation should be greater than zero.");
+            }
+
+            return (long)Math.Ceiling(requiredDonation);
+        }
+    }
+}
