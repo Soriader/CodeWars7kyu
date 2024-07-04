c96e99a5f471f685e65ed4de91f7c1daf7a3938a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-04 13:29:11 +0200

    [~] Task: Ordered Count of Characters

---
+++ CodeWars7kyu/OrderedCountOfCharactersTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class OrderedCountOfCharactersTask
+    {
+        public static List<Tuple<char, int>> OrderedCount(string input)
+        {
+
+            return input.GroupBy(c => c)
+            .OrderBy(g => input.IndexOf(g.Key))
+            .Select(g => Tuple.Create(g.Key, g.Count()))
+            .ToList();
+
+
+        }
+    }
+}
+//https://www.codewars.com/kata/57a6633153ba33189e000074/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,23 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void OrderedCountOfCharactersTest()
+                {
+                    var expected = new List<Tuple<char, int>>()
+        {
+            Tuple.Create('a', 5),
+            Tuple.Create('b', 2),
+            Tuple.Create('r', 2),
+            Tuple.Create('c', 1),
+            Tuple.Create('d', 1)
+        };
+
+                    var result = OrderedCountOfCharactersTask.OrderedCount("abracadabra");
+
+                    CollectionAssert.AreEqual(expected, result);
+                }
+
+                    [Test]
                 public static void LookingForABenefactorTest()
                 {
                     static void Testing(long actual, long expected)
