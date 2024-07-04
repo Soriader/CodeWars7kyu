d67f1989745e3802d476c2672bb9b5800cb40f70
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-04 13:54:24 +0200

    [~] Task: Elevator Distance

---
+++ CodeWars7kyu/ElevatorDistanceTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+    public class ElevatorDistanceTask
+    {
+        public static int ElevatorDistance(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                result += Math.Abs(array[i] - array[i + 1]);
+
+            }
+            return result;
+
+        }
+    }
+}
+//https://www.codewars.com/kata/59f061773e532d0c87000d16/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ElevatorDistanceTest()
+                {
+                    Assert.AreEqual(9, ElevatorDistanceTask.ElevatorDistance(new[] { 5, 2, 8 }));
+                    Assert.AreEqual(2, ElevatorDistanceTask.ElevatorDistance(new[] { 1, 2, 3 }));
+                    Assert.AreEqual(18, ElevatorDistanceTask.ElevatorDistance(new[] { 7, 1, 7, 1 }));
+
+                }
+
+                [Test]
                 public void OrderedCountOfCharactersTest()
                 {
                     var expected = new List<Tuple<char, int>>()
