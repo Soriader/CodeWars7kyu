398090ba265a6d5579319329cda7511bbb6bff24
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-06 16:35:54 +0200

    [~] Task: By 3, or not by 3? That is the question . . .

---
+++ CodeWars7kyu/ByThreeOrNotTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ByThreeOrNotTask
+    {
+        public static bool DivisibleByThree(string n)
+        {
+            var check = n.ToCharArray();
+            int result = 0;
+
+            foreach (char c in check)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += c;
+                }
+            }
+
+            if(result % 3 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+//https://www.codewars.com/kata/59f7fc109f0e86d705000043/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ByThreeOrNotTest()
+                {
+                    Assert.That(ByThreeOrNotTask.DivisibleByThree("1891009"), Is.EqualTo(false));
+                    Assert.That(ByThreeOrNotTask.DivisibleByThree("00009"), Is.EqualTo(true));
+                    Assert.That(ByThreeOrNotTask.DivisibleByThree("57"), Is.EqualTo(true));
+                }
+
+                [Test]
                 public void BuildingStringsFromAHashTest()
                 {
                     Assert.AreEqual("a = 1,b = 2", BuildingStringsFromAHashTask.StringifyDict(new Dictionary<char, int> { { 'a', 1 }, { 'b', 2 } }));
