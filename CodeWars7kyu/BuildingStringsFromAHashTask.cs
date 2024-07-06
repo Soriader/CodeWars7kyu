e3efb750c1cc7e8783fcfefb46eae4be6f831ce6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-06 15:54:07 +0200

    [~] Task: Building Strings From a Hash

---
+++ CodeWars7kyu/BuildingStringsFromAHashTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class BuildingStringsFromAHashTask
+    {
+        public static string StringifyDict<TKey, TValue>(Dictionary<TKey, TValue> hash)
+        {
+            string result = "";
+
+            foreach (var item in hash)
+            {
+                result += $"{item.Key} = {item.Value},";
+            }
+
+            return result.Trim(',');
+
+        }
+    }
+}
+//https://www.codewars.com/kata/51c7d8268a35b6b8b40002f2/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void BuildingStringsFromAHashTest()
+                {
+                    Assert.AreEqual("a = 1,b = 2", BuildingStringsFromAHashTask.StringifyDict(new Dictionary<char, int> { { 'a', 1 }, { 'b', 2 } }));
+                    Assert.AreEqual("b = 1,c = 2,e = 3", BuildingStringsFromAHashTask.StringifyDict(new Dictionary<char, int> { { 'b', 1 }, { 'c', 2 }, { 'e', 3 } }));
+                    Assert.AreEqual("", BuildingStringsFromAHashTask.StringifyDict(new Dictionary<char, int>()));
+                }
+
+                [Test]
                 public void ElevatorDistanceTest()
                 {
                     Assert.AreEqual(9, ElevatorDistanceTask.ElevatorDistance(new[] { 5, 2, 8 }));
