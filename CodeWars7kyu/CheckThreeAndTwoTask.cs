2f6eb6c973537a2e577e7784c03acd2a842e66cf
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-08 10:53:48 +0200

    [~] Task: Check three and two

---
+++ CodeWars7kyu/CheckThreeAndTwoTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CheckThreeAndTwoTask
+    {
+        public static bool CheckThreeAndTwo(string[] array)
+        {
+            Dictionary<string, int> checkTheLetter = new Dictionary<string, int>();
+
+            foreach (string item in array)
+            {
+                if (checkTheLetter.ContainsKey(item))
+                {
+                    checkTheLetter[item]++;
+                }
+                else
+                {
+                    checkTheLetter[item] = 1;
+                }
+            }
+
+            bool hasThree = checkTheLetter.ContainsValue(3);
+            bool hasTwo = checkTheLetter.ContainsValue(2);
+
+            return hasThree && hasTwo;
+        }
+    }
+}
+//https://www.codewars.com/kata/5a9e86705ee396d6be000091/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void CheckThreeAndTwoTest()
+                {
+                    Assert.AreEqual(true, CheckThreeAndTwoTask.CheckThreeAndTwo(new string[] { "a", "a", "a", "b", "b" }));
+                    Assert.AreEqual(false, CheckThreeAndTwoTask.CheckThreeAndTwo(new string[] { "a", "c", "a", "c", "b" }));
+                    Assert.AreEqual(false, CheckThreeAndTwoTask.CheckThreeAndTwo(new string[] { "a", "a", "a", "a", "a" }));
+                }
+
+                [Test]
                 public void StrongNumberTest()
                 {
                     Assert.AreEqual("STRONG!!!!", StrongNumberTask.StrongNumber(1));
