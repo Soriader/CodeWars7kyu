071ce887e450ed71ba9534b5268d744be51c44be
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-09 11:23:51 +0200

    [~] Task: Reverse the bits in an integer

---
+++ CodeWars7kyu/ReverseTheBitsTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ReverseTheBitsTask
+    {
+        public static long ReverseBits(long n)
+        {
+            string binaryString = Convert.ToString(n, 2);
+
+            char[] binaryArray = binaryString.ToCharArray();
+
+            Array.Reverse(binaryArray);
+
+            string reversedBinaryString = new string(binaryArray);
+
+            long reversedNumber = Convert.ToInt64(reversedBinaryString, 2);
+
+            return reversedNumber;
+        }
+    }
+}
+//https://www.codewars.com/kata/5959ec605595565f5c00002b/train/csharp
