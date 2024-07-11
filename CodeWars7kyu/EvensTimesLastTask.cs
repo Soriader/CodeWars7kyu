4268676be821e840dfb7e50f788e1cfefdde1a53
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-11 13:18:15 +0200

    [~]Task: Evens times last

---
+++ CodeWars7kyu/EvensTimesLastTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class EvensTimesLastTask
+    {
+        public static int EvenTimesLast(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return 0;
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += arr[i];
+                }
+            }
+
+            return sum * arr[^1];
+        }
+    }
+}
+//https://www.codewars.com/kata/5a1a9e5032b8b98477000004/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,18 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void EvensTimesLastTest()
+                {
+                    Assert.AreEqual(30, EvensTimesLastTask.EvenTimesLast(new int[] { 2, 3, 4, 5 }));
+                    Assert.AreEqual(0, EvensTimesLastTask.EvenTimesLast(new int[] { 2, 3, 4, 5, 0 }));
+                    Assert.AreEqual(-6, EvensTimesLastTask.EvenTimesLast(new int[] { 2, 3, 4, -1 }));
+                    Assert.AreEqual(91, EvensTimesLastTask.EvenTimesLast(new[] { 7, 13 }));
+                    Assert.AreEqual(49, EvensTimesLastTask.EvenTimesLast(new[] { 7 }));
+
+
+                }
+
+                [Test]
                 public void BandNameGeneratorTest()
                 {
                     Assert.AreEqual("The Knife", BandNameGeneratorTask.BandNameGenerator("knife"));
