b0e2af31d591314e6615a59b255bf767d62bfe44
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-15 19:44:55 +0200

    [~] Task: Row Weights

---
+++ CodeWars7kyu/RowWeightsTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class RowWeightsTask
+    {
+        public static int[] RowWeights(int[] a)
+        {
+            List<int> result = new List<int>();
+            int firstGroup = 0;
+            int secondGroup = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    firstGroup += a[i];
+                }
+                else
+                {
+                    secondGroup += a[i];
+                }
+            }
+
+            result.Add(firstGroup);
+            result.Add(secondGroup);
+
+            return result.ToArray();
+        }
+
+    }
+}
+//https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void RowWeightsTest()
+                {
+                    Assert.AreEqual(new int[] {0,0}, RowWeightsTask.RowWeights(new int[] {0}));
+                    Assert.AreEqual(new int[] { 0, 1 }, RowWeightsTask.RowWeights(new int[] {0, 1, 0}));
+                    Assert.AreEqual(new int[] { 62, 27 }, RowWeightsTask.RowWeights(new int[] { 13, 27,49 }));
+                    Assert.AreEqual(new int[] { 150, 151 }, RowWeightsTask.RowWeights(new int[] { 100, 51, 50, 100 }));
+                }
+
+                [Test]
                 public static void ReturnStringOfFirstCharactersTest()
                 {
                     Assert.AreEqual("cia", ReturnStringOfFirstCharactersTask.MakeString("coding is awesome"));
