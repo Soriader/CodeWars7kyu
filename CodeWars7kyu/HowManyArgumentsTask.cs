e8aaa0ed7e90411a004f799ce3dfc90c8007f803
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-15 19:50:24 +0200

    [~] Task: How many arguments

---
+++ CodeWars7kyu/HowManyArgumentsTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class HowManyArgumentsTask
+    {
+
+        public static int CountArgs(params object[] args)
+        {
+            //return args.Length;
+            return args.Count();
+        }
+    }
+}
+//https://www.codewars.com/kata/5c44b0b200ce187106452139/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,16 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void HowManyArgumentsTest()
+                {
+                    Assert.AreEqual(3, HowManyArgumentsTask.CountArgs(1, 2, 3));
+                    Assert.AreEqual(3, HowManyArgumentsTask.CountArgs(1, 2, "uhsaf uas"));
+                    Assert.AreEqual(1, HowManyArgumentsTask.CountArgs(1));
+                    Assert.AreEqual(4, HowManyArgumentsTask.CountArgs('a', 865, "asfhgajsf", new object[] { "dawdjio", null, new List<object>() }));
+                    Assert.AreEqual(0, HowManyArgumentsTask.CountArgs());
+                }
+
+                [Test]
                 public void RowWeightsTest()
                 {
                     Assert.AreEqual(new int[] {0,0}, RowWeightsTask.RowWeights(new int[] {0}));
