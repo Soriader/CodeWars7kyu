29ab990a0641b2548d2111c8757b1459b23c07ab
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-17 19:21:49 +0200

    [~] Task: Simple remove duplicates

---
+++ CodeWars7kyu/SimpleRemoveDuplicatesTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SimpleRemoveDuplicatesTask
+    {
+        public static int[] solve(int[] arr)
+        {
+            Dictionary<int, bool> seen = new Dictionary<int, bool>();
+            List<int> result = new List<int>();
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (!seen.ContainsKey(arr[i]))
+                {
+                    result.Add(arr[i]);
+                    seen[arr[i]] = true;
+                }
+            }
+
+            result.Reverse();
+
+            return result.ToArray();
+        }
+    }
+}
+//https://www.codewars.com/kata/5ba38ba180824a86850000f7/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,15 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void SimpleRemoveDuplicatesTest()
+                {
+                    Assert.AreEqual(new int[] { 4, 6, 3 }, SimpleRemoveDuplicatesTask.solve(new int[] { 3, 4, 4, 3, 6, 3 }));
+                    Assert.AreEqual(new int[] { 1, 2, 3 }, SimpleRemoveDuplicatesTask.solve(new int[] { 1, 2, 1, 2, 1, 2, 3 }));
+                    Assert.AreEqual(new int[] { 1, 2, 3, 4 }, SimpleRemoveDuplicatesTask.solve(new int[] { 1, 2, 3, 4 }));
+                    Assert.AreEqual(new int[] { 4, 5, 2, 1 }, SimpleRemoveDuplicatesTask.solve(new int[] { 1, 1, 4, 5, 1, 2, 1 }));
+                }
+
+                [Test]
                 public void BasicMathTest()
                 {
                     Test("10", "1plus2plus3plus4");
