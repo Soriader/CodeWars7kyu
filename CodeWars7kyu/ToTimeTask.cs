ab8beb2c7b50ff91cb0c9221efd635773081d724
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-18 20:24:45 +0200

    [~] Task: All Star Code Challenge #22

---
+++ CodeWars7kyu/ToTimeTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class ToTimeTask
+    {
+        public static string ToTime(int seconds)
+        {
+            int hour = 0;
+            int minute = 0;
+
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            hour = (int)time.TotalHours;
+            minute = (int)(time.TotalMinutes - hour*60);
+
+
+            return $"{hour} hour(s) and {minute} minute(s)";
+        }
+    }
+}
+//https://www.codewars.com/kata/5865cff66b5699883f0001aa/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,23 @@ namespace CodeWarsTest
 			public class Tests
             {
                 [Test]
+                public void ToTimeTest()
+                {
+                    Act(3600, "1 hour(s) and 0 minute(s)");
+                    Act(3601, "1 hour(s) and 0 minute(s)");
+                    Act(3500, "0 hour(s) and 58 minute(s)");
+                    Act(323500, "89 hour(s) and 51 minute(s)");
+                    Act(0, "0 hour(s) and 0 minute(s)");
+
+                    static void Act(int seconds, string expected)
+                    {
+                        var actual = ToTimeTask.ToTime(seconds);
+                        Console.WriteLine($"   Input: seconds = {seconds}\nExpected: {expected}\n  Actual: {actual}\n");
+                        Assert.AreEqual(expected, actual, $"Input: seconds = {seconds}");
+                    }
+                }
+
+                [Test]
                 public void HowManyConsecutiveTest()
                 {
                     Assert.AreEqual(2, HowManyConsecutiveTask.Consecutive(new int[] { 4, 8, 6 }));
