9ab943dcb4a08c37172bb09ad021afc9e4f54848
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-19 19:14:11 +0200

    [~] Task: Formatting decimal places #1

---
+++ CodeWars7kyu/FormattingDecimalPlacesTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FormattingDecimalPlacesTask
+	{
+		public static double TwoDecimalPlaces(double number)
+		{
+			string str = number.ToString("F15", System.Globalization.CultureInfo.InvariantCulture);
+
+			int decimalPointIndex = str.IndexOf('.');
+
+			if (decimalPointIndex != -1)
+			{
+				str = str.Substring(0, decimalPointIndex + 3);
+			}
+
+			return double.Parse(str, System.Globalization.CultureInfo.InvariantCulture);
+		}
+	}
+}
+//https://www.codewars.com/kata/5641c3f809bf31f008000042/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,7 +15,15 @@ namespace CodeWarsTest
         {
 			public class Tests
             {
-                [Test]
+				[Test]
+				public void FormattingDecimalPlacesTest()
+				{
+					Assert.AreEqual(10.12, FormattingDecimalPlacesTask.TwoDecimalPlaces(10.1289767789));
+					Assert.AreEqual(-7488.83, FormattingDecimalPlacesTask.TwoDecimalPlaces(-7488.83485834983));
+
+				}
+
+				[Test]
                 public void SampleTests()
                 {
                     TestToValidSpacingTask(true, "Hello world");
