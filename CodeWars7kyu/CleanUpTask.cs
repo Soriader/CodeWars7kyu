d59a8146f50c5fc915da90e764d3de5f32df40e0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-21 13:26:26 +0200

    [~] Task: Clean up after your dog

---
+++ CodeWars7kyu/CleanUpTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CleanUpTask
+	{
+		public static string Crap(char[,] x, int bags, int cap)
+		{
+			int trash = 0;
+			bool dogIsHere = false;
+
+			for (int i = 0; i < x.GetLength(0); i++) 
+			{
+				for (int j = 0; j < x.GetLength(1); j++) 
+				{
+					if (x[i, j] == '@')
+					{
+						trash++;
+					}
+
+					if (x[i, j] == 'D')
+					{
+						dogIsHere = true;
+					}
+				}
+			}
+
+				if (dogIsHere) 
+			{
+				return "Dog!!";
+			}
+
+			if(trash <= bags * cap)
+			{
+				return "Clean";
+			}
+
+			return "Cr@p";
+		}
+	}
+}
+//https://www.codewars.com/kata/57faa6ff9610ce181b000028/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
 			public class Tests
             {
 				[Test]
+				public void CleanUpTest()
+				{
+					Assert.AreEqual("Clean", CleanUpTask.Crap(new char[,] { { '_', '_', '_', '_' }, { '_', '_', '_', '@' }, { '_', '_', '@', '_' } }, 2, 2));
+					Assert.AreEqual("Cr@p", CleanUpTask.Crap(new char[,] { { '_', '_', '_', '_' }, { '_', '_', '_', '@' }, { '_', '_', '@', '_' } }, 1, 1));
+					Assert.AreEqual("Dog!!", CleanUpTask.Crap(new char[,] { { '_', '_' }, { '_', '@' }, { 'D', '_' } }, 2, 2));
+				}
+
+				[Test]
 				public void FormattingDecimalPlacesTest()
 				{
 					Assert.AreEqual(10.12, FormattingDecimalPlacesTask.TwoDecimalPlaces(10.1289767789));
