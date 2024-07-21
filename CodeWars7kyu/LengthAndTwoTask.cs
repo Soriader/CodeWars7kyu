7f7de804d1f508b07883095cb660318e9d5c2792
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-21 14:08:27 +0200

    [~] Task: Length and two values.

---
+++ CodeWars7kyu/LengthAndTwoTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class LengthAndTwoTask
+	{
+		public static object[] Alternate(int n, object firstValue, object secondValue)
+		{
+			List<object> result = new List<object>();
+
+			for (int i = 0; i < n; i++)
+			{
+				if (i % 2 == 0)
+				{
+					result.Add(firstValue);
+				}
+				else
+				{
+					result.Add(secondValue);
+				}
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/62a611067274990047f431a8/train/csharp
