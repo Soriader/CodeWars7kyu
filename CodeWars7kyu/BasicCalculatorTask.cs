e5bc25be5b7af750c01d54d828b49572668884a5
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-23 22:37:24 +0200

    [~] Task: Basic Calculator

---
+++ CodeWars7kyu/BasicCalculatorTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class BasicCalculatorTask
+	{
+		public static double Execute(double num1, char op, double num2)
+		{
+
+			switch (op)
+			{
+				case '+':
+					return num1 + num2;
+				case '-':
+					return num1 - num2;
+				case '*':
+					return num1 * num2;
+				case '/':
+					if (num2 == 0)
+					{
+						throw new ArgumentException();
+					}
+					return num1 / num2;
+				default:
+					throw new ArgumentException();
+			}
+
+			throw new ArgumentException();
+		}
+	}
+}
+//https://www.codewars.com/kata/5296455e4fe0cdf2e000059f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,17 @@ namespace CodeWarsTest
 			public class Tests
             {
 				[Test]
+				public void BasicCalculatorTest()
+				{
+					Assert.AreEqual(-4.8, BasicCalculatorTask.Execute(3.2, '-', 8));
+					Assert.AreEqual(11.2, BasicCalculatorTask.Execute(3.2, '+', 8));
+					Assert.AreEqual(0.4, BasicCalculatorTask.Execute(3.2, '/', 8));
+					Assert.AreEqual(25.6, BasicCalculatorTask.Execute(3.2, '*', 8));
+					Assert.Throws<ArgumentException>(() => BasicCalculatorTask.Execute(-3, 'w', 1));
+				}
+
+
+				[Test]
 				public void ArrayLeadersTest()
                 {
 					Assert.AreEqual(new int[] { 4 }, ArrayLeadersTask.ArrayLeaders(new int[] { 1, 2, 3, 4, 0 }));
