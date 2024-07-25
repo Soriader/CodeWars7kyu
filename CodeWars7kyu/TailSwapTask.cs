0452564b730bb255d99961b29efd0247072bf7cb
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-25 12:47:35 +0200

    [~] Task: Tail Swap

---
+++ CodeWars7kyu/TailSwapTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TailSwapTask
+	{
+		public static string[] TailSwap(string[] arr)
+		{
+			List<string> result = new List<string>();
+
+			string a = arr[0]; 
+			string b = arr[1]; 
+
+			var checkA = a.ToCharArray();
+			var checkB = b.ToCharArray();
+
+			var whereIsColonA = WhereIsColon(checkA);
+			var whereIsColonB = WhereIsColon(checkB);
+
+			string afterColonA = "";
+			string afterColonB = "";
+
+			for (int i = whereIsColonA + 1; i < a.Length; i++)
+			{
+				afterColonA += a[i];
+			}
+
+			for (int i = whereIsColonB + 1; i < b.Length; i++)
+			{
+				afterColonB+= b[i];
+			}
+
+			string resultA = "";
+			string resultB = "";
+
+			for (int i = 0; i < a.Length; i++)
+			{
+				if(i > whereIsColonA)
+				{
+					break;
+				}
+
+				resultA += a[i];
+			}
+
+			for (int i = 0; i < b.Length; i++)
+			{
+				if (i > whereIsColonB)
+				{
+					break;
+				}
+
+				resultB += b[i];
+			}
+
+			result.Add(resultA + afterColonB);
+			result.Add(resultB + afterColonA);
+
+			return result.ToArray();
+
+		}
+
+		public static int WhereIsColon(char[] x)
+		{
+			int colon = 0;
+
+			for (int i = 0; i < x.Length; i++)
+			{
+				if (x[i] == ':')
+				{
+					colon = i;
+				}
+			}
+
+			return colon;
+		}
+	}
+}
+//https://www.codewars.com/kata/5868812b15f0057e05000001/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,8 +16,18 @@ namespace CodeWarsTest
             public class Tests
             {
 
+				[Test]
+				public void TailSwapTest()
+				{
+					Assert.AreEqual(new[] { "a:d", "c:b" }, TailSwapTask.TailSwap(new[] { "a:b", "c:d" }));
+					Assert.AreEqual(new[] { "1:4", "3:2" }, TailSwapTask.TailSwap(new[] { "1:2", "3:4" }));
+					Assert.AreEqual(new[] { "abc:456", "cde:123" }, TailSwapTask.TailSwap(new[] { "abc:123", "cde:456" }));
+					Assert.AreEqual(new[] { "a:xyz", "777:12345" }, TailSwapTask.TailSwap(new[] { "a:12345", "777:xyz" }));
+					Assert.AreEqual(new[] { "(:]", "[:)" }, TailSwapTask.TailSwap(new[] { "(:)", "[:]" }));
+					Assert.AreEqual(new[] { ",:,", ",:;" }, TailSwapTask.TailSwap(new[] { ",:;", ",:," }));
+				}
 
-                [Test]
+				[Test]
                 public void ChangingLettersTest()
                 {
 					Assert.AreEqual("HEllOWOrld!", ChangingLettersTask.Swap("HelloWorld!"));
