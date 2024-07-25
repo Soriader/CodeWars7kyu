1a7c91bcac0390f84e416ecc1fa4135674222400
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-25 13:20:46 +0200

    [~] Task: Plus - minus - plus - plus - ... - Count

---
+++ CodeWars7kyu/PlusMinusCountTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PlusMinusCountTask
+	{
+		public static int CatchSignChange(int[] arr)
+		{
+			var result = 0;
+			for (var i = 0; i < arr.Length - 1; i++)
+			{
+				if ((arr[i] >= 0 && arr[i + 1] < 0) || (arr[i] < 0 && arr[i + 1] >= 0))
+				{
+					result++;
+				}
+			}
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/5bbb8887484fcd36fb0020ca/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -15,6 +15,17 @@ namespace CodeWarsTest
         {
             public class Tests
             {
+				[Test]
+				public void PlusMinusCountTest()
+				{
+					Assert.AreEqual(1, PlusMinusCountTask.CatchSignChange(new int[] { 2, 6, 3, 0, 5, -3 }));
+					Assert.AreEqual(1, PlusMinusCountTask.CatchSignChange(new int[] { -2, -2, -5, -4, 5, 2, 0, 6, 0 }));
+					Assert.AreEqual(2, PlusMinusCountTask.CatchSignChange(new int[] { 3, 7, -6, 2, 3, 1, 1 }));
+                    Assert.AreEqual(2, PlusMinusCountTask.CatchSignChange(new int[] { -1, 2, 2, 0, 2, -8, -1 }));
+					Assert.AreEqual(3, PlusMinusCountTask.CatchSignChange(new int[] { -47, 84, -30, -11, -5, 74, 77 }));
+                    Assert.AreEqual(4, PlusMinusCountTask.CatchSignChange(new int[] { -8, 4, -1, 5, -3, -3, -2, -2 }));
+
+				}
 
 				[Test]
 				public void TailSwapTest()
