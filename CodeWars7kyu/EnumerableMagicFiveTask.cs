53bf132ac45624d7609046df883d6b82ece43d4d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-26 14:34:46 +0200

    [~] Task: Enumerable Magic #5- True for Just One?

---
+++ CodeWars7kyu/EnumerableMagicFiveTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class EnumerableMagicFiveTask
+	{
+		public static bool One(int[] arr, Func<int, bool> fun)
+		{
+			int iterator = 0; 
+			foreach (int item in arr)
+			{
+				if (fun(item))
+				{
+					iterator++;
+				}
+			}
+
+			if (iterator == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/54599705cbae2aa60b0011a4/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void EnumerableMagicFiveTest()
+				{
+					Assert.AreEqual(true, EnumerableMagicFiveTask.One(new int[] { 1, 2, 3, 4, 5 }, v => v < 2));
+					Assert.AreEqual(false, EnumerableMagicFiveTask.One(new int[] { 1, 2, 3, 4, 5 }, v => v % 2 != 0));
+					Assert.AreEqual(false, EnumerableMagicFiveTask.One(new int[] { 1, 2, 3, 4, 5 }, v => v > 5));
+				}
+
+				[Test]
 				public void ProductOfMaximumsOfArrayTest()
 				{
 					Assert.AreEqual(20, ProductOfMaximumsOfArrayTask.MaxProduct(new int[] { 4, 3, 5 }, 2));
