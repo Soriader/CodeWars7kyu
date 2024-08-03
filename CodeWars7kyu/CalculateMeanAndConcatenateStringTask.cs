c3f9e022a296785d8f640b3f66027411c168cbee
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-03 15:53:41 +0200

    [~] Task: Calculate mean and concatenate string

---
+++ CodeWars7kyu/CalculateMeanAndConcatenateStringTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CalculateMeanAndConcatenateStringTask
+	{
+		public static object[] Mean(char[] lst)
+		{
+			double digit = 0;
+			string letters = "";
+
+			object[] result = new object[2];
+
+			for (int i = 0; i < lst.Length; i++)
+			{
+				if (char.IsDigit(lst[i]))
+				{
+					digit += lst[i] - '0';
+				}
+
+				if (char.IsLetter(lst[i]))
+				{
+					letters += lst[i];
+				}
+			}
+
+			result[0] = digit / 10;
+			result[1] = letters;
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/56f7493f5d7c12d1690000b6/train/csharp
