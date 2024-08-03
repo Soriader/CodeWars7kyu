aab8053cf80510daef8e9702613b7cf59a1065e0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-03 15:15:14 +0200

    [~] Task: Partial Word Searching

---
+++ CodeWars7kyu/PartialWordSearchingTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PartialWordSearchingTask
+	{
+		public static string[] WordSearch(string query, string[] seq)
+		{
+			query = query.ToLower();
+			List<string> result = new List<string>();
+
+			foreach (var word in seq)
+			{
+				if (word.ToLower().Contains(query))
+				{
+					result.Add(word);
+				}
+			}
+
+			if (result.Count == 0)
+			{
+				return new string[] { "Empty" };
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/54b81566cd7f51408300022d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -17,7 +17,16 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
-                [Test]
+				[Test]
+				public void PartialWordSearchingTest()
+				{
+					Assert.AreEqual(new string[] { "ab", "abc", "zab" }, PartialWordSearchingTask.WordSearch("ab", new string[] { "za", "ab", "abc", "zab", "zbc" }));
+					Assert.AreEqual(new string[] { "ab", "abc", "zab" }, PartialWordSearchingTask.WordSearch("aB", new string[] { "za", "ab", "abc", "zab", "zbc" }));
+					Assert.AreEqual(new string[] { "aB", "Abc", "zAB" }, PartialWordSearchingTask.WordSearch("ab", new string[] { "za", "aB", "Abc", "zAB", "zbc" }));
+					Assert.AreEqual(new string[] { "Empty" }, PartialWordSearchingTask.WordSearch("abcd", new string[] { "za", "aB", "Abc", "zAB", "zbc" }));
+				}
+
+				[Test]
                 public void SearchForLettersTest() 
                 {
 					Assert.AreEqual("11000000000000000000000001", SearchForLettersTask.Change("a **&  bZ"));
