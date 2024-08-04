8b1b8f921e45ab8e93a6b35aff993ae893de1a91
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-04 16:20:46 +0200

    [~] Task: Hells Kitchen

---
+++ CodeWars7kyu/HellsKitchenTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class HellsKitchenTask
+	{
+		public static string Gordon(string a)
+		{
+			var check = a.ToUpper().ToCharArray();
+			var vowels = new char[] { 'O', 'I', 'E', 'U', };
+			string result = "";
+
+			for (int i = 0; i < check.Length; i++)
+			{
+				if (char.IsWhiteSpace(check[i]))
+				{
+					result += "!!!!";
+				}
+
+				if (Array.Exists(vowels, element => element == check[i]))
+				{
+					result += '*';
+				}
+				else if (check[i] == 'A')
+				{
+					result += '@';
+				}
+				else
+				{
+					result += check[i];
+
+				}
+
+
+			}
+
+			return result + "!!!!";
+		}
+
+		
+	}
+}
+//https://www.codewars.com/kata/57d1f36705c186d018000813/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -18,6 +18,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void HellsKitchenTest()
+				{
+					Assert.AreEqual("WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!", HellsKitchenTask.Gordon("What feck damn cake"));
+					Assert.AreEqual("@R*!!!! Y**!!!! ST*!!!! P*D!!!!", HellsKitchenTask.Gordon("are you stu pid"));
+					Assert.AreEqual("*!!!! @M!!!! @!!!! CH*F!!!!", HellsKitchenTask.Gordon("i am a chef"));
+				}
+
+				[Test]
 				public static void NthRootOfANumberTest()
 				{
 					double actual1 = NthRootOfANumberTask.FindRoot(4, 2);
