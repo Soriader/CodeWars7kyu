c6cdd36b5b428b43fbdb6e4536392b835e4ee5f7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-04 15:05:43 +0200

    [~] Task: Nth Root of a Number

---
+++ CodeWars7kyu/NthRootOfANumberTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class NthRootOfANumberTask
+	{
+		public static double FindRoot(double x, int n)
+		{
+			return Math.Pow(x, 1.0 / n);
+		}
+	}
+}
+//https://www.codewars.com/kata/5520714decb43308ea000083/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -18,6 +18,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void NthRootOfANumberTest()
+				{
+					double actual1 = NthRootOfANumberTask.FindRoot(4, 2);
+					double actual2 = NthRootOfANumberTask.FindRoot(8, 3);
+					Assert.AreEqual(2, actual2, 1e-9);
+					Assert.AreEqual(2, actual1, 1e-9);
+				}
+
+				[Test]
 				public void EvenOrOddWhichIsGreaterTest()
 				{
 					Assert.AreEqual("Odd is greater than Even", EvenOrOddWhichIsGreaterTask.EvenOrOdd("123"));
