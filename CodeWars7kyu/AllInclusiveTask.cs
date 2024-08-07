3c6a94a95040d68aaddb133b1bb6f552f8db21a7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-07 19:21:14 +0200

    [~] Task: All Inclusive?

---
+++ CodeWars7kyu/AllInclusiveTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class AllInclusiveTask
+	{
+		public static Boolean ContainAllRots(string strng, List<string> arr)
+		{
+			if (strng == null)
+			{
+				return true;
+			}
+
+			List<string> rotations = new List<string>();
+			int length = strng.Length;
+			int iterator = 0;
+
+			for (int i = 0; i < length; i++)
+			{
+				string rotation = strng.Substring(i) + strng.Substring(0, i);
+				rotations.Add(rotation);
+			}
+
+			var check = rotations.Count();
+
+			foreach (string word in rotations) 
+			{
+				if (arr.Contains(word))
+				{
+					iterator++;
+				}
+			}
+
+			if( iterator == check) 
+			{
+				return true;
+			}
+
+			return false;
+
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5700c9acc1555755be00027e/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,24 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void AllInclusiveTest()
+				{
+					List<string> a = new List<string>() { "bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs" };
+					testing(AllInclusiveTask.ContainAllRots("bsjq", a), true);
+					a = new List<string>() { };
+					testing(AllInclusiveTask.ContainAllRots("", a), true);
+					a = new List<string>() { "bsjq", "qbsj" };
+					testing(AllInclusiveTask.ContainAllRots("", a), true);
+					a = new List<string>() { "TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY" };
+					testing(AllInclusiveTask.ContainAllRots("XjYABhR", a), false);
+
+					static void testing(bool actual, bool expected)
+					{
+						Assert.AreEqual(expected, actual);
+					}
+				}
+
+				[Test]
 				public void JumpingNumberTest()
 				{
 					Assert.That(JumpingNumberTask.JumpingNumber(00000079), Is.EqualTo("Not!!"));
