22ee3fe7fa7a94bf773c9bf37bc47273c079eb66
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-08 19:32:37 +0200

    [~] Task: Previous multiple of three

---
+++ CodeWars7kyu/PreviousMultipleOfThreeTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PreviousMultipleOfThreeTask
+	{
+		public static int? PreviousMultipleOfThree(int n)
+		{
+			if (n % 3 == 0)
+			{
+				return n;
+			}
+
+			while (n > 0)
+			{
+				n /= 10;
+
+				if (n % 3 == 0 && n > 0)
+				{
+					return n;
+				}
+			}
+
+			return null;
+
+		}
+		
+	}
+}
+//https://www.codewars.com/kata/61123a6f2446320021db987d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void PreviousMultipleOfThreeTest() 
+                { 
+					Assert.AreEqual(null, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(1));
+					Assert.AreEqual(null, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(25));
+					Assert.AreEqual(36, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(36));
+					Assert.AreEqual(12, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(1244));
+					Assert.AreEqual(9, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(952406));
+				}
+
+				[Test]
                 public static void ProductArrayTest() 
                 {
 					Assert.AreEqual(new int[] { 20, 12 }, ProductArrayTask.ProductArray(new int[] { 12, 20}));
