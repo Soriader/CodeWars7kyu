46cec1ce1d351244cb03c8d30ddf3df657fab8a0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-09 14:51:06 +0200

    [~] Task: Nice Array

---
+++ CodeWars7kyu/NiceArrayTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class NiceArrayTask
+	{
+		public static bool IsNice(int[] arr)
+		{
+			if (arr.Length == 0)
+			{
+				return false;
+			}
+
+			foreach (var num in arr)
+			{
+				bool hasNeighbor = false;
+
+				foreach (var otherNum in arr)
+				{
+					if (num + 1 == otherNum || num - 1 == otherNum)
+					{
+						hasNeighbor = true;
+						break;
+					}
+				}
+
+				if (!hasNeighbor)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
+//https://www.codewars.com/kata/59b844528bcb7735560000a0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SampleTest()
+				{
+					Assert.AreEqual(true, NiceArrayTask.IsNice(new int[] { 2, 10, 9, 3 }));
+					Assert.AreEqual(false, NiceArrayTask.IsNice(new int[] { 3, 4, 5, 7 }));
+				}
+
+				[Test]
 				public static void PreviousMultipleOfThreeTest() 
                 { 
 					Assert.AreEqual(null, PreviousMultipleOfThreeTask.PreviousMultipleOfThree(1));
