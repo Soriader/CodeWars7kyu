801e87caec9dd65d66ba31d0a944113f1e02a154
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-13 11:49:39 +0200

    [~] Task: last digits of a number

---
+++ CodeWars7kyu/lastDigitsOfANumberTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class lastDigitsOfANumberTask
+	{
+		public static int[] LastDigit(long n, int d)
+		{
+			var digits = n.ToString().ToCharArray();
+
+			if (d <= 0)
+			{
+				return new int[] { };
+			}
+
+			if (d > digits.Length)
+			{
+				d = digits.Length;
+			}
+
+			List<int> result = new List<int>();
+
+			for (int i = digits.Length - d; i < digits.Length; i++)
+			{
+				result.Add(digits[i] - '0');
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/5cd5ba1ce4471a00256930c0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void lastDigitsOfANumberTest()
+				{
+					Assert.AreEqual(new[] { 1 }, lastDigitsOfANumberTask.LastDigit(1, 1));
+					Assert.AreEqual(new[] { 3, 7, 6, 7 }, lastDigitsOfANumberTask.LastDigit(123767, 4));
+					Assert.AreEqual(new[] { 0 }, lastDigitsOfANumberTask.LastDigit(0, 1));
+					Assert.AreEqual(new[] { 5, 6, 4, 7, 8, 6, 7, 5, 8, 5 }, lastDigitsOfANumberTask.LastDigit(34625647867585, 10));
+					Assert.AreEqual(new int[] { }, lastDigitsOfANumberTask.LastDigit(1234, 0));
+					Assert.AreEqual(new int[] { }, lastDigitsOfANumberTask.LastDigit(24134, -4));
+					Assert.AreEqual(new[] { 1, 3, 4, 3 }, lastDigitsOfANumberTask.LastDigit(1343, 5));
+				}
+
+				[Test]
 				public void FindTheLuckyNumbersTest()
 				{
 					Assert.AreEqual(new int[] { 7, 70, 17 }, FindTheLuckyNumbersTask.FilterLucky(new int[] { 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }));
