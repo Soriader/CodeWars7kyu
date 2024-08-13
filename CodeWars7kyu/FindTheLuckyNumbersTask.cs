3de5cfd26fed923efa1b8ee29f93d2f754d3ed0b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-13 11:24:38 +0200

    [~] Task: Find the lucky numbers

---
+++ CodeWars7kyu/FindTheLuckyNumbersTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FindTheLuckyNumbersTask
+	{
+		public static int[] FilterLucky(int[] x)
+		{
+			List<int> result = new List<int>();
+
+
+			foreach (var item in x)
+			{
+				if (item.ToString().ToCharArray().Contains('7'))
+				{
+					result.Add(item);
+				}
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/580435ab150cca22650001fb/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FindTheLuckyNumbersTest()
+				{
+					Assert.AreEqual(new int[] { 7, 70, 17 }, FindTheLuckyNumbersTask.FilterLucky(new int[] { 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }));
+					Assert.AreEqual(new int[] { }, FindTheLuckyNumbersTask.FilterLucky(new int[] { }));
+				}
+
+				[Test]
 				public void UnscrambledEggsTest()
 				{
 					Assert.AreEqual("code here", UnscrambledEggsTask.UnscrambleEggs("ceggodegge heggeregge"));
