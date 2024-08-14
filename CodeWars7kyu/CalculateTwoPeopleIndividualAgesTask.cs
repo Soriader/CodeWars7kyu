090e652a328692e78ad1751563fb7ec1a97dd11c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-14 15:37:53 +0200

    [~] Task: Calculate Two People's Individual Ages

---
+++ CodeWars7kyu/CalculateTwoPeopleIndividualAgesTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CalculateTwoPeopleIndividualAgesTask
+	{
+		public static double[] GetAges(int sum, int difference)
+		{
+			if(sum < 0 || difference < 0)
+			{
+				return null;
+			}
+
+			double oldest = (double)(sum /2.0 + difference/2.0);
+			double youngest = (double)(sum/2.0 - difference/2.0);
+
+			if (oldest >= 0 && youngest >= 0)
+			{
+				return new double[] { oldest, youngest };
+			}
+
+			return null;
+		}
+	}
+}
+//https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void CalculateTwoPeopleIndividualAgesTest()
+				{
+					Assert.AreEqual(new double[] { 14, 10 }, CalculateTwoPeopleIndividualAgesTask.GetAges(24, 4), "it returns {14, 10} when passed 24, 4");
+					Assert.AreEqual(new double[] { 38.5, 24.5 }, CalculateTwoPeopleIndividualAgesTask.GetAges(63, 14), "it returns {38.5, 24.5} when passed 63, 14");
+					Assert.AreEqual(null, CalculateTwoPeopleIndividualAgesTask.GetAges(63, -14), "it returns null when passed 63, -14");
+				}
+
+				[Test]
 				public void CorrectTheTimeStringTest()
 				{
 					Assert.AreEqual("09:10:01", CorrectTheTimeStringTask.Correct("09:10:01"));
