6ccb56ae1cea97f9e31725762f6dc8ae0e6e6dfd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-15 18:16:31 +0200

    [~] Task: The Office III - Broken Photocopier

---
+++ CodeWars7kyu/BrokenPhotocopierTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class BrokenPhotocopierTask
+	{
+		public static string Broken(string x)
+		{
+			var check = x.ToCharArray();
+			string result = "";
+
+			foreach (char c in check) 
+			{
+				if (c == '1')
+				{
+					result += c.ToString().Replace("1", "0");
+				}
+				else if (c == '0')
+				{
+					result += c.ToString().Replace("0", "1");
+
+				}
+			}
+
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/57ed56657b45ef922300002b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void BrokenPhotocopierTest()
+				{
+					Assert.AreEqual("0", BrokenPhotocopierTask.Broken("1"));
+					Assert.AreEqual("01111111010010000001100110111", BrokenPhotocopierTask.Broken("10000000101101111110011001000"));
+					Assert.AreEqual("011101", BrokenPhotocopierTask.Broken("100010"));
+				}
+
+				[Test]
 				public void AreaOfACircleTest()
 				{
 					Assert.AreEqual(3.141, AreaOfACircleTask.CalculateAreaOfCircle(1), 0.01);
