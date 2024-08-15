0ad5909cb425e7301c0b12045ee4e32938501b24
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-15 18:53:06 +0200

    [~] Task: Maximum Gap (Array Series #4)

---
+++ CodeWars7kyu/MaximumGapTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MaximumGapTask
+	{
+		public static int MaxGap(int[] numbers)
+		{
+			var check = numbers.OrderBy(x => x).ToList();
+
+			int maxGap = 0;
+			int checkTheGap = 0;
+
+			for (int i = 0; i < check.Count - 1; i++) 
+			{
+				checkTheGap = check[i + 1] - check[i];
+
+				if (checkTheGap > maxGap) 
+				{
+					maxGap = checkTheGap;
+				}
+
+			}
+
+			return maxGap;
+		}
+	}
+}
+//https://www.codewars.com/kata/5a7893ef0025e9eb50000013/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void MaximumGapTest()
+                {
+					Assert.AreEqual(4, MaximumGapTask.MaxGap(new int[] { 13, 10, 2, 9, 5 }));
+					Assert.AreEqual(8, MaximumGapTask.MaxGap(new int[] { 13, 3, 5 }));
+					Assert.AreEqual(168, MaximumGapTask.MaxGap(new int[] { 24, 299, 131, 14, 26, 25 }));
+					Assert.AreEqual(767, MaximumGapTask.MaxGap(new int[] { -7, -42, -809, -14, -12 }));
+					Assert.AreEqual(0, MaximumGapTask.MaxGap(new int[] { 1, 1, 1 }));
+					Assert.AreEqual(576, MaximumGapTask.MaxGap(new int[] { -54, 37, 0, 64, -15, 640, 0 }));
+
+				}
+
+				[Test]
 				public void SpeedControlTest()
 				{
 					Assert.AreEqual(41, SpeedControlTask.Gps(20, new double[] { 0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61 }));
