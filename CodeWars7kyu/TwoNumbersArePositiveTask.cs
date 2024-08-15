94cb266fbccc291f597cabb224c08fdf95845d0d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-15 19:04:22 +0200

    [~] Task: Two numbers are positive

---
+++ CodeWars7kyu/TwoNumbersArePositiveTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TwoNumbersArePositiveTask
+	{
+		public static bool TwoArePositive(int a, int b, int c)
+		{
+			if ((a > 0 && b > 0 && c <= 0) || (a <= 0 && b > 0 && c > 0) || (a > 0 && b <= 0 && c > 0))
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/602db3215c22df000e8544f0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void TwoNumbersArePositiveTest()
+				{
+					Assert.AreEqual(true, TwoNumbersArePositiveTask.TwoArePositive(2, 4, -3), "(2, 4, -3)");
+					Assert.AreEqual(true, TwoNumbersArePositiveTask.TwoArePositive(-4, 6, 8), "(-4, 6, 8)");
+					Assert.AreEqual(true, TwoNumbersArePositiveTask.TwoArePositive(4, -6, 9), "(4, -6, 9)");
+					Assert.AreEqual(true, TwoNumbersArePositiveTask.TwoArePositive(4, 6, 0), "(4, 6, 0)");
+					Assert.AreEqual(false, TwoNumbersArePositiveTask.TwoArePositive(-4, 6, 0), "(-4, 6, 0)");
+					Assert.AreEqual(false, TwoNumbersArePositiveTask.TwoArePositive(4, 6, 10), "(4, 6, 10)");
+					Assert.AreEqual(false, TwoNumbersArePositiveTask.TwoArePositive(-14, -3, -4), "(-14, -3, -4)");
+				}
+
+				[Test]
 				public void MaximumGapTest()
                 {
 					Assert.AreEqual(4, MaximumGapTask.MaxGap(new int[] { 13, 10, 2, 9, 5 }));
