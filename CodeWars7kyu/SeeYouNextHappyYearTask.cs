a1b9cbc7ba8cf969bbbe9bcc4d347aeb3925a263
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-16 19:01:32 +0200

    [~] Task: See You Next Happy Year

---
+++ CodeWars7kyu/SeeYouNextHappyYearTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class SeeYouNextHappyYearTask
+	{
+		public short NextHappyYear(short year)
+		{
+			while (true)
+			{
+				year++;
+				if (HasUniqueDigits(year))
+				{
+					return year;
+				}
+			}
+		}
+
+		private static bool HasUniqueDigits(int year)
+		{
+			string checkYear = year.ToString();
+
+			HashSet<char> digits = new HashSet<char>();
+
+			foreach (char digit in checkYear)
+			{
+				if (!digits.Add(digit)) 
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
+//https://www.codewars.com/kata/5ae7e3f068e6445bc8000046/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,22 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SeeYouNextHappyYearTest()
+				{
+					SeeYouNextHappyYearTask kata = new SeeYouNextHappyYearTask();
+
+					Assert.AreEqual(kata.NextHappyYear(1001), 1023);
+					Assert.AreEqual(kata.NextHappyYear(1123), 1203);
+					Assert.AreEqual(kata.NextHappyYear(2001), 2013);
+					Assert.AreEqual(kata.NextHappyYear(2334), 2340);
+					Assert.AreEqual(kata.NextHappyYear(3331), 3401);
+					Assert.AreEqual(kata.NextHappyYear(2342), 2345);
+					Assert.AreEqual(kata.NextHappyYear(1987), 2013);
+					Assert.AreEqual(kata.NextHappyYear(2013), 2014);
+					Assert.AreEqual(kata.NextHappyYear(3000), 3012);
+				}
+
+				[Test]
 				public void FixMyPhoneNumbersTest()
 				{
 					Assert.AreEqual("02078834982", FixMyPhoneNumbersTask.IsItANum("S:)0207ERGQREG88349F82!efRF)"));
