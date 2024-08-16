1073e9c73b8ead6c01d3b93ea527d6db4a382a16
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-16 18:29:48 +0200

    [~] Task: Fix My Phone Numbers!

---
+++ CodeWars7kyu/FixMyPhoneNumbersTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FixMyPhoneNumbersTask
+	{
+		public static String IsItANum(string str)
+		{
+			string number = "";
+
+			foreach (char c in str)
+			{
+				if (char.IsDigit(c))
+				{
+					number += c; 
+				}
+			}
+
+			if (number.StartsWith("0") && number.Length == 11)
+			{
+				return number;
+			}
+
+			return "Not a phone number";
+		}
+	}
+}
+//https://www.codewars.com/kata/596343a24489a8b2a00000a2/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FixMyPhoneNumbersTest()
+				{
+					Assert.AreEqual("02078834982", FixMyPhoneNumbersTask.IsItANum("S:)0207ERGQREG88349F82!efRF)"));
+					Assert.AreEqual("Not a phone number", FixMyPhoneNumbersTask.IsItANum("sjfniebienvr12312312312ehfWh"));
+					Assert.AreEqual("Not a phone number", FixMyPhoneNumbersTask.IsItANum("0192387415456"));
+					Assert.AreEqual("02084564165", FixMyPhoneNumbersTask.IsItANum("v   uf  f 0tt2eg qe0b 8rtyq4eyq564()(((((165"));
+					Assert.AreEqual("Not a phone number", FixMyPhoneNumbersTask.IsItANum("stop calling me no I have never been in an accident"));
+				}
+
+				[Test]
 				public void GhostbustersWhitespaceRemovalTest()
 				{
 					Assert.AreEqual("Factory", GhostbustersWhitespaceRemovalTask.GhostBusters("Factor y"), "Nope, there may still be a ghost in the building. Try again.");
