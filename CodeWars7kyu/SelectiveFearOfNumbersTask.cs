48da7207264902babdd7eea194b1a22e3a48b142
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-17 22:00:53 +0200

    [~] Task: Selective fear of numbers

---
+++ CodeWars7kyu/SelectiveFearOfNumbersTask.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class SelectiveFearOfNumbersTask
+	{
+		public static bool AmIAfraid(string day, int num)
+		{
+			switch (day) 
+			{
+				case "Monday":
+				{
+					if(num == 12)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				case "Tuesday":
+					{
+						if (num > 95)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				case "Wednesday":
+					{
+						if (num == 34)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				case "Thursday":
+					{
+						if (num == 0)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				case "Friday":
+					{
+						if (num % 2 == 0)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				case "Saturday":
+					{
+						if (num == 56)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				case "Sunday":
+					{
+						if (num == -666 || num == 666)
+						{
+							return true;
+						}
+						else
+						{
+							return false;
+						}
+					}
+			}
+
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/55b1fd84a24ad00b32000075/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,23 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void TestFixed()
+				{
+					void Tester(string day, int num, bool expected)
+					{
+						bool submitted = SelectiveFearOfNumbersTask.AmIAfraid(day, num);
+						string message = $"day = \"{day}\"\n  num = {num}";
+						Assert.AreEqual(expected, submitted, message);
+					}
+
+					Tester("Monday", 13, false);
+					Tester("Sunday", -666, true);
+					Tester("Tuesday", 2, false);
+					Tester("Tuesday", 965, true);
+					Tester("Friday", 2, true);
+				}
+
+				[Test]
 				public void SeeYouNextHappyYearTest()
 				{
 					SeeYouNextHappyYearTask kata = new SeeYouNextHappyYearTask();
