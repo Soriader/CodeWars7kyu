1cfc7e3a01530a5bd626a48119b95da11b69256b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-17 22:16:46 +0200

    [~] Task: Max-min arrays

---
+++ CodeWars7kyu/MaxMinArraysTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MaxMinArraysTask
+	{
+		public static List<int> Solve(List<int> arr)
+		{
+			List<int> workList = new List<int>(arr);
+			List<int> result = new List<int>();
+
+			while (workList.Count > 0)
+			{
+				int max = workList.Max();
+				result.Add(max);
+				workList.Remove(max);
+
+				if (workList.Count > 0)
+				{
+					int min = workList.Min();
+					result.Add(min);
+					workList.Remove(min);
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/5a090c4e697598d0b9000004/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -18,6 +18,18 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+
+				[Test]
+				public void MaxMinArraysTest()
+				{
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 15, 11, 10, 7, 12 }), Is.EqualTo(new List<int> { 15, 7, 12, 10, 11 }));
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 91, 75, 86, 14, 82 }), Is.EqualTo(new List<int> { 91, 14, 86, 75, 82 }));
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 84, 79, 76, 61, 78 }), Is.EqualTo(new List<int> { 84, 61, 79, 76, 78 }));
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 52, 77, 72, 44, 74, 76, 40 }), Is.EqualTo(new List<int> { 77, 40, 76, 44, 74, 52, 72 }));
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 1, 6, 9, 4, 3, 7, 8, 2 }), Is.EqualTo(new List<int> { 9, 1, 8, 2, 7, 3, 6, 4 }));
+					Assert.That(MaxMinArraysTask.Solve(new List<int> { 78, 79, 52, 87, 16, 74, 31, 63, 80 }), Is.EqualTo(new List<int> { 87, 16, 80, 31, 79, 52, 78, 63, 74 }));
+				}
+
 				[Test]
 				public static void TestFixed()
 				{
