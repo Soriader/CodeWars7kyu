13963931870d7a8f2556771c99aadaa412958abe
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-18 12:16:45 +0200

    [~] Task: Multiply Word in String

---
+++ CodeWars7kyu/MultiplyWordInStringTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MultiplyWordInStringTask
+	{
+		public static string ModifyMultiply(string str, int loc, int num)
+		{
+			var check = str.Split(" ");
+
+			var wordWhatWeNeed = check[loc];
+
+			int iterator = 0;
+
+			string result = "";
+
+			while (iterator < num) 
+			{
+				result += wordWhatWeNeed + "-";
+				iterator++;
+			}
+
+			return result.Trim('-');
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5ace2d9f307eb29430000092/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -18,6 +18,15 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+				[Test]
+				public void MultiplyWordInStringTest()
+				{
+					Assert.AreEqual("is-is-is", MultiplyWordInStringTask.ModifyMultiply("is very easy to resolve that kata", 0, 3), "should return 'is-is-is'");
+					Assert.AreEqual("cheap-cheap-cheap-cheap", MultiplyWordInStringTask.ModifyMultiply("Talk is cheap Show me the code", 2, 4), "should return 'cheap-cheap-cheap'");
+					Assert.AreEqual("think-think-think-think-think-think", MultiplyWordInStringTask.ModifyMultiply("Everyone in this country should learn how to program because it teaches you how to think", 15, 6), "should return is-is-is");
+					Assert.AreEqual("ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance", MultiplyWordInStringTask.ModifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.", 6, 8), "should return 'ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance'");
+					Assert.AreEqual("around-around-around-around-around", MultiplyWordInStringTask.ModifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.", 2, 5), "should return'around-around-around-around-around'");
+				}
 
 				[Test]
 				public void MaxMinArraysTest()
