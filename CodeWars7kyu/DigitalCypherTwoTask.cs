780f41d6f4ce0c0d8ab1ece9176ced4dad741e8f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-19 13:11:03 +0200

    [~]Task: Digital cypher vol 2

---
+++ CodeWars7kyu/DigitalCypherTwoTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class DigitalCypherTwoTask
+	{
+		public static string Decode(int[] code, int key)
+		{
+			string keyString = key.ToString();
+			int keyLength = keyString.Length;
+
+			char[] decodedMessage = new char[code.Length];
+
+			for (int i = 0; i < code.Length; i++)
+			{
+				int keyDigit = int.Parse(keyString[i % keyLength].ToString());
+
+				int originalValue = code[i] - keyDigit;
+				decodedMessage[i] = (char)(originalValue + 'a' - 1);
+			}
+
+			return new string(decodedMessage);
+		}
+	}
+}
+//https://www.codewars.com/kata/592edfda5be407b9640000b2/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void DigitalCypherTwoTest()
+				{
+					Assert.AreEqual("scout", DigitalCypherTwoTask.Decode(new int[] { 20, 12, 18, 30, 21 }, 1939));
+					Assert.AreEqual("masterpiece", DigitalCypherTwoTask.Decode(new int[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 }, 1939));
+				}
+
+				[Test]
 				public void FibonacciTest()
 				{
 					Assert.AreEqual(5, FibonacciTask.Fib(5));
