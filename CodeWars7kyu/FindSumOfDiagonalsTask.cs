9d769e90af5342cde6e2725cafe1e3cb2cd36666
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-19 13:13:18 +0200

    [~] Task: Find sum of top-left to bottom-right diagonals

---
+++ CodeWars7kyu/FindSumOfDiagonalsTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FindSumOfDiagonalsTask
+	{
+		public static int DiagonalSum(int[,] matrix)
+		{
+			int sum = 0;
+			int size = matrix.GetLength(0);  
+
+			for (int i = 0; i < size; i++)
+			{
+				sum += matrix[i, i]; 
+			}
+
+			return sum;
+		}
+	}
+}
+//https://www.codewars.com/kata/5497a3c181dd7291ce000700/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FindSumOfDiagonalsTest()
+				{
+					Assert.AreEqual(12, FindSumOfDiagonalsTask.DiagonalSum(new int[,] { { 12 } }));
+					Assert.AreEqual(5, FindSumOfDiagonalsTask.DiagonalSum(new int[2, 2] { { 1, 2 }, { 3, 4 } }));
+					Assert.AreEqual(15, FindSumOfDiagonalsTask.DiagonalSum(new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
+					Assert.AreEqual(34, FindSumOfDiagonalsTask.DiagonalSum(new int[,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } }));
+				}
+
+				[Test]
 				public void DigitalCypherTwoTest()
 				{
 					Assert.AreEqual("scout", DigitalCypherTwoTask.Decode(new int[] { 20, 12, 18, 30, 21 }, 1939));
