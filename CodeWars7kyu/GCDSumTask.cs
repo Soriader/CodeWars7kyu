0cbcfef75b77d2812a32b30066c0d6a6f1ce4e1d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-25 15:12:09 +0200

    [~] Task: GCD sum

---
+++ CodeWars7kyu/GCDSumTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class GCDSumTask
+	{
+		public static (int, int) Solve(int s, int g)
+		{
+			if (s % g != 0)
+			{
+				return (-1, -1);
+			}
+
+			int x = s / g;
+
+			if (x <= 1)
+			{
+				return (-1, -1);
+			}
+
+			return (g, g * (x - 1));
+		}
+	}
+}
+//https://www.codewars.com/kata/5dd259444228280032b1ed2a/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,22 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void GCDSumTest()
+				{
+					Assert.AreEqual((3, 3), GCDSumTask.Solve(6, 3));
+					Assert.AreEqual((2, 6), GCDSumTask.Solve(8, 2));
+					Assert.AreEqual((-1, -1), GCDSumTask.Solve(10, 3));
+					Assert.AreEqual((4, 8), GCDSumTask.Solve(12, 4));
+					Assert.AreEqual((-1, -1), GCDSumTask.Solve(12, 5));
+					Assert.AreEqual((10, 40), GCDSumTask.Solve(50, 10));
+					Assert.AreEqual((-1, -1), GCDSumTask.Solve(50, 4));
+					Assert.AreEqual((5, 5), GCDSumTask.Solve(10, 5));
+					Assert.AreEqual((5, 95), GCDSumTask.Solve(100, 5));
+					Assert.AreEqual((5, 995), GCDSumTask.Solve(1000, 5));
+
+				}
+
+				[Test]
 				public void ThePonyExpressTest()
 				{
 					Assert.AreEqual(1, ThePonyExpressTask.Riders(new int[] { 18, 15 }));
