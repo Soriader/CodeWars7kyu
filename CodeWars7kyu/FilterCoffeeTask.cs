5413f24873e9b107ccd7d8fe0405f3040850679d
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-26 17:05:10 +0200

    [~] Task: Filter Coffee

---
+++ CodeWars7kyu/FilterCoffeeTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FilterCoffeeTask
+	{
+		public static string Search(int budget, int[] prices)
+		{
+			List<int> numberOfBudgetLocals = prices.Where(cost => cost <= budget).ToList();
+
+			numberOfBudgetLocals.Sort();
+
+			string result = string.Join(",", numberOfBudgetLocals);
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/56069d0c4af7f633910000d3/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FilterCoffeeTest()
+                {
+					Assert.AreEqual("1,2,2", FilterCoffeeTask.Search(3, new int[] { 6, 1, 2, 9, 2 }));
+					Assert.AreEqual("3,7,7,9,14", FilterCoffeeTask.Search(14, new int[] { 7, 3, 23, 9, 14, 20, 7 }));
+					Assert.AreEqual("", FilterCoffeeTask.Search(0, new int[] { 6, 1, 2, 9, 2 }));
+
+				}
+				[Test]
 				public void GCDSumTest()
 				{
 					Assert.AreEqual((3, 3), GCDSumTask.Solve(6, 3));
