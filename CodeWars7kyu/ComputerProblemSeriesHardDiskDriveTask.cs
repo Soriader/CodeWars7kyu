8b11f25e88c251513ad6dbc861f1496abe2d7bda
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-27 13:20:57 +0200

    [~] Task: Computer problem series #1: Fill the Hard Disk Drive

---
+++ CodeWars7kyu/ComputerProblemSeriesHardDiskDriveTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ComputerProblemSeriesHardDiskDriveTask
+	{
+		public static int Save(int[] sizes, int hd)
+		{
+			int filesAdded = 0;
+			int result = 0;
+
+
+			if (sizes.Sum() <= hd)
+			{
+				return sizes.Length;
+			}
+
+
+			for (int i = 0; i < sizes.Length -1; i++)
+			{
+				if(sizes.First() > hd)
+				{
+					return 0;
+				}
+
+				filesAdded += sizes[i];
+				result++;
+
+				if (filesAdded == hd || filesAdded + sizes[i + 1] > hd)
+				{
+					break;
+				}
+
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/5d49c93d089c6e000ff8428c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SaveFilesTest()
+				{
+					Assert.AreEqual(3, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 4, 4, 4, 3, 3 }, 12));
+					Assert.AreEqual(2, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 4, 4, 4, 3, 3 }, 11));
+					Assert.AreEqual(6, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 4, 8, 15, 16, 23, 42 }, 108));
+					Assert.AreEqual(1, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 13 }, 13));
+					Assert.AreEqual(4, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 1, 2, 3, 4 }, 250));
+					Assert.AreEqual(1, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 100 }, 500));
+					Assert.AreEqual(0, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 11, 13, 15, 17, 19 }, 8));
+					Assert.AreEqual(0, ComputerProblemSeriesHardDiskDriveTask.Save(new int[] { 45 }, 12));
+				}
+				[Test]
 				public void CatDogTest()
 				{
 					Assert.AreEqual((1, 1), CatDogTask.OwnedCatAndDog(15, 15));
