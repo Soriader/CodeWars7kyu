f49e2eb2e2c85592892f77bc3f08a1cff668d73f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-29 13:41:13 +0200

    [~] Task: Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark

---
+++ CodeWars7kyu/RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask
+	{
+		public static string Remove(string s)
+		{
+			var check = s.Split(" ");
+			int iterator = 0;
+			string result = "";
+
+			foreach (var word in check) 
+			{
+				var howManyExclamation = word.ToCharArray();
+
+
+				foreach (var exclamation in howManyExclamation)
+				{
+					if(exclamation == '!')
+					{
+						iterator++;
+					}
+				}
+
+				if (iterator >= 2 || iterator == 0)
+				{
+					result += word + " ";
+				}
+
+				iterator = 0;
+			}
+
+			return result.TrimEnd();
+		}
+	}
+}
+//https://www.codewars.com/kata/57fafb6d2b5314c839000195/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,24 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTest()
+				{
+					Assert.AreEqual("", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi!"));
+					Assert.AreEqual("", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("!Hi"));
+					Assert.AreEqual("", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi! Hi! Hi!"));
+					Assert.AreEqual("", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi! !Hi Hi!"));
+					Assert.AreEqual("", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi! Hi!"));
+					Assert.AreEqual("!Hi!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("!Hi!"));
+					Assert.AreEqual("Hi!!!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi!!!"));
+					Assert.AreEqual("Hi", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi Hi! Hi!"));
+					Assert.AreEqual("Hi hi hI", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi hi hI"));
+					Assert.AreEqual("Hi!!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi! Hi!! Hi!"));
+					Assert.AreEqual("!Hi!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("Hi! !Hi! Hi!"));
+					Assert.AreEqual("!Hi! !Hi!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("!Hi! ! !Hi!"));
+					Assert.AreEqual("!!!Hi !!hi!!!", RemoveWordsFromTheSentenceIfItContainsOneExclamationMarkTask.Remove("!!!Hi !!hi!!! !hi"));
+				}
+
+				[Test]
 				public void MoveAllExclamationMarksToTheEndOfTheSentenceTest()
 				{
 					Assert.AreEqual("Hi!", MoveAllExclamationMarksToTheEndOfTheSentenceTask.Remove("Hi!"));
