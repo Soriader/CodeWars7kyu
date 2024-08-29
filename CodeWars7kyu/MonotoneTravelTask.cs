914796c34d463bcd0bd07bcc2ce7fb5279243a5c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-29 13:56:54 +0200

    [~] Task: Monotone travel

---
+++ CodeWars7kyu/MonotoneTravelTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MonotoneTravelTask
+	{
+		public static bool IsMonotone(int[] arr)
+		{
+
+			for (int i = 0; i < arr.Length - 1; i++) 
+			{
+				if(arr[i + 1] >= arr[i])
+				{
+					continue;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			return true;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/54466996990c921f90000d61/train/csharp
