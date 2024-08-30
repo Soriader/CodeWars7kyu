a6501f4b879206a2b4a1e4a7ca37c771eea4e370
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-30 13:47:28 +0200

    [~] Task: What dominates your array?

---
+++ CodeWars7kyu/WhatDominatesYourArrayTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class WhatDominatesYourArrayTask
+	{
+		public static int WhoDominates(int[] array)
+		{
+			Dictionary<int, int> howManyNumbersInArray = new Dictionary<int, int>();
+
+			int n = array.Length;
+
+			foreach (int number in array)
+			{
+				if (howManyNumbersInArray.ContainsKey(number))
+				{
+					howManyNumbersInArray[number]++;
+				}
+				else
+				{
+					howManyNumbersInArray.Add(number, 1);
+				}
+			}
+
+			foreach (var kvp in howManyNumbersInArray)
+			{
+				if (kvp.Value > n / 2)
+				{
+					return kvp.Key; 
+				}
+			}
+
+			return -1;
+		}
+	}
+	
+}
+//https://www.codewars.com/kata/559e10e2e162b69f750000b4/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void WhatDominatesYourArrayTest()
+				{
+					Assert.AreEqual(3, WhatDominatesYourArrayTask.WhoDominates(new int[] { 3, 4, 3, 2, 3, 1, 3, 3 }));
+					Assert.AreEqual(-1, WhatDominatesYourArrayTask.WhoDominates(new int[] { 1, 2, 3, 4, 5 }));
+					Assert.AreEqual(-1, WhatDominatesYourArrayTask.WhoDominates(new int[] { 1, 1, 1, 2, 2, 2 }));
+					Assert.AreEqual(2, WhatDominatesYourArrayTask.WhoDominates(new int[] { 1, 1, 1, 2, 2, 2, 2 }));
+				}
+
+				[Test]
 				public void MonotoneTravelTest()
 				{
 					Assert.AreEqual(true, MonotoneTravelTask.IsMonotone(new int[] { 1, 2, 3, 3, 4, 5 }));
