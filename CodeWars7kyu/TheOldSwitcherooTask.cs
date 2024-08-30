2b8f3b658989a4d3c7dd4a2ff6bcbb73e438913b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-30 14:07:41 +0200

    [~] Task: The old switcheroo 2

---
+++ CodeWars7kyu/TheOldSwitcherooTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TheOldSwitcherooTask
+	{
+		public static string Encode(string str)
+		{
+			Dictionary<char, int> letterToNumber = new Dictionary<char, int>();
+
+			for (char letter = 'a'; letter <= 'z'; letter++)
+			{
+				letterToNumber[letter] = letter - 'a' + 1;
+			}
+
+			string result = "";
+
+			var check = str.ToLower().ToCharArray();
+
+			foreach (char letter in check)
+			{
+				if (char.IsLetter(letter)) 
+				{
+					result += letterToNumber[letter].ToString();             
+				}
+				else
+				{
+					result += letter;
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/55d6a0e4ededb894be000005/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void TheOldSwitcherooTest()
+                {
+					Assert.AreEqual("123", TheOldSwitcherooTask.Encode("abc"));
+					Assert.AreEqual("1234", TheOldSwitcherooTask.Encode("ABCD"));
+					Assert.AreEqual("2626262626", TheOldSwitcherooTask.Encode("ZzzzZ"));
+					Assert.AreEqual("123-#@5", TheOldSwitcherooTask.Encode("abc-#@5"));
+					Assert.AreEqual("208919 919 1 1215147 1920189147!! 161251195 [51431545] @30181853201225", TheOldSwitcherooTask.Encode("this is a long string!! Please [encode] @C0RrEctly"));
+				}
+
+				[Test]
 				public void WhatDominatesYourArrayTest()
 				{
 					Assert.AreEqual(3, WhatDominatesYourArrayTask.WhoDominates(new int[] { 3, 4, 3, 2, 3, 1, 3, 3 }));
