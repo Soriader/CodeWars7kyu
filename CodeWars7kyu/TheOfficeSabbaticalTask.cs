7193ab08cee6b83306512841532bef5d4a1e2241
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-31 13:10:20 +0200

    [~] Task: The Office VI - Sabbatical

---
+++ CodeWars7kyu/TheOfficeSabbaticalTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TheOfficeSabbaticalTask
+	{
+		public static string Sabb(string s, int val, int happiness)
+		{
+			char[] sabbatical = { 's', 'a', 'b', 't', 'i', 'c', 'l' };
+			int iterator = 0;
+
+			var check = s.ToLower().ToCharArray();
+
+			for (int i = 0; i < check.Length; i++)
+			{
+				if (sabbatical.Contains(check[i]))
+				{
+					iterator++;
+				}
+			}
+
+			if (iterator + val + happiness > 22)
+			{
+				return "Sabbatical! Boom!";
+			}
+			else
+			{
+				return "Back to your desk, boy.";
+			}
+		}
+	}
+}
+//https://www.codewars.com/kata/57fe50d000d05166720000b1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void TheOfficeSabbaticalTest()
+				{
+					Assert.AreEqual("Sabbatical! Boom!", TheOfficeSabbaticalTask.Sabb("Can I have a sabbatical?", 5, 5));
+					Assert.AreEqual("Back to your desk, boy.", TheOfficeSabbaticalTask.Sabb("Why are you shouting?", 7, 2));
+					Assert.AreEqual("Sabbatical! Boom!", TheOfficeSabbaticalTask.Sabb("What do you mean I cant learn to code??", 8, 9));
+					Assert.AreEqual("Back to your desk, boy.", TheOfficeSabbaticalTask.Sabb("Please calm down", 9, 1));
+				}
+
+				[Test]
 				public void TheOldSwitcherooTest()
                 {
 					Assert.AreEqual("123", TheOldSwitcherooTask.Encode("abc"));
