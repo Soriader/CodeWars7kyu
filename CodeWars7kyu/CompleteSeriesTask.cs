3445ec4e0ca98b4ee549234694a6868719d3513c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-31 13:50:17 +0200

    [~] Task: Complete Series

---
+++ CodeWars7kyu/CompleteSeriesTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CompleteSeriesTask
+	{
+		public static int[] CompleteSeries(int[] arr)
+		{
+			Dictionary<int,int> checkTheRepeat = new Dictionary<int,int>();
+
+			foreach (int item in arr) 
+			{
+				if (checkTheRepeat.ContainsKey(item))
+				{
+					checkTheRepeat[item]++;
+				}
+				else
+				{
+					checkTheRepeat.Add(item, 1);
+				}
+			}
+
+			foreach (var pair in checkTheRepeat)
+			{
+				if (pair.Value >= 2)
+				{
+					return new int[] { 0 };
+				}
+			}
+
+			int maxValue = arr.Max();
+			int iterator = 0;
+			List<int> result = new List<int>();
+
+			while (maxValue >= iterator) 
+			{
+				result.Add(iterator);
+				iterator++;
+			}
+
+			return result.ToArray();	
+		}
+	}
+}
+//https://www.codewars.com/kata/580a4001d6df740d61000301/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void CompleteSeriesTest()
+				{
+					Assert.AreEqual(new int[] { 0, 1 }, CompleteSeriesTask.CompleteSeries(new int[] { 0, 1 }));
+					Assert.AreEqual(new int[] { 0, 1, 2, 3, 4, 5, 6 }, CompleteSeriesTask.CompleteSeries(new int[] { 1, 4, 6 }));
+					Assert.AreEqual(new int[] { 0, 1, 2, 3, 4, 5 }, CompleteSeriesTask.CompleteSeries(new int[] { 3, 4, 5 }));
+					Assert.AreEqual(new int[] { 0, 1, 2 }, CompleteSeriesTask.CompleteSeries(new int[] { 2, 1 }));
+					Assert.AreEqual(new int[] { 0 }, CompleteSeriesTask.CompleteSeries(new int[] { 1, 4, 4, 6 }));
+				}
+
+				[Test]
 				public void TheOfficeSabbaticalTest()
 				{
 					Assert.AreEqual("Sabbatical! Boom!", TheOfficeSabbaticalTask.Sabb("Can I have a sabbatical?", 5, 5));
