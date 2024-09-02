f283d7457fe71d52df80c855b49a7dc015c9066c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-02 15:12:37 +0200

    [~] Task: Moves in squared strings (I)

---
+++ CodeWars7kyu/MovesInSquaredStringsTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MovesInSquaredStringsTask
+	{
+		public static string VertMirror(string strng)
+		{
+			return string.Join("\n", strng.Split('\n').Select(line => new string(line.Reverse().ToArray())));
+		}
+
+		public static string HorMirror(string strng)
+		{
+			return string.Join("\n", strng.Split('\n').Reverse());
+		}
+
+		public static string Oper(Func<string, string> fct, string s)
+		{
+			return fct(s);
+		}
+	}
+}
+//https://www.codewars.com/kata/56dbe0e313c2f63be4000b25/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,25 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void MovesInSquaredStringsTest()
+				{
+					Console.WriteLine("Fixed Tests VertMirror");
+					testing(MovesInSquaredStringsTask.Oper(MovesInSquaredStringsTask.VertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"),
+						"QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw");
+					testing(MovesInSquaredStringsTask.Oper(MovesInSquaredStringsTask.VertMirror, "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx"),
+						"EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP");
+
+					Console.WriteLine("Fixed Tests HorMirror");
+					testing(MovesInSquaredStringsTask.Oper(MovesInSquaredStringsTask.HorMirror, "lVHt\nJVhv\nCSbg\nyeCt"), "yeCt\nCSbg\nJVhv\nlVHt");
+					testing(MovesInSquaredStringsTask.Oper(MovesInSquaredStringsTask.HorMirror, "njMK\ndbrZ\nLPKo\ncEYz"), "cEYz\nLPKo\ndbrZ\nnjMK");
+
+					static void testing(string actual, string expected)
+					{
+						Assert.AreEqual(expected, actual);
+					}
+				}
+
+				[Test]
 				public void CompleteSeriesTest()
 				{
 					Assert.AreEqual(new int[] { 0, 1 }, CompleteSeriesTask.CompleteSeries(new int[] { 0, 1 }));
