49bb0eddd9ceeb0971ec6475f5b18997dff60c11
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-02 15:27:03 +0200

    [~] Task: Linked Lists - Push & BuildOneTwoThree

---
+++ CodeWars7kyu/LinkedListsPushBuildOneTwoThreeTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeWars7kyu
+{
+	public class LinkedListsPushBuildOneTwoThreeTask
+	{
+		public int Data;
+		public LinkedListsPushBuildOneTwoThreeTask Next;
+
+		public LinkedListsPushBuildOneTwoThreeTask(int data)
+		{
+			this.Data = data;
+			this.Next = null;
+		}
+
+		public static LinkedListsPushBuildOneTwoThreeTask Push(LinkedListsPushBuildOneTwoThreeTask head, int data)
+		{
+			LinkedListsPushBuildOneTwoThreeTask newNode = new LinkedListsPushBuildOneTwoThreeTask(data);
+			newNode.Next = head;
+			return newNode;
+		}
+
+		public static LinkedListsPushBuildOneTwoThreeTask BuildOneTwoThree()
+		{
+			LinkedListsPushBuildOneTwoThreeTask list = null;
+			list = Push(list, 3); 
+			list = Push(list, 2); 
+			list = Push(list, 1); 
+			return list;
+		}
+	}
+}
+//https://www.codewars.com/kata/55be95786abade3c71000079/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,20 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void pushTests()
+				{
+					Assert.AreEqual(1, LinkedListsPushBuildOneTwoThreeTask.Push(null, 1).Data, "Should be able to create a new linked list with push().");
+					Assert.AreEqual(null, LinkedListsPushBuildOneTwoThreeTask.Push(null, 1).Next, "Should be able to create a new linked list with push().");
+					Assert.AreEqual(2, LinkedListsPushBuildOneTwoThreeTask.Push(new LinkedListsPushBuildOneTwoThreeTask(1), 2).Data, "Should be able to prepend a node to an existing node.");
+					Assert.AreEqual(1, LinkedListsPushBuildOneTwoThreeTask.Push(new LinkedListsPushBuildOneTwoThreeTask(1), 2).Next.Data, "Should be able to prepend a node to an existing node.");
+
+					Assert.AreEqual(1, LinkedListsPushBuildOneTwoThreeTask.BuildOneTwoThree().Data, "First node should should have 1 as data.");
+					Assert.AreEqual(2, LinkedListsPushBuildOneTwoThreeTask.BuildOneTwoThree().Next.Data, "Second node should should have 2 as data.");
+					Assert.AreEqual(3, LinkedListsPushBuildOneTwoThreeTask.BuildOneTwoThree().Next.Next.Data, "Third node should should have 3 as data.");
+					Assert.AreEqual(null, LinkedListsPushBuildOneTwoThreeTask.BuildOneTwoThree().Next.Next.Next, "Third node should be the tail of the linked list");
+				}
+
+				[Test]
 				public static void MovesInSquaredStringsTest()
 				{
 					Console.WriteLine("Fixed Tests VertMirror");
