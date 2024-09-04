17cce2c7be7b7b842fd1045e2876093a9c3dfca6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-04 15:05:38 +0200

    [~] Task: Difference between years. (Level 1)

---
+++ CodeWars7kyu/DifferenceBetweenYaersTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class DifferenceBetweenYaersTask
+	{
+		public static int HowManyYears(string date1, string date2)
+		{
+			var firstDate = date1.Split('/').Select(x => int.Parse(x)).ToList();
+			var secondDate = date2.Split('/').Select(x => int.Parse(x)).ToList(); 
+
+			if(secondDate[0] > firstDate[0])
+			{
+				return secondDate[0] - firstDate[0];
+			}
+
+			return firstDate[0] - secondDate[0];
+		}
+	}
+}
+//https://www.codewars.com/kata/588f5a38ec641b411200005b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void DifferenceBetweenYaersTest()
+				{
+					Assert.AreEqual(18, DifferenceBetweenYaersTask.HowManyYears("1997/10/10", "2015/10/10"));
+					Assert.AreEqual(25, DifferenceBetweenYaersTask.HowManyYears("1990/10/10", "2015/10/10"));
+					Assert.AreEqual(25, DifferenceBetweenYaersTask.HowManyYears("2015/10/10", "1990/10/10"));
+					Assert.AreEqual(23, DifferenceBetweenYaersTask.HowManyYears("1992/10/24", "2015/10/24"));
+					Assert.AreEqual(18, DifferenceBetweenYaersTask.HowManyYears("2018/10/10", "2000/10/10"));
+					Assert.AreEqual(0, DifferenceBetweenYaersTask.HowManyYears("2000/10/10", "2000/10/10"));
+				}
+
+				[Test]
 				public void TriangularNumbersTest()
 				{
 					Assert.AreEqual(true, TriangularNumbersTask.IsTriangular(6));
