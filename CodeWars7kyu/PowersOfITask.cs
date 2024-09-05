1a03f3123a2c5bc5a013cf7104a70f74f9326812
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-05 15:51:43 +0200

    [~] Task: Powers of i

---
+++ CodeWars7kyu/PowersOfITask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PowersOfITask
+	{
+		public static string Pofi(int n)
+		{
+			switch (n % 4)
+			{
+				case 0:
+					return "1";     
+				case 1:
+					return "i";     
+				case 2:
+					return "-1";    
+				case 3:
+					return "-i";    
+			}
+
+			return "1";
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5a97387e5ee396e70a00016d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,25 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void PowersOfITest()
+				{
+					int i = 4;
+					Assert.AreEqual("1", PowersOfITask.Pofi(0));
+					Assert.AreEqual("i", PowersOfITask.Pofi(1));
+					Assert.AreEqual("-1", PowersOfITask.Pofi(2));
+					Assert.AreEqual("-i", PowersOfITask.Pofi(3));
+					Assert.AreEqual("1", PowersOfITask.Pofi(i));
+					Assert.AreEqual("i", PowersOfITask.Pofi(i + 1));
+					Assert.AreEqual("-1", PowersOfITask.Pofi(i + 2));
+					Assert.AreEqual("-i", PowersOfITask.Pofi(i + 3));
+					i += 4;
+					Assert.AreEqual("1", PowersOfITask.Pofi(i));
+					Assert.AreEqual("i", PowersOfITask.Pofi(i + 1));
+					Assert.AreEqual("-1", PowersOfITask.Pofi(i + 2));
+					Assert.AreEqual("-i", PowersOfITask.Pofi(i + 3));
+				}
+
+				[Test]
 				public void PossibilitiesArrayTest()
 				{
 					Assert.AreEqual(true, PossibilitiesArrayTask.IsAllPossibilities(new int[] { 0, 1, 2, 3 }));
