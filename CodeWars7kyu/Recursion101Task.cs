1974bd0772f222f1aae55ae9d8c5fb2a22dcf3dd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-06 15:40:09 +0200

    [~] Task: Recursion 101

---
+++ CodeWars7kyu/Recursion101Task.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class Recursion101Task
+	{
+		public static (int, int) Solve(int a, int b)
+		{
+			while (a != 0 && b != 0)
+			{
+				if (a >= 2 * b)
+				{
+					a -= 2 * b;
+				}
+				else if (b >= 2 * a)
+				{
+					b -= 2 * a;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			return (a, b);
+		}
+	}
+}
+//https://www.codewars.com/kata/5b752a42b11814b09c00005d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void Recursion101Test()
+				{
+					Assert.AreEqual((6, 7), Recursion101Task.Solve(6, 19));
+					Assert.AreEqual((0, 1), Recursion101Task.Solve(2, 1));
+					Assert.AreEqual((0, 1), Recursion101Task.Solve(22, 5));
+					Assert.AreEqual((2, 2), Recursion101Task.Solve(2, 10));
+					Assert.AreEqual((1019, 1442), Recursion101Task.Solve(8796203, 7556));
+					Assert.AreEqual((19394, 19394), Recursion101Task.Solve(19394, 19394));
+
+				}
+
+				[Test]
 				public void GeometricProgressionSequenceTest()
 				{
 					Assert.AreEqual("2, 6, 18, 54, 162", GeometricProgressionSequenceTask.GeometricSequenceElements(2, 3, 5));
