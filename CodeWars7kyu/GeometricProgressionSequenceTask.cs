400063bdfd6ebd7c3c020c52b626ce1061612f10
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-06 14:50:07 +0200

    [~] Task: Geometric Progression Sequence

---
+++ CodeWars7kyu/GeometricProgressionSequenceTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class GeometricProgressionSequenceTask
+	{
+		public static string GeometricSequenceElements(int a, int r, int n)
+		{
+			if(n == 1)
+			{
+				return $"{a}";
+			}
+
+			string result = "";
+			int actualElement = a;
+			int iterator = 1;
+
+			result += actualElement + ", ";
+
+			for (int i = 1; i < n; i++) 
+			{
+				iterator++;
+
+				if (iterator == n)
+				{
+					actualElement = actualElement * r;
+					result += actualElement;
+					break;
+				}
+
+				actualElement = actualElement * r;
+				result += actualElement + ", ";
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/55caef80d691f65cb6000040/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void GeometricProgressionSequenceTest()
+				{
+					Assert.AreEqual("2, 6, 18, 54, 162", GeometricProgressionSequenceTask.GeometricSequenceElements(2, 3, 5));
+					Assert.AreEqual("2, 4, 8, 16, 32, 64, 128, 256, 512, 1024", GeometricProgressionSequenceTask.GeometricSequenceElements(2, 2, 10));
+					Assert.AreEqual("1, -2, 4, -8, 16, -32, 64, -128, 256, -512", GeometricProgressionSequenceTask.GeometricSequenceElements(1, -2, 10));
+				}
+
+				[Test]
 				public static void FindTheLongestGapTest()
 				{
 					Assert.AreEqual(2, FindTheLongestGapTask.Gap(9));
