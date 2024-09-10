66ee8f8a8c42f2bca48d7147a04d9b85afa6ee6a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-10 12:39:01 +0200

    [~] Task: Find the index of the second occurrence of a letter in a string

---
+++ CodeWars7kyu/FindTheIndexOfTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FindTheIndexOfTask
+	{
+		public static int SecondSymbol(string str, char symbol)
+		{
+			var check = str.ToCharArray();
+			int iteratror = 0;
+			bool findOne = false;
+			bool notFind = true;
+			int howManySymbol = 0;
+
+			foreach (char c in check) 
+			{
+				if (c == symbol)
+				{
+					findOne = true;
+					howManySymbol++;
+				}
+
+				if (c == symbol && findOne == true && howManySymbol == 2) 
+				{
+					notFind = false;
+					break;
+				}
+
+				iteratror++;
+			}
+
+			if (notFind)
+			{
+				return -1;
+			}
+
+			return iteratror;
+		}
+	}
+}
+//https://www.codewars.com/kata/63f96036b15a210058300ca9/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FindTheIndexOfTest()
+				{
+					Assert.AreEqual(3, FindTheIndexOfTask.SecondSymbol("Hello world!!", 'l'), "Find the index of the second symbol \"l\" in the string");
+					Assert.AreEqual(7, FindTheIndexOfTask.SecondSymbol("Hello world!!", 'o'), "Find the index of the second symbol \"o\" in the string");
+					Assert.AreEqual(-1, FindTheIndexOfTask.SecondSymbol("Hello world!!", 'A'), "The symbol \"A\" is not in the string");
+					Assert.AreEqual(-1, FindTheIndexOfTask.SecondSymbol("", 'q'), "The symbol \"q\" is not in the string");
+					Assert.AreEqual(-1, FindTheIndexOfTask.SecondSymbol("Hello", '!'), "The symbol \"!\" is not in the string");
+					Assert.AreEqual(-1, FindTheIndexOfTask.SecondSymbol("lHgMvSCkzgRgmSUtfSWnWENGgADkfLaYMbrjjzJp", 'l'));
+				}
+
+				[Test]
 				public void WhatIsMyGolfScoreTest()
 				{
 					Assert.That(WhatIsMyGolfScoreTask.GolfScoreCalculator("453454444344544443", "354445334534445348"), Is.EqualTo(3));
