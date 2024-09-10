b67e306f99ba327d0e53ae78a998a94aea874a5b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-10 12:26:26 +0200

    [~] Task: What's my golf score?

---
+++ CodeWars7kyu/WhatIsMyGolfScoreTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace CodeWars7kyu
+{
+	public class WhatIsMyGolfScoreTask
+	{
+		public static int GolfScoreCalculator(string par, string score)
+		{
+			var oneScore = score.ToCharArray();
+			var onePar = par.ToCharArray();
+
+			int result = 0;
+
+			for (int i = 0; i < oneScore.Length; i++)
+			{
+				result += (oneScore[i] - '0') - (onePar[i] - '0');
+			}
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/59f7a0a77eb74bf96b00006a/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void WhatIsMyGolfScoreTest()
+				{
+					Assert.That(WhatIsMyGolfScoreTask.GolfScoreCalculator("453454444344544443", "354445334534445348"), Is.EqualTo(3));
+					Assert.That(WhatIsMyGolfScoreTask.GolfScoreCalculator("333333333333333333", "444444444444444444"), Is.EqualTo(18));
+				}
+
+				[Test]
 				public void Recursion101Test()
 				{
 					Assert.AreEqual((6, 7), Recursion101Task.Solve(6, 19));
