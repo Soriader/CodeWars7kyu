00114d8b90d82a41c187afa83da4f2fa15412014
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-12 12:06:56 +0200

    [~] Task: Scaling Squared Strings

---
+++ CodeWars7kyu/ScalingSquaredStringsTask.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars7kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ScalingSquaredStringsTask
+	{
+		public static string Scale(string strng, int k, int n)
+		{
+			if (string.IsNullOrEmpty(strng)) 
+			{
+				return "";
+			}
+
+			var lines = strng.Split('\n');
+
+			var scaledLines = lines
+				.Select(line => string.Concat(line.Select(ch => new string(ch, k))))
+				.SelectMany(scaledLine => Enumerable.Repeat(scaledLine, n));
+
+			return string.Join("\n", scaledLines);			
+		}
+	}
+}
+//https://www.codewars.com/kata/56ed20a2c4e5d69155000301/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,24 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void test1()
+				{
+                    
+					string a = "abcd\nefgh\nijkl\nmnop";
+					string r = "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\n"
+							+ "iijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp";
+					testing(ScalingSquaredStringsTask.Scale(a, 2, 3), r);
+					testing(ScalingSquaredStringsTask.Scale("", 5, 5), "");
+                    testing(ScalingSquaredStringsTask.Scale("Kj\nSH", 1, 2), "Kj\nKj\nSH\nSH");
+
+
+						static void testing(string actual, string expected)
+					{
+						Assert.AreEqual(expected, actual);
+					}
+				}
+
+				[Test]
 				public void NumberPairsTest()
 				{
 					int[] arr1 = new int[] { 13, 64, 5, 7, 88 };
