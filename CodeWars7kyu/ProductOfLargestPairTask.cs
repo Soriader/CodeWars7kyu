22dd2df7c4a1e64c928c0dd813b90873c1ff1ced
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-14 21:34:56 +0200

    [~]Task: Product of Largest Pair

---
+++ CodeWars7kyu/ProductOfLargestPairTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ProductOfLargestPairTask
+	{
+		public static int MaxProduct(int[] array)
+		{
+			var bigNumbers = array.OrderByDescending(x => x).Take(2).ToArray();
+
+			return bigNumbers[0] * bigNumbers[1];
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5784c89be5553370e000061b/train/csharp
