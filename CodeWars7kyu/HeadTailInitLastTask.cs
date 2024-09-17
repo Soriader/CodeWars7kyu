bbe2669f31ff34f22d37d08028e313d72dbaf100
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-17 19:21:09 +0200

    [~] Task: Head, Tail, Init and Last

---
+++ CodeWars7kyu/HeadTailInitLastTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public static class HeadTailInitLastTask
+	{
+		public static TSource Head<TSource>(this List<TSource> list)
+		{
+			return list.First();
+		}
+
+		public static List<TSource> Tail<TSource>(this List<TSource> list)
+		{
+			return list.Skip(1).ToList();
+		}
+
+		public static List<TSource> Init<TSource>(this List<TSource> list)
+		{
+			return list.Take(list.Count - 1).ToList();
+		}
+
+		public static TSource GetLast<TSource>(this List<TSource> list)
+		{
+			return list.Last();
+		}
+	}
+}
+//https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void HeadTailInitLastTest()
+				{
+					Assert.AreEqual(5, HeadTailInitLastTask.Head(new List<int> { 5, 1 }));
+					Assert.AreEqual(new List<int> { 2, 3 }, HeadTailInitLastTask.Tail(new List<int> { 1, 2, 3 }));
+					Assert.AreEqual(new List<int> { 1, 5, 7 }, HeadTailInitLastTask.Init(new List<int> { 1, 5, 7, 9 }));
+					Assert.AreEqual(2, HeadTailInitLastTask.GetLast(new List<int> { 7, 2 }));
+				}
+
+				[Test]
 				public void RemoveAllExclamationMarksFromSentenceExceptAtTheEndTest()
 				{
 					Assert.AreEqual("Hi!", RemoveAllExclamationMarksFromSentenceExceptAtTheEndTask.Remove("Hi!"));
