48ee460e208ab1378b0292383eaa4c58c947dee1
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-18 17:04:02 +0200

    [~] Task: Count the Digit

---
+++ CodeWars7kyu/CountTheDigitTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CountTheDigitTask
+	{
+		public static int NbDig(int n, int d)
+		{
+			int result = 0;
+			char digit = d.ToString()[0];
+
+			for (int i = 0; i <= n; i++)
+			{
+				int square = i * i; 
+
+				string squareStr = square.ToString();
+
+				foreach (char c in squareStr)
+				{
+					if (c == digit)
+					{
+						result++;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/566fc12495810954b1000030/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void CountTheDigitTest()
+				{
+					Assert.AreEqual(4700, CountTheDigitTask.NbDig(5750, 0));
+					Assert.AreEqual(9481, CountTheDigitTask.NbDig(11011, 2));
+					Assert.AreEqual(7733, CountTheDigitTask.NbDig(12224, 8));
+				}
+
+				[Test]
 				public void SuzukiNeedsHelpLiningUpTest()
 				{
 					string students = "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi";
