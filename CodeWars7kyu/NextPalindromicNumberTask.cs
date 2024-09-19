55cccc3944ad12ed757a809ee6db78f9d4aa2069
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-19 20:33:27 +0200

    [~] Task: Next Palindromic Number

---
+++ CodeWars7kyu/NextPalindromicNumberTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class NextPalindromicNumberTask
+    {
+		public static int NextPal(int val)
+		{
+			var check = val;
+			bool isPalindrom = true;
+
+			while (isPalindrom)
+			{
+				check += 1;
+
+				string checkStr = check.ToString();
+
+				string reversedStr = new string(checkStr.Reverse().ToArray());
+
+				if (checkStr == reversedStr)
+				{
+					isPalindrom = false;
+				}
+			}
+
+			return check;
+
+			
+		}
+	}
+}
+//https://www.codewars.com/kata/56a6ce697c05fb4667000029/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void NextPalindromicNumberTest()
+				{
+					Assert.AreEqual(22, NextPalindromicNumberTask.NextPal(11));
+					Assert.AreEqual(191, NextPalindromicNumberTask.NextPal(188));
+					Assert.AreEqual(202, NextPalindromicNumberTask.NextPal(191));
+					Assert.AreEqual(2552, NextPalindromicNumberTask.NextPal(2541));
+				}
+
+				[Test]
 				public void BirthdayICakeTest()
 				{
 					Assert.AreEqual("That was close!", BirthdayICakeTask.Cake(900, "abcdef"));
