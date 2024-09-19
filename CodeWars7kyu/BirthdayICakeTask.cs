115160c3d6287b3dd58fba66be5998c8a83f0bcd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-19 19:56:56 +0200

    [~] Task: Birthday I - Cake

---
+++ CodeWars7kyu/BirthdayICakeTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class BirthdayICakeTask
+	{
+		public static string Cake(int x, string y)
+		{
+			if(x <= 0 || y == null)
+			{
+				return "That was close!";
+			}
+
+			var check = y.ToCharArray();
+			int sum = 0;
+
+			Dictionary<char, int> letterToNumber = new Dictionary<char, int>();
+
+			for (char letter = 'a'; letter <= 'z'; letter++)
+			{
+				letterToNumber[letter] = letter - 'a' + 1;
+			}
+
+			for (int i = 0; i < check.Length; i++)
+			{
+				if (i % 2 == 0)  
+				{
+					sum += (int)check[i];
+				}
+				else if (letterToNumber.ContainsKey(check[i])) 
+				{
+					sum += letterToNumber[check[i]];
+				}
+			}
+
+			int seventyPercent = x*70/100;
+
+			if(sum >= seventyPercent)
+			{
+				return "Fire!";
+
+			}
+
+			return "That was close!";
+		}
+	}
+}
+//https://www.codewars.com/kata/5805ed25c2799821cb000005/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void BirthdayICakeTest()
+				{
+					Assert.AreEqual("That was close!", BirthdayICakeTask.Cake(900, "abcdef"));
+					Assert.AreEqual("Fire!", BirthdayICakeTask.Cake(56, "ifkhchlhfd"));
+					Assert.AreEqual("Fire!", BirthdayICakeTask.Cake(256, "aaaaaddddr"));
+				}
+
+				[Test]
                 public void FloatPrecisionTest()
                 {
 					Assert.AreEqual(2.34, FloatPrecisionTask.Round(2.34));
