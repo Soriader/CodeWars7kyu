deca06b50685d67cb5b6d03904be1d2bebc985e8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-20 20:26:43 +0200

    [~] Task: Pairs of integers from m to n

---
+++ CodeWars7kyu/PairsOfIntegersFromTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PairsOfIntegersFromTask
+	{
+		public static IEnumerable<(int, int)> GeneratePairs(int m, int n)
+		{
+			int iteratorToM = m;
+			int iteratorToN = m;
+
+			while (iteratorToM <= n) 
+			{
+				yield return (iteratorToM, iteratorToN);
+
+				if(iteratorToN == n)
+				{
+					iteratorToM++;
+					iteratorToN = iteratorToM;
+				}
+				else
+				{
+					iteratorToN++;
+				}
+
+			}
+
+		}
+	}
+}
+//https://www.codewars.com/kata/588e2a1ad1140d31cb00008c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,23 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void PairsOfIntegersFromTest()
+				{
+					Act(2, 4, new[] { (2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4) });
+					Act(0, 1, new[] { (0, 0), (0, 1), (1, 1) });
+					Act(0, 0, new[] { (0, 0) });
+
+					static void Act(int m, int n, (int, int)[] expected)
+					{
+						var msg = $"Invalid answer for m: {m}, n: {n}";
+						var actual = PairsOfIntegersFromTask.GeneratePairs(m, n).ToArray();
+						CollectionAssert.AreEqual(Format(expected), Format(actual), msg);
+					}
+
+					static string Format((int, int)[] xs) => string.Join(", ", xs);
+				}
+
+				[Test]
 				public void NextPalindromicNumberTest()
 				{
 					Assert.AreEqual(22, NextPalindromicNumberTask.NextPal(11));
