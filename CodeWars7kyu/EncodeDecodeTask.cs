2bf4d4b2db1f1a12aee08cb6bca26ac65d3693c7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-22 17:26:14 +0200

    [~] Task: GA-DE-RY-PO-LU-KI cypher

---
+++ CodeWars7kyu/EncodeDecodeTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class EncodeDecodeTask
+    {
+		public static string Encode(string str)
+		{
+			var codeToEncode = "gaderypoluki";
+			string result = "";
+			var check = str.ToCharArray();
+
+			foreach (var c in check)
+			{
+				if (char.IsLetter(c) && codeToEncode.Contains(char.ToLower(c)))
+				{
+					char lowerC = char.ToLower(c);
+					int index = codeToEncode.IndexOf(lowerC);
+
+					char encodedChar;
+					if (index % 2 == 0)
+					{
+						encodedChar = codeToEncode[index + 1];
+					}
+					else
+					{
+						encodedChar = codeToEncode[index - 1];
+					}
+
+					if (char.IsUpper(c))
+					{
+						result += char.ToUpper(encodedChar);
+					}
+					else
+					{
+						result += encodedChar;
+					}
+				}
+				else
+				{
+					result += c;
+				}
+			}
+
+			return result;
+		}
+
+		public static string Decode(string str)
+		{
+			return Encode(str);
+		}
+	}
+}
+//https://www.codewars.com/kata/592a6ad46d6c5a62b600003f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void EncodeDecodeTest()
+				{
+					Assert.AreEqual("Gug hgs g cgt", EncodeDecodeTask.Encode("Ala has a cat"));
+					Assert.AreEqual("GBCE", EncodeDecodeTask.Encode("ABCD"));
+					Assert.AreEqual("Gug hgs g cgt", EncodeDecodeTask.Encode("Ala has a cat"));
+					Assert.AreEqual("agedyropulik", EncodeDecodeTask.Encode("gaderypoluki"));
+                    Assert.AreEqual("Ala has a cat", EncodeDecodeTask.Decode("Gug hgs g cgt"));
+                    Assert.AreEqual("gaderypoluki", EncodeDecodeTask.Decode("agedyropulik"));
+                    Assert.AreEqual("ABCD", EncodeDecodeTask.Decode("GBCE"));
+                }
+
+				[Test]
 				public void GetNthNodeTest()
 				{
 					GetNthNodeTask head = new GetNthNodeTask(1);
