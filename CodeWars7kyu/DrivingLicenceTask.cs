64b7fe2428a355a817c4bc61d285573d5a634805
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-22 18:24:41 +0200

    [~] Task: Driving Licence

---
+++ CodeWars7kyu/DrivingLicenceTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class DrivingLicenceTask
+	{
+		public string Driver(params string[] data)
+		{
+			var surname = data[2].Length >= 5 ? data[2].Substring(0, 5).ToUpper() : data[2].ToUpper().PadRight(5, '9');
+			var birthYear = data[3].Split('-')[2];
+			var decade = birthYear[2].ToString(); 
+
+			string monthOfBirth = "";
+			var checkTheMonth = data[3].Split('-');
+			string monthString = checkTheMonth[1].Length > 3 ? checkTheMonth[1].Substring(0, 3).ToUpper() : checkTheMonth[1].ToUpper();
+			string monthNumber = DateTime.ParseExact(monthString, "MMM", System.Globalization.CultureInfo.InvariantCulture).Month.ToString("D2");
+
+			if (data[4] == "F")
+			{
+				monthOfBirth = (int.Parse(monthNumber) + 50).ToString();
+			}
+			else
+			{
+				monthOfBirth = monthNumber;
+			}
+
+			var dateWithinTheMonthOfBirth = int.Parse(checkTheMonth[0]).ToString("D2");
+
+			var theYearDigit = birthYear[3].ToString();
+
+			var firstNameLetter = data[0].Substring(0, 1).ToUpper();
+			var secondNameLetter = data[1].Length > 0 ? data[1].Substring(0, 1).ToUpper() : "9";
+
+			var arbitraryDigit = "9";
+			var checkDigits = "AA";
+			return surname + decade + monthOfBirth + dateWithinTheMonthOfBirth + theYearDigit + firstNameLetter + secondNameLetter + arbitraryDigit + checkDigits;
+		}
+	}
+}
+//https://www.codewars.com/kata/586a1af1c66d18ad81000134/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void DrivingLicenceTest()
+				{
+					DrivingLicenceTask test2 = new DrivingLicenceTask();
+					string[] data2 = new string[] { "Johanna", "", "Gibbs", "13-Dec-1981", "F" };
+					Assert.AreEqual("GIBBS862131J99AA", test2.Driver(data2));
+
+					DrivingLicenceTask test = new DrivingLicenceTask();
+					string[] data = new string[] { "Andrew", "Robert", "Lee", "02-September-1981", "M" };
+					Assert.AreEqual("LEE99809021AR9AA", test.Driver(data));
+				}
+
+				[Test]
 				public void EncodeDecodeTest()
 				{
 					Assert.AreEqual("Gug hgs g cgt", EncodeDecodeTask.Encode("Ala has a cat"));
