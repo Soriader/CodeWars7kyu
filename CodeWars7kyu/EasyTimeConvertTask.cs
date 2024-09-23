51e4972600afcae93c4e15018f1d09a10476de45
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-23 18:45:18 +0200

    [~] Task: Easy Time Convert

---
+++ CodeWars7kyu/EasyTimeConvertTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class EasyTimeConvertTask
+	{
+		public static string TimeConvert(int num)
+		{
+			if (num < 0)
+			{
+				return "00:00";
+			}
+
+			int hours = num / 60; 
+			int minutes = num % 60; 
+
+			return $"{hours:D2}:{minutes:D2}";
+		}
+	}
+}
+//https://www.codewars.com/kata/5a084a098ba9146690000969/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void EasyTimeConvertTest()
+				{
+					Assert.AreEqual("00:00", EasyTimeConvertTask.TimeConvert(0));
+					Assert.AreEqual("00:00", EasyTimeConvertTask.TimeConvert(-6));
+					Assert.AreEqual("00:08", EasyTimeConvertTask.TimeConvert(8));
+					Assert.AreEqual("00:32", EasyTimeConvertTask.TimeConvert(32));
+					Assert.AreEqual("02:14", EasyTimeConvertTask.TimeConvert(134));
+					Assert.AreEqual("01:03", EasyTimeConvertTask.TimeConvert(63));
+					Assert.AreEqual("9429:17", EasyTimeConvertTask.TimeConvert(565757));
+				}
+				[Test]
 				public void IsItALetterTest()
 				{
 					Assert.AreEqual(true, IsItALetterTask.IsItLetter('a'));
