b87bb46d7c35c0e04d459e41d407cff8ff4503a3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-23 17:46:10 +0200

    [~] Task: Thinkful - Object Drills: Quarks

---
+++ CodeWars7kyu/QuarksTask.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class QuarksTask
+	{
+		private string _color;
+		private string _flavor;
+		private readonly double _baryonNumber = 1d / 3d; 
+
+		public string Color
+		{
+			get { return _color; }
+			set
+			{
+				if (value == "red" || value == "blue" || value == "green")
+				{
+					_color = value;
+				}
+				else
+				{
+					throw new ArgumentException("Color can only be red, blue, or green.");
+				}
+			}
+		}
+
+		public string Flavor
+		{
+			get { return _flavor; }
+			set
+			{
+				if (value == "up" || value == "down" || value == "strange" || value == "charm" || value == "top" || value == "bottom")
+				{
+					_flavor = value;
+				}
+				else
+				{
+					throw new ArgumentException("Choose correct flavor.");
+				}
+			}
+		}
+
+		public double BaryonNumber
+		{
+			get { return _baryonNumber; } 
+		}
+
+		public QuarksTask(string color, string flavor)
+		{
+			Color = color;
+			Flavor = flavor;
+		}
+
+		public void Interact(QuarksTask anotherQuark)
+		{
+			string tempColor = this.Color;  
+			this.Color = anotherQuark.Color; 
+			anotherQuark.Color = tempColor;  
+		}
+	}
+}
+//https://www.codewars.com/kata/5882b052bdeafec15e0000e6/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,19 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void QuarksTest()
+				{
+					QuarksTask q1 = new QuarksTask("red", "up");
+					QuarksTask q2 = new QuarksTask("blue", "strange");
+					Assert.AreEqual("red", q1.Color);
+					Assert.AreEqual("strange", q2.Flavor);
+					Assert.AreEqual(1 / 3d, q2.BaryonNumber);
+
+					q1.Interact(q2);
+					Assert.AreEqual("blue", q1.Color);
+				}
+
+				[Test]
 				public void DrivingLicenceTest()
 				{
 					DrivingLicenceTask test2 = new DrivingLicenceTask();
