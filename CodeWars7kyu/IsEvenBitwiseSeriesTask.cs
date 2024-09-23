4fae2a676c10e2bccf41f5b40436d5e9e1fab580
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-23 18:24:24 +0200

    [~] Task: isEven? - Bitwise Series

---
+++ CodeWars7kyu/IsEvenBitwiseSeriesTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+	public class IsEvenBitwiseSeriesTask
+	{
+		public static bool IsEven(int n)
+		{
+			bool isOdd = (n & 1) != 0;
+
+			return !isOdd;
+		}
+	}
+}
+//https://www.codewars.com/kata/592a33e549fe9840a8000ba1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void IsEvenBitwiseSeriesTest()
+				{
+					Assert.AreEqual(true, IsEvenBitwiseSeriesTask.IsEven(2), "2 is even");
+					Assert.AreEqual(false, IsEvenBitwiseSeriesTask.IsEven(3), "3 is odd");
+					Assert.AreEqual(true, IsEvenBitwiseSeriesTask.IsEven(14), "14 is even");
+					Assert.AreEqual(false, IsEvenBitwiseSeriesTask.IsEven(15), "15 is odd");
+					Assert.AreEqual(true, IsEvenBitwiseSeriesTask.IsEven(26), "26 is even");
+					Assert.AreEqual(false, IsEvenBitwiseSeriesTask.IsEven(27), "27 is odd");
+				}
+
+				[Test]
 				public void QuarksTest()
 				{
 					QuarksTask q1 = new QuarksTask("red", "up");
