25215af5eb2fc038cf1980910057ad3c7e9fdf49
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-24 19:25:34 +0200

    [~] Task: The Poet And The Pendulum

---
+++ CodeWars7kyu/ThePoetAndThePendulumTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ThePoetAndThePendulumTask
+	{
+		public static int[] Pendulum(int[] values)
+		{
+			var sortedValues = values.OrderBy(x => x).ToArray();
+
+			int n = sortedValues.Length;
+			int[] result = new int[n];
+
+			int centerIndex = (n - 1) / 2;
+
+			result[centerIndex] = sortedValues[0];
+
+			int leftIndex = centerIndex - 1;
+			int rightIndex = centerIndex + 1;
+
+			for (int i = 1; i < n; i++)
+			{
+				if (i % 2 == 1)
+				{
+					result[rightIndex] = sortedValues[i];
+					rightIndex++;
+				}
+				else
+				{
+					result[leftIndex] = sortedValues[i];
+					leftIndex--;
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/5bd776533a7e2720c40000e5/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -19,6 +19,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ThePoetAndThePendulumTest()
+				{
+					Assert.AreEqual(new[] { 10, 4, 9 }, ThePoetAndThePendulumTask.Pendulum(new[] { 4, 10, 9 }));
+					Assert.AreEqual(new[] { 8, 3, 7, 10 }, ThePoetAndThePendulumTask.Pendulum(new[] { 8, 7, 10, 3 }));
+					Assert.AreEqual(new[] { 10, 6, 5, 6, 8 }, ThePoetAndThePendulumTask.Pendulum(new[] { 6, 6, 8, 5, 10 }));
+					Assert.AreEqual(new[] { 9, 5, 4, 4, 6, 10 }, ThePoetAndThePendulumTask.Pendulum(new[] { 9, 4, 6, 4, 10, 5 }));
+					Assert.AreEqual(new[] { 8, 6, 4, 5, 7 }, ThePoetAndThePendulumTask.Pendulum(new[] { 4, 6, 8, 7, 5 }));
+					Assert.AreEqual(new[] { 10, 5, 6, 10 }, ThePoetAndThePendulumTask.Pendulum(new[] { 10, 5, 6, 10 }));
+					Assert.AreEqual(new[] { 12, 11, 12 }, ThePoetAndThePendulumTask.Pendulum(new[] { 11, 12, 12 }));
+				}
+
+				[Test]
 				public void JavascriptFilterTest()
 				{
 					string[][] a = { new[] { "foo_", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
