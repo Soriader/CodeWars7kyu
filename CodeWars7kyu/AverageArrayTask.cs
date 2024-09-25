dc1854c1fb5ade88c2a4e08c1e7967845e400b88
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-25 12:35:49 +0200

    [~] Task: Average Array

---
+++ CodeWars7kyu/AverageArrayTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class AverageArrayTask
+	{
+		public static double[] AvgArray(double[][] arr)
+		{
+			int length = arr[0].Length; 
+			double[] result = new double[length];
+
+			for (int i = 0; i < length; i++)
+			{
+				double sum = 0;
+				for (int j = 0; j < arr.Length; j++)
+				{
+					sum += arr[j][i];
+				}
+				result[i] = sum / arr.Length;
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/596f6385e7cd727fff0000d6/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void AverageArrayTest()
+				{
+					Assert.AreEqual(new double[] { 3, 4, 5, 6 }, AverageArrayTask.AvgArray(new double[][] { new double[] { 1, 2, 3, 4 }, new double[] { 5, 6, 7, 8 } }));
+					Assert.AreEqual(new double[] { 22.5, 11, 38.75, 38.25, 19.5 }, AverageArrayTask.AvgArray(new double[][] { new double[] { 2, 3, 9, 10, 7 }, new double[] { 12, 6, 89, 45, 3 }, new double[] { 9, 12, 56, 10, 34 }, new double[] { 67, 23, 1, 88, 34 } }));
+				}
+
+				[Test]
 				public void RockPaperScissorsLizardSpockTest()
                 { 
 					Assert.AreEqual(Ordering.LT, RockPaperScissorsLizardSpockTask.Rpsls(Value.Rock, Value.Lizard));
