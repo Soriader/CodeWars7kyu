5ea33f22841092ebabb0bc6396b6ee769726c4ba
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-29 11:12:11 +0200

    [~] Task: From A to Z

---
+++ CodeWars7kyu/FromAToZTask.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FromAToZTask
+	{
+		public static string GimmeTheLetters(string sp)
+		{
+			bool isUpper = false;
+
+			if (char.IsUpper(sp.ToCharArray()[0]))
+			{
+				isUpper = true;
+			}
+
+			var alphabet = "abcdefghijklmnopqrstuvwxyz";
+			var checkTheAlphabet = alphabet.ToCharArray();
+			var range = sp.ToLower().ToCharArray();
+			var start = range[0];
+			var end = range[2];
+			string result = "";
+			int indexToStart = 0;
+			int indexToEnd = 0;
+
+			for (var i = 0; i < checkTheAlphabet.Length; i++) 
+			{
+				if (checkTheAlphabet[i] == start)
+				{
+					indexToStart = i;
+				}
+
+				if (checkTheAlphabet[i] == end)
+				{
+					indexToEnd = i;
+				}
+			}
+
+			for (int i = indexToStart; i <= checkTheAlphabet.Length; i++)
+			{
+				result += checkTheAlphabet[i];
+
+				if(i == indexToEnd)
+				{
+					break;
+				}
+			}
+
+			if (isUpper)
+			{
+				return result.ToUpper();
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/6512b3775bf8500baea77663/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,25 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FromAToZTest()
+				{
+					Assert.AreEqual("J", FromAToZTask.GimmeTheLetters("J-J"));
+					Assert.AreEqual("Z", FromAToZTask.GimmeTheLetters("Z-Z"));
+					Assert.AreEqual("a", FromAToZTask.GimmeTheLetters("a-a"));
+					Assert.AreEqual("ab", FromAToZTask.GimmeTheLetters("a-b"));
+					Assert.AreEqual("yz", FromAToZTask.GimmeTheLetters("y-z"));
+					Assert.AreEqual("HI", FromAToZTask.GimmeTheLetters("H-I"));
+					Assert.AreEqual("QRSTUVWXYZ", FromAToZTask.GimmeTheLetters("Q-Z"));
+					Assert.AreEqual("FGHIJKLMNO", FromAToZTask.GimmeTheLetters("F-O"));
+					Assert.AreEqual("CDEFGHIJKLMNOPQR", FromAToZTask.GimmeTheLetters("C-R"));
+					Assert.AreEqual("hijklmno", FromAToZTask.GimmeTheLetters("h-o"));
+					Assert.AreEqual("efghijk", FromAToZTask.GimmeTheLetters("e-k"));
+					Assert.AreEqual("abcdefghijklmnopq", FromAToZTask.GimmeTheLetters("a-q"));
+					Assert.AreEqual("abcdefghijklmnopqrstuvwxyz", FromAToZTask.GimmeTheLetters("a-z"));
+					Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ", FromAToZTask.GimmeTheLetters("A-Z"));
+				}
+
+				[Test]
 				public void SPoNgEbObMeMeTest()
 				{
 					Assert.AreEqual("StOp mAkInG SpOnGeBoB MeMeS!", SPoNgEbObMeMeTask.SpongeMeme("stop Making spongebob Memes!"));
