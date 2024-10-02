d07730f4e5da88364f92a7e342b4af581778e57c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-02 11:40:46 +0200

    [~] Task: Integer Difference

---
+++ CodeWars7kyu/IntegerDifferenceTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class IntegerDifferenceTask
+	{
+		public static int IntDiff(int[] arr, int n)
+		{
+			int result = 0;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					if (Math.Abs(arr[i] - arr[j]) == n)
+					{
+						result++;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/57741d8f10a0a66915000001/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void IntegerDifferenceTest()
+				{
+					Assert.AreEqual(3, IntegerDifferenceTask.IntDiff(new int[] { 1, 1, 5, 6, 9, 16, 27 }, 4));
+					Assert.AreEqual(4, IntegerDifferenceTask.IntDiff(new int[] { 1, 1, 3, 3 }, 2));
+				}
+
+				[Test]
 				public void FromAToZTest()
 				{
 					Assert.AreEqual("J", FromAToZTask.GimmeTheLetters("J-J"));
