3039245a6ba51590c254e50143854d91d5fcd721
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-03 12:38:01 +0200

    [~] Task: String prefix and suffix

---
+++ CodeWars7kyu/StringPrefixAndSuffixTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class StringPrefixAndSuffixTask
+	{
+		public static int Solve(string s)
+		{
+			int n = s.Length;
+
+			for (int i = n / 2; i > 0; i--)
+			{
+				if (s.Substring(0, i) == s.Substring(n - i, i))
+				{
+					return i;
+				}
+			}
+
+			return 0;
+		}
+	}
+}
+//https://www.codewars.com/kata/5ce969ab07d4b7002dcaa7a1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,24 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void StringPrefixAndSuffixTest()
+				{
+					Assert.AreEqual(0, StringPrefixAndSuffixTask.Solve("abcd"));
+					Assert.AreEqual(1, StringPrefixAndSuffixTask.Solve("abcda"));
+					Assert.AreEqual(3, StringPrefixAndSuffixTask.Solve("abcdabc"));
+					Assert.AreEqual(3, StringPrefixAndSuffixTask.Solve("abcabc"));
+					Assert.AreEqual(1, StringPrefixAndSuffixTask.Solve("abcabca"));
+					Assert.AreEqual(0, StringPrefixAndSuffixTask.Solve("abcdabcc"));
+					Assert.AreEqual(2, StringPrefixAndSuffixTask.Solve("aaaaa"));
+					Assert.AreEqual(2, StringPrefixAndSuffixTask.Solve("aaaa"));
+					Assert.AreEqual(1, StringPrefixAndSuffixTask.Solve("aaa"));
+					Assert.AreEqual(1, StringPrefixAndSuffixTask.Solve("aa"));
+					Assert.AreEqual(0, StringPrefixAndSuffixTask.Solve("a"));
+					Assert.AreEqual(0, StringPrefixAndSuffixTask.Solve("acbacc"));
+
+				}
+
+				[Test]
 				public void ShadesOfGreyTest()
 				{
 					Assert.AreEqual(254, ShadesOfGreyTask.ShadesOfGrey(255).Length);
