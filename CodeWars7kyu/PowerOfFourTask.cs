59996af6bfa3f50f2ea2c72b6232be54c6beaf70
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-04 11:36:48 +0200

    [~] Task: Power of 4

---
+++ CodeWars7kyu/PowerOfFourTask.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PowerOfFourTask
+	{
+		public static bool PowerOf4(object n)
+		{
+			if (n is int number && number > 0)
+			{
+				return (number & (number - 1)) == 0 && (number - 1) % 3 == 0;
+			}
+			return false;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/544d114f84e41094a9000439/train/csharp
+
+//First result but the process is too long for this task
+
+//public static bool PowerOf4(object n)
+//{
+//	if (n is string || n is null)
+//	{
+//		return false;
+//	}
+
+//	double checkTheNumber = 0;
+//	var parseTheNumber = double.TryParse(n.ToString(), out checkTheNumber);
+
+//	if (!parseTheNumber)
+//	{
+//		return false;
+//	}
+
+//	if (checkTheNumber == 1)
+//	{
+//		return true;
+//	}
+
+//	double power = 1;
+
+//	while (true)
+//	{
+//		if (Math.Pow(4, power) == checkTheNumber && checkTheNumber != 0)
+//		{
+//			return true;
+//		}
+//		else if (Math.Pow(4, power) > checkTheNumber)
+//		{
+//			return false;
+//		}
+
+//		power++;
+//	}
+
+//}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,21 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void PowerOfFourTest()
+				{
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(0));
+					Assert.AreEqual(true, PowerOfFourTask.PowerOf4(4));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(72));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(20));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(12));
+					Assert.AreEqual(true, PowerOfFourTask.PowerOf4(16));
+					Assert.AreEqual(true, PowerOfFourTask.PowerOf4(1));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(3.1415));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4("4"));
+					Assert.AreEqual(false, PowerOfFourTask.PowerOf4(null));
+				}
+
+				[Test]
 				public void StringPrefixAndSuffixTest()
 				{
 					Assert.AreEqual(0, StringPrefixAndSuffixTask.Solve("abcd"));
