d55f64e837e059332098d8e58d9e777f6117ed04
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-04 12:19:35 +0200

    [~] Task: Invalid Input - Error Handling #1

---
+++ CodeWars7kyu/InvalidInputErrorHandlingTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class InvalidInputErrorHandlingTask
+	{
+		public int Vowels { get; set; }
+		public int Consonants { get; set; }
+
+		public InvalidInputErrorHandlingTask(int vowels, int consonants)
+		{
+			Vowels = vowels;
+			Consonants = consonants;
+		}
+
+		public static InvalidInputErrorHandlingTask GetCount(object word)
+		{
+			if (word == null || !(word is string))
+			{
+				return new InvalidInputErrorHandlingTask(0, 0);
+			}
+
+			string input = word.ToString().ToLower();
+			string vowelsForCheck = "aeiou";
+
+			int vowelsCount = 0;
+			int consonantsCount = 0;
+
+			foreach (char c in input)
+			{
+				if (char.IsLetter(c))
+				{
+					if (vowelsForCheck.Contains(c))
+					{
+						vowelsCount++;
+					}
+					else
+					{
+						consonantsCount++;
+					}
+				}
+			}
+
+			return new InvalidInputErrorHandlingTask(vowelsCount, consonantsCount);
+		}
+	}
+	
+}
+//https://www.codewars.com/kata/55e6125ad777b540d9000042/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,30 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void FixedTest()
+				{
+					InvalidInputErrorHandlingTask re = InvalidInputErrorHandlingTask.GetCount("Test");
+					Assert.AreEqual(1, re.Vowels);
+					Assert.AreEqual(3, re.Consonants);
+
+					re = InvalidInputErrorHandlingTask.GetCount("Here is some text!");
+					Assert.AreEqual(6, re.Vowels);
+					Assert.AreEqual(8, re.Consonants);
+
+					re = InvalidInputErrorHandlingTask.GetCount("To be a Codewarrior or not to be");
+					Assert.AreEqual(12, re.Vowels);
+					Assert.AreEqual(13, re.Consonants);
+
+					re = InvalidInputErrorHandlingTask.GetCount("To Kata or not to Kata");
+					Assert.AreEqual(8, re.Vowels);
+					Assert.AreEqual(9, re.Consonants);
+
+					re = InvalidInputErrorHandlingTask.GetCount("aeiou");
+					Assert.AreEqual(5, re.Vowels);
+					Assert.AreEqual(0, re.Consonants);
+				}
+
+				[Test]
 				public void CountSalutesTest()
 				{
 					static void Act(string hallway, int expected)
