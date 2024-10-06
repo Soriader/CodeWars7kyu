0d885e6d5905b24dd25f20df7a625340508680bd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-06 16:09:30 +0200

    [~] Task: Fun with ES6 Classes #2 - Animals and Inheritance

---
+++ CodeWars7kyu/AnimalsAndInheritanceTask.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeWars7kyu
+{
+	public class AnimalsAndInheritanceTask
+	{
+		public int Age;
+		public int Legs;
+		public string Name;
+		public string Species;
+		public string Status;
+
+		public AnimalsAndInheritanceTask(string name, int age, int legs, string species, string status)
+		{
+			this.Name = name;
+			this.Age = age;
+			this.Legs = legs;
+			this.Species = species;
+			this.Status = status;
+		}
+
+		public virtual string Introduce()
+		{
+			return $"Hello, my name is {this.Name} and I am {this.Age} years old.";
+		}
+		
+	}
+
+	public class Shark : AnimalsAndInheritanceTask
+	{
+		public Shark(string name, int age, string status) : base(name, age, 0, "shark", status)
+		{
+		}
+	}
+
+	public class Cat : AnimalsAndInheritanceTask
+	{
+		public Cat(string name, int age, string status) : base(name, age, 4, "cat", status)
+		{
+		}
+
+		public override string Introduce()
+		{
+			return base.Introduce() + "  Meow meow!";
+		}
+	}
+
+	public class Dog : AnimalsAndInheritanceTask
+	{
+		public string Master { get; private set; }
+
+		public Dog(string name, int age, string status, string master)
+			: base(name, age, 4, "dog", status)
+		{
+			this.Master = master;
+		}
+
+		public string GreetMaster()
+		{
+			return $"Hello {this.Master}";
+		}
+	}
+}
+//https://www.codewars.com/kata/56f935002e6c0d55fa000d92/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void AnimalsAndInheritanceTest()
+				{
+					Dog doug = new Dog("Doug", 12, "Serving his master", "Eliza");
+					Assert.AreEqual("Doug", doug.Name);
+					Assert.AreEqual(12, doug.Age);
+					Assert.AreEqual(4, doug.Legs);
+					Assert.AreEqual("dog", doug.Species);
+					Assert.AreEqual("Serving his master", doug.Status);
+					Assert.AreEqual("Hello Eliza", doug.GreetMaster());
+				}
+
+				[Test]
 				public void StringReverseSlicing101Test()
 				{
 					Assert.AreEqual(new string[] { "321", "21", "1" }, StringReverseSlicing101Task.ReverseSlice("123"));
