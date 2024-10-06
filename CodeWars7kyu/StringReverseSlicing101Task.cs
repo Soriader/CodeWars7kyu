46b9c8cb57c4f958213eff00d4ad0fec9a993959
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-06 15:52:24 +0200

    [~] Task: String reverse slicing 101

---
+++ CodeWars7kyu/StringReverseSlicing101Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class StringReverseSlicing101Task
+    {
+		public static string[] ReverseSlice(string str)
+		{
+			var stringToAdded = new string(str.Reverse().ToArray());
+			int lenght = stringToAdded.Length;
+			int iterator = 0;
+			List<string> result = new List<string>();
+
+			while (iterator != lenght)
+			{
+				result.Add(stringToAdded);
+				stringToAdded = stringToAdded.Remove(0, 1);
+				iterator++;
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/586efc2dcf7be0f217000619/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,13 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void StringReverseSlicing101Test()
+				{
+					Assert.AreEqual(new string[] { "321", "21", "1" }, StringReverseSlicing101Task.ReverseSlice("123"));
+					Assert.AreEqual(new string[] { "edcba", "dcba", "cba", "ba", "a" }, StringReverseSlicing101Task.ReverseSlice("abcde"));
+				}
+
+				[Test]
 				public static void FixedTest()
 				{
 					InvalidInputErrorHandlingTask re = InvalidInputErrorHandlingTask.GetCount("Test");
