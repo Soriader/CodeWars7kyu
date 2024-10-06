990195deb3499da2ad20ca68858df7f080d4c54c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-06 16:26:46 +0200

    [~] Task: Cartesian neighbors

---
+++ CodeWars7kyu/CartesianNeighborsTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CartesianNeighborsTask
+	{
+		public static IEnumerable<int[]> CartesianNeighbor(int x, int y)
+		{
+			List<int[]> result = new List<int[]>();
+
+			result.Add(new int[] { x - 1, y - 1 });
+			result.Add(new int[] { x - 1, y });
+			result.Add(new int[] { x - 1, y + 1 });
+			result.Add(new int[] { x, y - 1 });
+			result.Add(new int[] { x, y + 1 });
+			result.Add(new int[] { x + 1, y - 1 });
+			result.Add(new int[] { x + 1, y });
+			result.Add(new int[] { x + 1, y + 1 });
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/58989a079c70093f3e00008d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,22 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void CartesianNeighborsTest()
+				{
+					var r = new List<int[]>{
+	  new int[]{1,1},
+	  new int[]{1,2},
+	  new int[]{1,3},
+	  new int[]{2,1},
+	  new int[]{2,3},
+	  new int[]{3,1},
+	  new int[]{3,2},
+	  new int[]{3,3},
+	};
+					Assert.AreEqual(r, CartesianNeighborsTask.CartesianNeighbor(2, 2));
+				}
+
+				[Test]
 				public void AnimalsAndInheritanceTest()
 				{
 					Dog doug = new Dog("Doug", 12, "Serving his master", "Eliza");
