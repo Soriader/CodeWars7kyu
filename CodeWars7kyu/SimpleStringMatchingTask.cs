8b30c37da9c862e98264ebebba1711a1cd5e8002
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-06 17:26:56 +0200

    [~] Task: Simple string matching

---
+++ CodeWars7kyu/SimpleStringMatchingTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class SimpleStringMatchingTask
+	{
+		public static bool Solve(string a, string b)
+		{
+			if (!a.Contains("*"))
+			{
+				return a == b;
+			}
+
+			int starIndex = a.IndexOf('*');
+
+			string prefix = a.Substring(0, starIndex);
+			string suffix = a.Substring(starIndex + 1);
+
+			if (b.Length < prefix.Length + suffix.Length)
+			{
+				return false;
+			}
+
+			return b.StartsWith(prefix) && b.EndsWith(suffix);
+		}
+	}
+
+}
+//https://www.codewars.com/kata/5bc052f93f43de7054000188/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SimpleStringMatchingTest()
+				{
+					Assert.AreEqual(true, SimpleStringMatchingTask.Solve("code*s", "codewars"));
+					Assert.AreEqual(true, SimpleStringMatchingTask.Solve("codewar*s", "codewars"));
+					Assert.AreEqual(true, SimpleStringMatchingTask.Solve("*c", "c"));
+					Assert.AreEqual(true, SimpleStringMatchingTask.Solve("*", "asterisk"));
+					Assert.AreEqual(false, SimpleStringMatchingTask.Solve("d*oll", "dual"));
+					Assert.AreEqual(false, SimpleStringMatchingTask.Solve("*osd", "asterisk"));
+					Assert.AreEqual(false, SimpleStringMatchingTask.Solve("*ea", "bean"));
+				}
+
+				[Test]
 				public void CartesianNeighborsTest()
 				{
 					var r = new List<int[]>{
