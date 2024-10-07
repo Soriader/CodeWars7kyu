df76986ca74a34fafd0e9945208b1a7fafb17c74
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-07 19:08:21 +0200

    [~] Task: Are there doubles?

---
+++ CodeWars7kyu/AreThereDoublesTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class AreThereDoublesTask
+    {
+		public static bool DoubleCheck(string strng) 
+		{
+			strng = strng.ToLower();
+
+			for (int i = 0; i < strng.Length - 1; i++) 
+			{
+				if (strng[i] == strng[i + 1])
+				{
+					return true;
+				}
+			}
+			return false;	
+		}
+		
+	}
+}
+//https://www.codewars.com/kata/56a24b4d9f3671584d000039/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,19 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void AreThereDoublesTest()
+				{
+					Assert.AreEqual(false, AreThereDoublesTask.DoubleCheck("abca"));
+					Assert.AreEqual(true, AreThereDoublesTask.DoubleCheck("aabc"));
+					Assert.AreEqual(true, AreThereDoublesTask.DoubleCheck("a 11 c d"));
+					Assert.AreEqual(true, AreThereDoublesTask.DoubleCheck("AabBcC"));
+					Assert.AreEqual(true, AreThereDoublesTask.DoubleCheck("a b  c"));
+					Assert.AreEqual(false, AreThereDoublesTask.DoubleCheck("a b c d e f g h i h k"));
+					Assert.AreEqual(false, AreThereDoublesTask.DoubleCheck("2020"));
+					Assert.AreEqual(false, AreThereDoublesTask.DoubleCheck("a!@€?#$%^&*()_-+=}]{[|\':;?/>.<,~"));
+				}
+
+				[Test]
 				public void SillyCASETest()
 				{
 					Assert.That(SillyCASETask.SillyCase("foobar"), Is.EqualTo("fooBAR"));
