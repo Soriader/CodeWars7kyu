bee834708090c792b42aa28d01210d69784695ac
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-08 19:07:25 +0200

    [~] Task: Shared Bit Counter

---
+++ CodeWars7kyu/SharedBitCounterTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+    public class SharedBitCounterTask
+    {
+		public static bool SharedBits(int a, int b)
+		{
+			string aToBit = Convert.ToString(a, 2).PadLeft(32, '0');
+			string bToBit = Convert.ToString(b, 2).PadLeft(32, '0');
+
+			int iterator = 0;
+
+			for (int i = 0; i < aToBit.Length; i++)
+			{
+				if (aToBit[i] == '1' && bToBit[i] == '1')
+				{
+					iterator++;
+				}
+			}
+
+			return iterator >= 2;
+		}
+	}
+}
+//https://www.codewars.com/kata/58a5aeb893b79949eb0000f1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,20 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SharedBitCounterTest()
+				{
+					Assert.AreEqual(true, SharedBitCounterTask.SharedBits(7, 15));
+					Assert.AreEqual(true, SharedBitCounterTask.SharedBits(23, 7));
+					Assert.AreEqual(true, SharedBitCounterTask.SharedBits(43, 77));
+					Assert.AreEqual(false, SharedBitCounterTask.SharedBits(7, 10));
+					Assert.AreEqual(false, SharedBitCounterTask.SharedBits(2, 3));
+					Assert.AreEqual(false, SharedBitCounterTask.SharedBits(1, 1));
+					Assert.AreEqual(false, SharedBitCounterTask.SharedBits(16, 8));
+					Assert.AreEqual(false, SharedBitCounterTask.SharedBits(1, 2));
+
+				}
+
+				[Test]
 				public void AreThereDoublesTest()
 				{
 					Assert.AreEqual(false, AreThereDoublesTask.DoubleCheck("abca"));
