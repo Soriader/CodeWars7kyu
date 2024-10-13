642dd6704ce150ef65e6cc3cb6bb0bb35916eb1a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-13 18:55:55 +0200

    [~] Task: Vampire Numbers

---
+++ CodeWars7kyu/VampireNumbersTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class VampireNumbersTask
+	{
+		public static bool Vampire(long x, long y)
+		{
+			if (x <= 0 && y <= 0)
+			{
+				return false;
+			}
+
+			var vampireNumber = x * y;
+
+			if(vampireNumber.ToString().Length == x.ToString().Length + y.ToString().Length)
+			{
+				var fangsDigits = (x.ToString() + y.ToString()).OrderBy(c => c).ToArray();
+				var vampireDigits = vampireNumber.ToString().OrderBy(c => c).ToArray();
+
+				return fangsDigits.SequenceEqual(vampireDigits);
+			}
+
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/54d418bd099d650fa000032d/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void VampireNumbersTest()
+				{
+					Assert.AreEqual(true, VampireNumbersTask.Vampire(21, 6), "Basic: 21 * 6 = 126 should return True");
+					Assert.AreEqual(true, VampireNumbersTask.Vampire(30, -51), "One Negative: 30 * -51 = -1530 should return True");
+					Assert.AreEqual(false, VampireNumbersTask.Vampire(-246, -510), "Double Negatives: -246 * -510 = 125460 should return False (The negative signs aren't present on the product)");
+					Assert.AreEqual(true, VampireNumbersTask.Vampire(204, 615), "Basic: 204 * 615 = 125460 should return True");
+					Assert.AreEqual(true, VampireNumbersTask.Vampire(204, 615), "Basic: 204 * 615 = 125460 should return True");
+
+				}
+
+				[Test]
 				public void SharedBitCounterTest()
 				{
 					Assert.AreEqual(true, SharedBitCounterTask.SharedBits(7, 15));
