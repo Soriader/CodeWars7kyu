86662155ced10634baad18efb478c42d619c7511
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-23 11:36:43 +0200

    [~] Task: CompoundArray

---
+++ CodeWars7kyu/CompoundArrayTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class CompoundArrayTask
+	{
+		public static int[] CompoundArray(int[] a, int[] b)
+		{
+			List<int> result = new List<int>();
+			int iteratorA = 0;
+			int iteratorB = 0;
+			bool sequence = true;
+
+			while (iteratorA < a.Length && iteratorB < b.Length)
+			{
+				if (sequence)
+				{
+					result.Add(a[iteratorA]);
+					iteratorA++;
+					sequence = false;
+				}
+				else
+				{
+					result.Add(b[iteratorB]);
+					iteratorB++;
+					sequence = true;
+				}
+			}
+
+			while (iteratorA < a.Length)
+			{
+				result.Add(a[iteratorA]);
+				iteratorA++;
+			}
+
+			while (iteratorB < b.Length)
+			{
+				result.Add(b[iteratorB]);
+				iteratorB++;
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/56044de2aa75e28875000017/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,17 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void CompoundArrayTest()
+				{
+					Assert.AreEqual(new int[] { 1, 9, 2, 8, 3, 7, 4, 6, 5, 6 },
+					  CompoundArrayTask.CompoundArray(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 9, 8, 7, 6 }));
+					Assert.AreEqual(new int[] { 0, 9, 1, 8, 2, 7, 6, 5, 4, 3, 2, 1, 0 },
+					  CompoundArrayTask.CompoundArray(new int[] { 0, 1, 2 }, new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }));
+					Assert.AreEqual(new int[] { 11, 21, 12, 22, 23, 24 },
+					  CompoundArrayTask.CompoundArray(new int[] { 11, 12 }, new int[] { 21, 22, 23, 24 }));
+				}
+
+				[Test]
 				public void BabySharkLyricsGeneratorTest()
 				{
 					Assert.AreEqual(string.Join("\n", new string[] {
