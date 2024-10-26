2cea7bf395d58b09456d7960ad0e6cbb168f7063
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-26 19:15:47 +0200

    [~] Task: Katastrophe!

---
+++ CodeWars7kyu/KatastropheTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class KatastropheTask
+	{
+		public static string StrongEnough(int[][] earthquake, int age)
+		{
+			double buildingPower = 1000 * Math.Pow(0.99, age); 
+			var earthquakePower = EarthquakePower(earthquake);
+
+			if(buildingPower >= earthquakePower)
+			{
+				return "Safe!";
+			}
+
+
+			return "Needs Reinforcement!";
+		}
+
+		static int EarthquakePower(int[][] earthquake)
+		{
+			int earthquakePower = 1;
+
+			foreach (var innerArray in earthquake)
+			{
+				int sum = 0; 
+
+				foreach (var number in innerArray)
+				{
+					sum += number;
+				}
+
+				earthquakePower *= sum;
+			}
+
+			return earthquakePower; 
+		}
+	}
+}
+//https://www.codewars.com/kata/55a3cb91d1c9ecaa2900001b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void KatastropheTest()
+				{
+					Assert.AreEqual("Safe!", KatastropheTask.StrongEnough(new int[][] { new int[] { 2, 3, 1 }, new int[] { 3, 1, 1, }, new int[] { 1, 1, 2 } }, 2));
+					Assert.AreEqual("Safe!", KatastropheTask.StrongEnough(new int[][] { new int[] { 5, 8, 7 }, new int[] { 3, 3, 1, }, new int[] { 4, 1, 2 } }, 2));
+					Assert.AreEqual("Needs Reinforcement!", KatastropheTask.StrongEnough(new int[][] { new int[] { 5, 8, 7 }, new int[] { 3, 3, 1, }, new int[] { 4, 1, 2 } }, 3));
+				}
+
+				[Test]
 				public void BorrowerSpeakTest()
 				{
 					Assert.AreEqual("whatfickdamncake", BorrowerSpeakTask.Borrow("WhAt! FiCK! DaMn CAke?"));
