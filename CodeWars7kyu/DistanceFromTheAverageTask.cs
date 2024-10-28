54cfca2e004e5b458b5fff78d7ce67ceee2e2b0f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-28 19:09:34 +0100

    [~] Task: Distance from the average

---
+++ CodeWars7kyu/DistanceFromTheAverageTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class DistanceFromTheAverageTask
+	{
+		public static double[] DistancesFromAverage(int[] input)
+		{
+			double average = input.Average();
+			List<double> result = new List<double>();
+
+			foreach (int i in input) 
+			{
+				var check = average - i;
+				result.Add(Math.Round(check, 2));
+			}
+
+			return result.ToArray();	
+		}
+	}
+}
+//https://www.codewars.com/kata/568ff914fc7a40a18500005c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void DistanceFromTheAverageTest()
+				{
+					Assert.AreEqual(new[] { 4.2, -35.8, -2.8, 23.2, 11.2 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 55, 95, 62, 36, 48 }));
+					Assert.AreEqual(new[] { 0, 0, 0, 0, 0 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 1, 1, 1, 1, 1 }));
+					Assert.AreEqual(new[] { -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 1, -1, 1, -1, 1, -1 }));
+					Assert.AreEqual(new[] { -0.8, 1.2, -0.8, 1.2, -0.8 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 1, -1, 1, -1, 1 }));
+					Assert.AreEqual(new[] { -2.0, 2.0 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 2, -2 }));
+					Assert.AreEqual(new[] { 0 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 1 }));
+					Assert.AreEqual(new[] { 6197.6, 6385.6, -26111.4, 6673.6, 6854.6 }, DistanceFromTheAverageTask.DistancesFromAverage(new[] { 123, -65, 32432, -353, -534 }));
+				}
+
+				[Test]
 				public void GreenGlassDoorTest()
 				{
 					Assert.AreEqual(true, GreenGlassDoorTask.StepThroughWith("moon"));
