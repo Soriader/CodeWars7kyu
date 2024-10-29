120512807e914dd6222f621ece5f3e518f756168
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-29 19:27:30 +0100

    [~] Task: Find missing numbers

---
+++ CodeWars7kyu/FindMissingNumbersTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FindMissingNumbersTask
+	{
+		public static int[] FindMissingNumbers(int[] arr)
+		{
+			if (arr.Length == 0)
+			{
+				return new int[0];
+			}
+
+			List<int> result = new List<int>();
+
+			var uniqueNumbers = new HashSet<int>(arr);
+
+			int min = arr.Min();
+			int max = arr.Max();
+
+			for (int i = min; i <= max; i++)
+			{
+				if (!uniqueNumbers.Contains(i))
+				{
+					result.Add(i);
+				}
+			}
+
+			return result.ToArray();
+
+		}
+	}
+}
+//https://www.codewars.com/kata/56d02e6cc6c8b49c510005bb/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FindMissingNumbersTest()
+				{
+					Assert.AreEqual(new int[] { -1, 0, 2, 3 }, FindMissingNumbersTask.FindMissingNumbers(new int[] { -3, -2, 1, 4 }));
+					Assert.AreEqual(new int[0], FindMissingNumbersTask.FindMissingNumbers(new int[] { -1, 0, 1, 2, 3, 4 }));
+					Assert.AreEqual(new int[0], FindMissingNumbersTask.FindMissingNumbers(new int[0]));
+				}
+
+				[Test]
 				public void NaughtyOrNiceTest()
 				{
 					var naughty = new List<NaughtyOrNiceTask> {
