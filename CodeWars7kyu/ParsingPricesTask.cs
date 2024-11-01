1eba966d1c7caa6040965e881b2dc8ab89368975
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-01 16:24:02 +0100

    [~] Task: Regexp basics - parsing prices

---
+++ CodeWars7kyu/ParsingPricesTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ParsingPricesTask
+	{
+		public static int? ToCents(string price)
+		{
+			Match m = Regex.Match(price, @"\$\d+\.\d{2}");
+			return m.Success && m.Value == price ? (int)(decimal.Parse(m.Value.Substring(1)) * 100) : (int?)null;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/56833b76371e86f8b6000015/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,24 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ParsingPricesTest()
+				{
+					Assert.AreEqual(null, ParsingPricesTask.ToCents(""));
+					Assert.AreEqual(null, ParsingPricesTask.ToCents("1"));
+					Assert.AreEqual(null, ParsingPricesTask.ToCents("1.23"));
+					Assert.AreEqual(null, ParsingPricesTask.ToCents("$1"));
+					Assert.AreEqual(123, ParsingPricesTask.ToCents("$1.23"));
+					Assert.AreEqual(9999, ParsingPricesTask.ToCents("$99.99"));
+					Assert.AreEqual(1234567890, ParsingPricesTask.ToCents("$12345678.90"));
+					Assert.AreEqual(969, ParsingPricesTask.ToCents("$9.69"));
+					Assert.AreEqual(970, ParsingPricesTask.ToCents("$9.70"));
+					Assert.AreEqual(971, ParsingPricesTask.ToCents("$9.71"));
+					Assert.AreEqual(69, ParsingPricesTask.ToCents("$0.69"));
+					Assert.AreEqual(null, ParsingPricesTask.ToCents("$9.69$4.3.7"));
+					Assert.AreEqual(null, ParsingPricesTask.ToCents("$9.692"));
+				}
+
+				[Test]
 				public void OnesComplementTest()
 				{
 					Assert.AreEqual("1", OnesComplementTask.OnesComplement("0"));
