bb2eb4545e6688463e4541b0c482c0c94dae8448
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-01 12:56:51 +0100

    [~] Task: Ones' Complement

---
+++ CodeWars7kyu/OnesComplementTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class OnesComplementTask
+	{
+		public static string OnesComplement(string n)
+		{
+			return new string(n.Select(x => x == '0' ? '1' : (x == '1' ? '0' : x)).ToArray());
+
+		}
+	}
+}
+//https://www.codewars.com/kata/59b11f57f322e5da45000254/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void OnesComplementTest()
+				{
+					Assert.AreEqual("1", OnesComplementTask.OnesComplement("0"));
+					Assert.AreEqual("0", OnesComplementTask.OnesComplement("1"));
+					Assert.AreEqual("01", OnesComplementTask.OnesComplement("10"));
+					Assert.AreEqual("10", OnesComplementTask.OnesComplement("01"));
+					Assert.AreEqual("0010", OnesComplementTask.OnesComplement("1101"));
+				}
+
+				[Test]
 				public static void BuildingSpheresTest()
 				{
 					BuildingSpheresTask ball = new BuildingSpheresTask(2, 50);
