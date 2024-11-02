d9733944e4bb3ae1827d9fd18f65b6794db2337c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-02 14:22:14 +0100

    [~] Task: Hungarian Vowel Harmony (easy)

---
+++ CodeWars7kyu/HungarianVowelHarmonyTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class HungarianVowelHarmonyTask
+	{
+		public static string Dative(string word)
+		{
+			var front = "eéiíöőüű";
+			var back = "aáoóuú";
+
+			for (var i = word.Length - 1; i >= 0; i--)
+			{
+				if (word[i].ToString().Any(ch => front.Contains(ch)))
+				{
+					return word + "nek";
+				}
+				else if (word[i].ToString().Any(ch => back.Contains(ch)))
+				{
+					return word + "nak";
+				}
+			} 
+
+			return word;
+		}
+	}
+}
+//https://www.codewars.com/kata/57fd696e26b06857eb0011e7/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,20 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void HungarianVowelHarmonyTest()
+				{
+					Assert.AreEqual("ablaknak", HungarianVowelHarmonyTask.Dative("ablak"));
+					Assert.AreEqual("tükörnek", HungarianVowelHarmonyTask.Dative("tükör"));
+					Assert.AreEqual("keretnek", HungarianVowelHarmonyTask.Dative("keret"));
+					Assert.AreEqual("otthonnak", HungarianVowelHarmonyTask.Dative("otthon"));
+					Assert.AreEqual("virágnak", HungarianVowelHarmonyTask.Dative("virág"));
+					Assert.AreEqual("tettnek", HungarianVowelHarmonyTask.Dative("tett"));
+					Assert.AreEqual("rokkantnak", HungarianVowelHarmonyTask.Dative("rokkant"));
+					Assert.AreEqual("rossznak", HungarianVowelHarmonyTask.Dative("rossz"));
+					Assert.AreEqual("gonosznak", HungarianVowelHarmonyTask.Dative("gonosz"));
+				}
+
+				[Test]
 				public void WeirdWordsTest()
 				{
 					Assert.AreEqual("Nz Obnf Jt App", WeirdWordsTask.NextLetter("My Name Is Zoo"));
