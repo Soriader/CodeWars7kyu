77701e0622ad774d2be02e6136c220f4b4418190
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-03 18:20:03 +0100

    [~] Task: Elapsed Seconds

---
+++ CodeWars7kyu/ElapsedSecondsTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ElapsedSecondsTask
+	{
+		public static int ElapsedSeconds(DateTime startDate, DateTime endDate)
+		{
+			TimeSpan timeSpan = endDate - startDate;
+
+			return (int)timeSpan.TotalSeconds;
+		}
+	}
+}
+//https://www.codewars.com/kata/517b25a48557c200b800000c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ElapsedSecondsTest()
+				{
+					var start = new DateTime(2013, 1, 1, 0, 0, 1);
+					var end = new DateTime(2013, 1, 1, 0, 0, 2);
+					var end2 = new DateTime(2013, 1, 1, 0, 0, 20);
+					var end3 = new DateTime(2013, 1, 1, 0, 1, 20);
+					Assert.AreEqual(1, ElapsedSecondsTask.ElapsedSeconds(start, end));
+					Assert.AreEqual(19, ElapsedSecondsTask.ElapsedSeconds(start, end2));
+					Assert.AreEqual(79, ElapsedSecondsTask.ElapsedSeconds(start, end3));
+				}
+
+				[Test]
 				public void ORingArraysTest()
 				{
 					Assert.AreEqual(new[] { 1, 2, 3 }, ORingArraysTask.OrArrays(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }));
