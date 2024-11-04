7984164a2affb34fdec001969b15a23429679282
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-04 15:05:06 +0100

    [~] Task: Password Check - Binary to String

---
+++ CodeWars7kyu/PasswordCheckBinaryToStringTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PasswordCheckBinaryToStringTask
+	{
+		public static string DecodePass(string[] passArr, string bin)
+		{
+			var partOfPassword = bin.Split(' ');
+			string result = "";
+
+			foreach (var part in partOfPassword)
+			{
+				string paddedPart = part.PadLeft(8, '0');
+
+				int asciiValue = Convert.ToInt32(paddedPart, 2);
+				result += (char)asciiValue;
+			}
+
+			return passArr.Contains(result) ? result : null;
+		}
+	}
+}
+//https://www.codewars.com/kata/5a731b36e19d14400f000c19/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void PasswordCheckBinaryToStringTest()
+				{
+					Assert.AreEqual("password123",
+						PasswordCheckBinaryToStringTask.DecodePass(new string[] { "password123", "admin", "admin1" },
+							"01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011"));
+				}
+
+				[Test]
 				public void ConvertIntegerToBinaryTest()
 				{
 					Assert.AreEqual("10", ConvertIntegerToBinaryTask.ToBinary(2));
