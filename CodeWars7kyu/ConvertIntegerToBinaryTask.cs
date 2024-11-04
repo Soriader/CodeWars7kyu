91e7fbe75d09e2fa1c946ccd16e9eea9dbb722a4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-04 14:11:49 +0100

    [~] Task: Convert Integer to Binary

---
+++ CodeWars7kyu/ConvertIntegerToBinaryTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ConvertIntegerToBinaryTask
+	{
+		public static string ToBinary(int n)
+		{
+			return Convert.ToString(n, 2);
+		}
+	}
+}
+//https://www.codewars.com/kata/55606aeebf1f0305f900006f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,15 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ConvertIntegerToBinaryTest()
+				{
+					Assert.AreEqual("10", ConvertIntegerToBinaryTask.ToBinary(2));
+					Assert.AreEqual("11", ConvertIntegerToBinaryTask.ToBinary(3));
+					Assert.AreEqual("100", ConvertIntegerToBinaryTask.ToBinary(4));
+					Assert.AreEqual("101", ConvertIntegerToBinaryTask.ToBinary(5));
+				}
+
+				[Test]
 				public void DoubletonNumberTest()
 				{
 					Assert.AreEqual(121, DoubletonNumberTask.Doubleton(120));
