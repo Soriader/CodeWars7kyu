08b1ca9d1955f98b8305aa4095968594dec9bce3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-04 18:51:15 +0100

    [~] Task: Mean vs. Median

---
+++ CodeWars7kyu/MeanVsMedianTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class MeanVsMedianTask
+	{
+		public static string MeanVsMedian(int[] numbers)
+		{
+			Array.Sort(numbers);
+			double median = numbers[numbers.Length / 2];
+
+			long sum = numbers.Sum(x => (long)x);
+			double mean = (double)sum / numbers.Length;
+
+			// Porównujemy wartości
+			if (median > mean)
+			{
+				return "median";
+			}
+			else if (median < mean)
+			{
+				return "mean";
+			}
+
+			return "same";
+		}
+	}
+}
+//https://www.codewars.com/kata/5806445c3f1f9c2f72000031/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void MeanVsMedianTest()
+				{
+					Assert.AreEqual("median", MeanVsMedianTask.MeanVsMedian(new int[] { 7, 14, -70 }));
+					Assert.AreEqual("mean", MeanVsMedianTask.MeanVsMedian(new int[] { 1, 2, 37 }));
+					Assert.AreEqual("same", MeanVsMedianTask.MeanVsMedian(new int[] { 1, 1, 1 }));
+
+
+				}
+
+				[Test]
 				public void NegationOfAValueTest()
 				{
 					Assert.AreEqual(true, NegationOfAValueTask.NegationValue("!", false));
