6b65596f9a3e44388f5753fa4017030bf3ecd1bf
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-05 14:27:48 +0100

    [~] Task: Simple Fun #384: Is Turing's Equation?

---
+++ CodeWars7kyu/IsTuringEquationTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class IsTuringEquationTask
+	{
+		public static bool IsTuringEquation(string str)
+		{
+			string[] parts = str.Split('=');
+			string leftSide = parts[0];
+			string result = parts[1].TrimStart('0'); ;
+
+			string[] operands = leftSide.Split('+');
+			string first = operands[0].TrimStart('0');
+			string second = operands[1].TrimStart('0'); ; 
+
+			var newFirst = new string(first.Reverse().ToArray()).TrimStart('0');
+			var newSecond = new string(second.Reverse().ToArray()).TrimStart('0');
+			var newResult = new string(result.Reverse().ToArray()).TrimStart('0');
+
+			newFirst = string.IsNullOrEmpty(newFirst) ? "0" : newFirst;
+			newSecond = string.IsNullOrEmpty(newSecond) ? "0" : newSecond;
+			newResult = string.IsNullOrEmpty(newResult) ? "0" : newResult;
+
+			var newFirstToInt = int.Parse(newFirst);
+			var newSecondToInt = int.Parse(newSecond);
+			var newResultToInt = int.Parse(newResult);
+
+			return newFirstToInt + newSecondToInt == newResultToInt;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5a1e6323ffe75f71ae000026/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,19 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void IsTuringEquationTest()
+				{
+					Assert.That(IsTuringEquationTask.IsTuringEquation("73+42=16"), Is.EqualTo(true));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("5+8=13"), Is.EqualTo(false));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("10+20=30"), Is.EqualTo(true));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("0001000+000200=00030"), Is.EqualTo(true));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("1234+5=1239"), Is.EqualTo(false));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("1+0=0"), Is.EqualTo(false));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("7000+8000=51"), Is.EqualTo(true));
+					Assert.That(IsTuringEquationTask.IsTuringEquation("0+0=0"), Is.EqualTo(true));
+				}
+
+				[Test]
 				public void PasswordSystemTest()
 				{
 					string expected = "Yes";
