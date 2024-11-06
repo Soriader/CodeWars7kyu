dd5133ff97f17feaa8736705abd5c2127b874a13
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-06 19:34:25 +0100

    [~] Task: Turn any word into a beef taco

---
+++ CodeWars7kyu/TurnAnyWordIntoABeefTacoTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TurnAnyWordIntoABeefTacoTask
+	{
+		public static string[] Tacofy(string word)
+		{
+			List<string> taco = new List<string>() {"shell"};
+			string vovels = "aeiou";
+			var check = word.ToLower().ToCharArray();
+
+			foreach (char c in check) 
+			{
+				switch (c)
+				{
+					case 't':
+						{
+							taco.Add("tomato");
+							break;
+						}
+					case 'l':
+						{
+							taco.Add("lettuce");
+							break;
+						}
+					case 'c':
+						{
+							taco.Add("cheese");
+							break;
+						}
+					case 'g':
+						{
+							taco.Add("guacamole");
+							break;
+						}
+					case 's':
+						{
+							taco.Add("salsa");
+							break;
+						}
+					default: 
+						{
+							if (vovels.Contains(c))
+							{
+								taco.Add("beef");
+							}
+							break;
+						}
+
+				}
+			}
+
+			taco.Add("shell");
+
+			return taco.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/59414b46d040b7b8f7000021/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void BasicTest()
+				{
+					Assert.AreEqual(new string[] { "shell", "shell" }, TurnAnyWordIntoABeefTacoTask.Tacofy(""));
+					Assert.AreEqual(new string[] { "shell", "beef", "shell" }, TurnAnyWordIntoABeefTacoTask.Tacofy("a"));
+					Assert.AreEqual(new string[] { "shell", "guacamole", "guacamole", "guacamole", "shell" }, TurnAnyWordIntoABeefTacoTask.Tacofy("ggg"));
+					Assert.AreEqual(new string[] { "shell", "beef", "guacamole", "lettuce", "shell" }, TurnAnyWordIntoABeefTacoTask.Tacofy("ogl"));
+					Assert.AreEqual(new string[] { "shell", "tomato", "beef", "shell" }, TurnAnyWordIntoABeefTacoTask.Tacofy("ydjkpwqrzto"));
+				}
+
+				[Test]
 				public void ConsecutiveVowelsInAStringTest()
 				{
 					Assertion(0, string.Empty);
