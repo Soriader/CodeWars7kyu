0d1bab9bfb934f4b3d083fb145715844022a1773
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-07 16:32:42 +0100

    [~] Task: Sum of differences between products and LCMs

---
+++ CodeWars7kyu/LCMTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class LCMTask
+	{
+		public static int SumDifferencesBetweenProductsAndLCMs(int[][] pairs)
+		{
+			if(pairs is null)
+			{
+				return 0;
+			}
+
+			List<int> result = new List<int>();
+
+			for (int i = 0; i < pairs.Length; i++)
+			{
+				if (pairs[i][0] == 0)
+				{
+					continue;
+				}
+
+				int a = pairs[i][0];
+				int b = pairs[i][1];
+
+				int product = a * b;
+				int lcm = (a * b) / Gcd(a, b);
+
+				result.Add(product - lcm);
+			}
+
+			return result.Sum();
+		}
+
+		private static int Gcd(int a, int b)
+		{
+			while (b != 0)
+			{
+				int temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+	}
+}
+//https://www.codewars.com/kata/56e56756404bb1c950000992/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,15 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+				[Test]
+				public void LCMTest()
+				{
+					Assert.AreEqual(840, LCMTask.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 15, 18 }, new int[] { 4, 5 }, new int[] { 12, 60 } }));
+					Assert.AreEqual(1092, LCMTask.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 1, 1 }, new int[] { 0, 0 }, new int[] { 13, 91 } }));
+					Assert.AreEqual(0, LCMTask.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 15, 7 }, new int[] { 4, 5 }, new int[] { 19, 60 } }));
+					Assert.AreEqual(1890, LCMTask.SumDifferencesBetweenProductsAndLCMs(new int[][] { new int[] { 20, 50 }, new int[] { 10, 10 }, new int[] { 50, 20 } }));
+					Assert.AreEqual(0, LCMTask.SumDifferencesBetweenProductsAndLCMs(new int[][] { }));
+				}
 
 				[Test]
 				public void TurnAnyWordIntoABeefTacoTest()
