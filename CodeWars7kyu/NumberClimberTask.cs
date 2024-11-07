73920988216d0ed917d8c67a10f1b0944c61dacd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-07 21:41:48 +0100

    [~] Task: Number climber

---
+++ CodeWars7kyu/NumberClimberTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class NumberClimberTask
+	{
+		public static int[] Climb(int n)
+		{ 
+			if(n == 1)
+			{
+				return new int[] {1};
+			}
+
+			List<int> result = new List<int>() { n };
+			bool isOne = true ;
+			var actualNumberInSequence = n;
+
+			while(isOne)
+			{
+				if(actualNumberInSequence % 2 != 0)
+				{
+					actualNumberInSequence = (actualNumberInSequence - 1) / 2;
+					result.Add(actualNumberInSequence);
+				}
+				else if(actualNumberInSequence % 2 == 0)
+				{
+					actualNumberInSequence = actualNumberInSequence / 2;
+					result.Add(actualNumberInSequence);
+				}
+
+				if(actualNumberInSequence == 1)
+				{
+					isOne = false;
+				}
+			}
+
+			return result.OrderBy(x => x).ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/559760bae64c31556c00006b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,13 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+				[Test]
+				public void NumberClimberTest()
+				{
+					Assert.AreEqual(string.Join(",", new int[] { 1 }), string.Join(",", NumberClimberTask.Climb(1)));
+					Assert.AreEqual(string.Join(",", new int[] { 1, 2, 5, 10 }), string.Join(",", NumberClimberTask.Climb(10)));
+					Assert.AreEqual(string.Join(",", new int[] { 1, 3, 6, 13 }), string.Join(",", NumberClimberTask.Climb(13)));
+				}
 
 				[Test]
 				public void LCMTest()
