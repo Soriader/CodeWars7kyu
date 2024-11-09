948eb83d902de328c72d8c0216d6dec232bbd763
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-09 20:48:42 +0100

    [~] Task: Is It Negative Zero (-0)?

---
+++ CodeWars7kyu/IsItNegativeZeroTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars7kyu
+{
+	public class IsItNegativeZeroTask
+	{
+		public static bool IsNegativeZero(double n)
+		{
+			return n == 0.0 && double.IsNegativeInfinity(1.0 / n);
+		}
+	}
+}
+//https://www.codewars.com/kata/5c5086287bc6600001c7589a/train/csharp
