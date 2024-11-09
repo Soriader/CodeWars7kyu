fdca7a0d8eefdd6f578e8bfe2c7f9740710bdbb1
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-09 21:16:15 +0100

    [~] Task: ⚠️Fusion Chamber Shutdown⚠️

---
+++ CodeWars7kyu/FusionChamberShutdownTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FusionChamberShutdownTask
+	{
+		public static (int, int, int) Burner(int c, int h, int o)
+		{
+			int water = 0;
+			int co2 = 0;
+			int methane = 0;
+
+			while (h >= 2 && o >= 1) 
+			{
+				h -= 2;
+				o -= 1;
+				water++;
+			}
+
+			while (c >= 1 && o >= 2) 
+			{
+				c -= 1;
+				o -= 2;
+				co2++;
+			}
+
+			while (h >= 4 && c >= 1)
+			{
+				c -= 1;
+				h -= 4;
+				methane++;
+			}
+
+			return (water, co2, methane);
+		}
+	}
+}
+//https://www.codewars.com/kata/5fde1ea66ba4060008ea5bd9/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FusionChamberShutdownTest()
+				{
+					Assert.AreEqual((5, 45, 0), FusionChamberShutdownTask.Burner(45, 11, 100));
+					Assert.AreEqual((0, 0, 354), FusionChamberShutdownTask.Burner(354, 1023230, 0));
+					Assert.AreEqual((1, 346, 0), FusionChamberShutdownTask.Burner(939, 3, 694));
+					Assert.AreEqual((20, 215, 0), FusionChamberShutdownTask.Burner(215, 41, 82100));
+					Assert.AreEqual((0, 26, 0), FusionChamberShutdownTask.Burner(113, 0, 52));
+				}
+
+				[Test]
 				public void MyTest()
 				{
 					Assert.AreEqual(GetInitialsFromPersonNameTask.ToInitials("Kerry King"), "K. K.");
