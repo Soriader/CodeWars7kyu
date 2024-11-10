0a3f2a6ebbe042fec25dc15f2260c7e4a29f2b9a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-10 15:10:38 +0100

    [~] Task: Point in a unit circle

---
+++ CodeWars7kyu/PointInAUnitCircleTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class PointInAUnitCircleTask
+	{
+		public static bool PointInCircle(double x, double y)
+		{
+			if(Math.Sqrt((Math.Pow(x, 2) + Math.Pow(y, 2))) < 1)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/58da7ae9b340a2440500009c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+
+				public void PointInAUnitCircleTest()
+				{
+					Assert.AreEqual(true, PointInAUnitCircleTask.PointInCircle(0, 0));
+					Assert.AreEqual(false, PointInAUnitCircleTask.PointInCircle(2, 0));
+					Assert.AreEqual(true, PointInAUnitCircleTask.PointInCircle(0, 0.9));
+					Assert.AreEqual(true, PointInAUnitCircleTask.PointInCircle(0.5, 0.5));
+					Assert.AreEqual(false, PointInAUnitCircleTask.PointInCircle(1, 0));
+				}
+				[Test]
 				public void CyclicalPermutationTest()
 				{
 					Assert.AreEqual("1234567\n2345671\n3456712\n4567123\n5671234\n6712345\n7123456", CyclicalPermutationTask.Pattern(7));
