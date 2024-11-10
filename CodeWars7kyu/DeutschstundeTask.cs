842a064dda1e6f812f0647c0b3113f432adb00ac
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-10 15:40:26 +0100

    [~] Task: Deutschstunde

---
+++ CodeWars7kyu/DeutschstundeTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class DeutschstundeTask
+	{
+		public static string DerDieDas(string wort)
+		{
+			string vovels = "aeiouäöü";
+			int iterator = 0;
+			foreach (char item in wort.ToLower())
+			{
+				if (vovels.Contains(item))
+				{
+					iterator++;
+				}
+			}
+
+			if(iterator < 2)
+			{
+				return $"das {wort}";
+			}
+			else if(iterator == 2 || iterator == 3)
+			{
+				return $"die {wort}";
+			}
+			else
+			{
+				return $"der {wort}";
+			}
+		}
+	}
+}
+//https://www.codewars.com/kata/552cd8624a414ec2b2000086/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,7 +21,18 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void DeutschstundeTest()
+				{
+					Assert.AreEqual("der OOOpa", DeutschstundeTask.DerDieDas("OOOpa"));
+					Assert.AreEqual("die Suppenhuhn", DeutschstundeTask.DerDieDas("Suppenhuhn"));
+					Assert.AreEqual("der Geheimdienst", DeutschstundeTask.DerDieDas("Geheimdienst"));
+					Assert.AreEqual("der Ofenkartoffel", DeutschstundeTask.DerDieDas("Ofenkartoffel"));
+					Assert.AreEqual("das Fisch", DeutschstundeTask.DerDieDas("Fisch"));
+					Assert.AreEqual("die Knödel", DeutschstundeTask.DerDieDas("Knödel"));
+					Assert.AreEqual("der Leberkaassemmi", DeutschstundeTask.DerDieDas("Leberkaassemmi"));
+				}
 
+				[Test]
 				public void PointInAUnitCircleTest()
 				{
 					Assert.AreEqual(true, PointInAUnitCircleTask.PointInCircle(0, 0));
