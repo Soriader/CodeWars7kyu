ccb097e7c4bf30896853f09b61214d3fb69f550b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-10 20:54:11 +0100

    [~] Task: Regexp Basics - is it a eight bit unsigned number?

---
+++ CodeWars7kyu/EightBitUnsignedNumberTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class EightBitUnsignedNumberTask
+	{
+		public static bool EightBitNumber(string str)
+		{
+			if (string.IsNullOrEmpty(str) || !str.All(char.IsDigit) || str.Any(char.IsWhiteSpace))
+			{
+				return false;
+			}
+
+			if (str.Length > 1 && str.StartsWith("0"))
+			{
+				return false;
+			}
+
+			if (int.TryParse(str, out int number))
+			{
+				return number >= 0 && number <= 255;
+			}
+
+			return false;
+		}
+	}
+}
+//https://www.codewars.com/kata/567e8f7b4096f2b4b1000005/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,21 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void EightBitUnsignedNumberTest()
+				{
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber(""));
+					Assert.AreEqual(true, EightBitUnsignedNumberTask.EightBitNumber("0"));
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber("00"));
+					Assert.AreEqual(true, EightBitUnsignedNumberTask.EightBitNumber("55"));
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber("042"));
+					Assert.AreEqual(true, EightBitUnsignedNumberTask.EightBitNumber("123"));
+					Assert.AreEqual(true, EightBitUnsignedNumberTask.EightBitNumber("255"));
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber("256"));
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber("999"));
+					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber("-1"));
+				}
+
+				[Test]
 				public void DeutschstundeTest()
 				{
 					Assert.AreEqual("der OOOpa", DeutschstundeTask.DerDieDas("OOOpa"));
