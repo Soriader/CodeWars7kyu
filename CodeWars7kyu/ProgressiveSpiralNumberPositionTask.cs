fb8dd94696b576252616518057a76231e48f1545
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-11 22:30:51 +0100

    [~] Task: Progressive Spiral Number Position

---
+++ CodeWars7kyu/ProgressiveSpiralNumberPositionTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ProgressiveSpiralNumberPositionTask
+	{
+		public static long Layers(long n)
+		{
+			if (n == 1)
+			{
+				return 1;
+			}
+
+			int result = 1;
+			long maxInLayer = 1;
+
+			while (maxInLayer < n)
+			{
+				maxInLayer += 8 * result;
+				result++;
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/5a254114e1ce0ecf6a000168/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ProgressiveSpiralNumberPositionTest()
+				{
+					Assert.AreEqual(1L, ProgressiveSpiralNumberPositionTask.Layers(1));
+					Assert.AreEqual(2L, ProgressiveSpiralNumberPositionTask.Layers(5));
+					Assert.AreEqual(3L, ProgressiveSpiralNumberPositionTask.Layers(25));
+					Assert.AreEqual(4L, ProgressiveSpiralNumberPositionTask.Layers(30));
+					Assert.AreEqual(5L, ProgressiveSpiralNumberPositionTask.Layers(50));
+				}
+
+				[Test]
 				public void EightBitUnsignedNumberTest()
 				{
 					Assert.AreEqual(false, EightBitUnsignedNumberTask.EightBitNumber(""));
