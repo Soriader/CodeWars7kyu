9ed40164e08b0e77acecb6c39649963477cf88e4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-12 23:56:23 +0100

    [~] Task: Zebulan's Nightmare

---
+++ CodeWars7kyu/ZebulanNightmareTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ZebulanNightmareTask
+	{
+		public static string ZebulansNightmare(string functionName)
+		{
+			var check = functionName.Split("_").ToArray();
+			string result = "";
+
+			for (int i = 0; i < check.Length; i++)
+			{
+				if (check[i].Length > 0)
+				{
+					if (i == 0)
+					{
+						result += char.ToLower(check[i][0]) + check[i].Substring(1).ToLower();
+					}
+					else
+					{
+						result += char.ToUpper(check[i][0]) + check[i].Substring(1).ToLower();
+					}
+				}
+			}
+
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/570fd7ad34e6130455001835/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,19 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void ZebulansNightmareTest()
+				{
+					Assert.AreEqual("camelCase", ZebulanNightmareTask.ZebulansNightmare("camel_case"));
+					Assert.AreEqual("markAsIssue", ZebulanNightmareTask.ZebulansNightmare("mark_as_issue"));
+					Assert.AreEqual("copyPastePep8", ZebulanNightmareTask.ZebulansNightmare("copy_paste_pep8"));
+					Assert.AreEqual("gotoNextKata", ZebulanNightmareTask.ZebulansNightmare("goto_next_kata"));
+					Assert.AreEqual("repeat", ZebulanNightmareTask.ZebulansNightmare("repeat"));
+					Assert.AreEqual("trollingIsFun", ZebulanNightmareTask.ZebulansNightmare("trolling_is_fun"));
+					Assert.AreEqual("why", ZebulanNightmareTask.ZebulansNightmare("why"));
+					Assert.AreEqual("123AbcDef", ZebulanNightmareTask.ZebulansNightmare("123_abc_def"));
+				}
+
+				[Test]
 				public void StackedBalls2DTest()
 				{
 					double EPSILON = 0.001;
