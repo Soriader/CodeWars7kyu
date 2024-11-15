ce763a878a2883f92dd6d8d8ae1f2f32155adf67
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-15 16:50:07 +0100

    [~] Task: Trimming a string

---
+++ CodeWars7kyu/TrimmingAStringTask.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TrimmingAStringTask
+	{
+		public static string TrimString(string phrase, int len)
+		{
+			if (phrase.Length <= len)
+			{
+				return phrase;
+			}
+
+			string result = "";
+
+			if (len <= 3)
+			{
+				for (int i = 0; i < len; i++)
+				{
+					result += phrase[i];
+				}
+
+				return result + "...";
+			}
+
+
+			result = phrase.Substring(0, len - 3);
+
+			return result + "...";
+		}
+	}
+}
+//https://www.codewars.com/kata/563fb342f47611dae800003c/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,20 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void TrimmingAStringTest()
+				{
+					Assert.AreEqual("Creating ka...", TrimmingAStringTask.TrimString("Creating kata is fun", 14));
+					Assert.AreEqual("H...", TrimmingAStringTask.TrimString("He", 1));
+					Assert.AreEqual("He...", TrimmingAStringTask.TrimString("Hey", 2));
+					Assert.AreEqual("Hey", TrimmingAStringTask.TrimString("Hey", 3));
+					Assert.AreEqual("Cr...", TrimmingAStringTask.TrimString("Creating kata is fun", 2));
+					Assert.AreEqual("Cod...", TrimmingAStringTask.TrimString("Code Wars is pretty rad", 3));
+					Assert.AreEqual("Coding rocks", TrimmingAStringTask.TrimString("Coding rocks", 12));
+					Assert.AreEqual("Code Wars is pretty rad", TrimmingAStringTask.TrimString("Code Wars is pretty rad", 50));
+					Assert.AreEqual("London is freezing", TrimmingAStringTask.TrimString("London is freezing", 18));
+				}
+
+				[Test]
 				public void ZebulansNightmareTest()
 				{
 					Assert.AreEqual("camelCase", ZebulanNightmareTask.ZebulansNightmare("camel_case"));
