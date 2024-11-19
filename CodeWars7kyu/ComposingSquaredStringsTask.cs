b3c2f8078c427a5e9a34f99c74d1e3eac367a3f9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-19 15:50:27 +0100

    [~] Task: Composing squared strings

---
+++ CodeWars7kyu/ComposingSquaredStringsTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ComposingSquaredStringsTask
+	{
+		public static string Compose(string s1, string s2)
+		{
+			string result = "";
+			var s1Parts = s1.Split('\n');
+			var s2Parts = s2.Split('\n');
+			int n = s1Parts.Length;
+
+			for (int i = 0; i < n; i++)
+			{
+				result += s1Parts[i].Substring(0, i + 1) + s2Parts[n - i - 1].Substring(0, n - i);
+
+				if (i < n - 1)
+				{
+					result += '\n';
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/56f253dd75e340ff670002ac/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,22 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void ComposingSquaredStringsTest()
+				{
+					static void testing(string actual, string expected)
+					{
+						Assert.AreEqual(expected, actual);
+					}
+
+					testing(ComposingSquaredStringsTask.Compose("byGt\nhTts\nRTFF\nCnnI", "jIRl\nViBu\nrWOb\nNkTB"),
+							"bNkTB\nhTrWO\nRTFVi\nCnnIj");
+					testing(ComposingSquaredStringsTask.Compose("HXxA\nTGBf\nIPhg\nuUMD", "Hcbj\nqteH\nGbMJ\ngYPW"),
+							"HgYPW\nTGGbM\nIPhqt\nuUMDH");
+					testing(ComposingSquaredStringsTask.Compose("tSrJ\nOONy\nsqPF\nxMkB", "hLqw\nEZuh\nmYFl\nzlYf"),
+							"tzlYf\nOOmYF\nsqPEZ\nxMkBh");
+				}
+
+				[Test]
 				public void DescribeAListTest()
 				{
 					Assert.AreEqual("empty", DescribeAListTask.DescribeList(new List<int>()));
