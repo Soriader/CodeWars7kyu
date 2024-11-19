1180f143312dd98cb9ae10629fec515831660258
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-19 16:21:12 +0100

    [~] Task: Target Date

---
+++ CodeWars7kyu/TargetDateTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TargetDateTask
+	{
+		public static string DateNbDays(double a0, double a, double p)
+		{
+			var startDate = new DateTime(2016, 1, 1);
+			int days = 0;
+			var moneyWeNeed = a0;
+
+			while (moneyWeNeed <= a)
+			{
+				moneyWeNeed += moneyWeNeed * (p / 36000);
+				days++;
+			}
+			DateTime resultDate = startDate.AddDays(days);
+
+			return resultDate.ToString("yyyy-MM-dd");
+
+		}
+	}
+}
+//https://www.codewars.com/kata/569218bc919ccba77000000b/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,26 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public static void TargetDateTest()
+				{
+					static void testing(string actual, string expected)
+					{
+						Assert.AreEqual(expected, actual);
+					}
+
+					testing(TargetDateTask.DateNbDays(4281, 5087, 2), "2024-07-03");
+					testing(TargetDateTask.DateNbDays(4620, 5188, 2), "2021-09-19");
+					testing(TargetDateTask.DateNbDays(9999, 11427, 6), "2018-03-13");
+					testing(TargetDateTask.DateNbDays(3525, 4822, 3), "2026-04-18");
+					testing(TargetDateTask.DateNbDays(5923, 6465, 6), "2017-06-10");
+					testing(TargetDateTask.DateNbDays(4254, 4761, 8), "2017-05-22");
+					testing(TargetDateTask.DateNbDays(1244, 2566, 4), "2033-11-04");
+					testing(TargetDateTask.DateNbDays(6328, 7517, 5), "2019-05-25");
+					testing(TargetDateTask.DateNbDays(2920, 3834, 2), "2029-06-03");
+					testing(TargetDateTask.DateNbDays(7792, 8987, 4), "2019-07-09");
+				}
+
+				[Test]
 				public static void ComposingSquaredStringsTest()
 				{
 					static void testing(string actual, string expected)
