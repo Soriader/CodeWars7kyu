f4802e9a09ede8399259777576cc1c5cb7aa65cf
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-20 18:41:55 +0100

    [~] Task: How many urinals are free?

---
+++ CodeWars7kyu/HowManyUrinalsTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class HowManyUrinalsTask
+	{
+		public static int GetFreeUrinals(string urinals)
+		{
+			if (urinals.Contains("11"))
+			{
+				return -1;
+
+			}
+
+			int result = 0;
+			char[] urinalsArray = urinals.ToCharArray();
+
+			for (int i = 0; i < urinalsArray.Length; i++)
+			{
+				if (urinalsArray[i] == '0')
+				{
+					bool leftEmpty = (i == 0 || urinalsArray[i - 1] == '0');
+					bool rightEmpty = (i == urinalsArray.Length - 1 || urinalsArray[i + 1] == '0');
+
+					if (leftEmpty && rightEmpty)
+					{
+						urinalsArray[i] = '1';
+						result++;
+					}
+				}
+			}
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/5e2733f0e7432a000fb5ecc4/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,23 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void HowManyUrinalsTest()
+				{
+					Assert.AreEqual(1, HowManyUrinalsTask.GetFreeUrinals("10001"));
+					Assert.AreEqual(0, HowManyUrinalsTask.GetFreeUrinals("1001"));
+					Assert.AreEqual(3, HowManyUrinalsTask.GetFreeUrinals("00000"));
+					Assert.AreEqual(2, HowManyUrinalsTask.GetFreeUrinals("0000"));
+					Assert.AreEqual(1, HowManyUrinalsTask.GetFreeUrinals("01000"));
+					Assert.AreEqual(2, HowManyUrinalsTask.GetFreeUrinals("10000"));
+					Assert.AreEqual(0, HowManyUrinalsTask.GetFreeUrinals("1"));
+					Assert.AreEqual(1, HowManyUrinalsTask.GetFreeUrinals("0"));
+					Assert.AreEqual(0, HowManyUrinalsTask.GetFreeUrinals("10"));
+					Assert.AreEqual(-1, HowManyUrinalsTask.GetFreeUrinals("110"));
+					Assert.AreEqual(-1, HowManyUrinalsTask.GetFreeUrinals("101100001"));
+					Assert.AreEqual(-1, HowManyUrinalsTask.GetFreeUrinals("0000100000101010011"));
+				}
+
+				[Test]
 				public static void TargetDateTest()
 				{
 					static void testing(string actual, string expected)
