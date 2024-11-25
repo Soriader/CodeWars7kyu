388da8913f341431d028c4b2bff4afdba8bb5c33
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-25 17:11:37 +0100

    [~] Task: Bob's Short Forms

---
+++ CodeWars7kyu/BobShortFormsTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class BobShortFormsTask
+	{
+		public string ShortForm(string str)
+		{
+			var vovels = "aeiouAEIOU";
+			string result = "";
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (vovels.Contains(str[i]) && i != 0 && i != str.Length - 1)
+				{
+					continue;
+				}
+				else
+				{
+					result += str[i];
+				}
+			}	
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/570cbe88f616a8f4f50011ac/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,40 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void BobShortFormsTest()
+				{
+					var kata = new BobShortFormsTask();
+
+					//Should return word without vowels
+					Assert.AreEqual("typhd", kata.ShortForm("typhoid"));
+					Assert.AreEqual("fre", kata.ShortForm("fire"));
+					Assert.AreEqual("dstry", kata.ShortForm("destroy"));
+					Assert.AreEqual("kta", kata.ShortForm("kata"));
+					Assert.AreEqual("cdwrs", kata.ShortForm("codewars"));
+
+					//Should ignore vowels at beginning or end of word
+					Assert.AreEqual("assrt", kata.ShortForm("assert"));
+					Assert.AreEqual("insne", kata.ShortForm("insane"));
+					Assert.AreEqual("nce", kata.ShortForm("nice"));
+					Assert.AreEqual("amzng", kata.ShortForm("amazing"));
+					Assert.AreEqual("incrrgble", kata.ShortForm("incorrigible"));
+
+					//Should be case-insenstive
+					Assert.AreEqual("HllO", kata.ShortForm("HeEllO"));
+					Assert.AreEqual("inCRdBLE", kata.ShortForm("inCRediBLE"));
+					Assert.AreEqual("IMpsSblE", kata.ShortForm("IMpOsSiblE"));
+					Assert.AreEqual("UnnTNtNl", kata.ShortForm("UnInTENtiONAl"));
+					Assert.AreEqual("AWSme", kata.ShortForm("AWESOme"));
+
+					//Should support input with no vowels
+					Assert.AreEqual("rhythm", kata.ShortForm("rhythm"));
+					Assert.AreEqual("hymn", kata.ShortForm("hymn"));
+					Assert.AreEqual("lynx", kata.ShortForm("lynx"));
+					Assert.AreEqual("nymph", kata.ShortForm("nymph"));
+					Assert.AreEqual("pygmy", kata.ShortForm("pygmy"));
+				}
+
+				[Test]
 				public void HowManyUrinalsTest()
 				{
 					Assert.AreEqual(1, HowManyUrinalsTask.GetFreeUrinals("10001"));
