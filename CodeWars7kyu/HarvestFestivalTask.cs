70c85c27664f7ad96e47ac61ba8b631264edadef
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-25 19:22:39 +0100

    [~] Task: Harvest Festival

---
+++ CodeWars7kyu/HarvestFestivalTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class HarvestFestivalTask
+	{
+		public static string Plant(char seed, int water, int fert, int temp)
+		{
+			if (temp > 30 || temp < 20)
+			{
+				return String.Concat(Enumerable.Repeat("-", water * water)) + seed;
+			}
+
+			string flower = "";
+			int flowerLength = (water + fert) * water;
+			int iterator = 0;
+
+			while (iterator < flowerLength)
+			{
+				flower += String.Concat(Enumerable.Repeat("-", water)) + String.Concat(Enumerable.Repeat(seed, fert));
+				iterator+= water + fert;
+			}
+
+			return flower;
+		}
+	}
+}
+//https://www.codewars.com/kata/606efc6a9409580033837dfb/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,33 +20,37 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+
+				[Test]
+				public void HarvestFestivalTest()
+				{
+					Assert.AreEqual("----@@@----@@@----@@@----@@@", HarvestFestivalTask.Plant('@', 4, 3, 23));
+					Assert.AreEqual("-~~~~~~", HarvestFestivalTask.Plant('~', 1, 6, 30));
+					Assert.AreEqual("----------------------------------------------------------------------------------------------------#", HarvestFestivalTask.Plant('#', 10, 2, 15));
+					Assert.AreEqual("----*", HarvestFestivalTask.Plant('*', 2, 4, 8));
+					Assert.AreEqual("--<<--<<", HarvestFestivalTask.Plant('<', 2, 2, 28));
+				}
+
+
 				[Test]
 				public void BobShortFormsTest()
 				{
 					var kata = new BobShortFormsTask();
-
-					//Should return word without vowels
 					Assert.AreEqual("typhd", kata.ShortForm("typhoid"));
 					Assert.AreEqual("fre", kata.ShortForm("fire"));
 					Assert.AreEqual("dstry", kata.ShortForm("destroy"));
 					Assert.AreEqual("kta", kata.ShortForm("kata"));
 					Assert.AreEqual("cdwrs", kata.ShortForm("codewars"));
-
-					//Should ignore vowels at beginning or end of word
 					Assert.AreEqual("assrt", kata.ShortForm("assert"));
 					Assert.AreEqual("insne", kata.ShortForm("insane"));
 					Assert.AreEqual("nce", kata.ShortForm("nice"));
 					Assert.AreEqual("amzng", kata.ShortForm("amazing"));
 					Assert.AreEqual("incrrgble", kata.ShortForm("incorrigible"));
-
-					//Should be case-insenstive
 					Assert.AreEqual("HllO", kata.ShortForm("HeEllO"));
 					Assert.AreEqual("inCRdBLE", kata.ShortForm("inCRediBLE"));
 					Assert.AreEqual("IMpsSblE", kata.ShortForm("IMpOsSiblE"));
 					Assert.AreEqual("UnnTNtNl", kata.ShortForm("UnInTENtiONAl"));
 					Assert.AreEqual("AWSme", kata.ShortForm("AWESOme"));
-
-					//Should support input with no vowels
 					Assert.AreEqual("rhythm", kata.ShortForm("rhythm"));
 					Assert.AreEqual("hymn", kata.ShortForm("hymn"));
 					Assert.AreEqual("lynx", kata.ShortForm("lynx"));
