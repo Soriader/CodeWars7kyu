cf598cfb45b74bb6e958049e1f9d768dd9f3f926
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-26 18:14:18 +0100

    [~] Task: Chinese Zodiac

---
+++ CodeWars7kyu/ChineseZodiacTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class ChineseZodiacTask
+	{
+		enum Animal
+		{
+			Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, Pig
+		}
+
+		enum Elements
+		{
+			Wood, Fire, Earth, Metal, Water
+		}
+
+		public static string ChineseZodiac(int year)
+		{
+			int startYear = 1924;
+			int iteratorForAnimal = 0;
+			int iteratorForElement = 0;
+
+			while(startYear != year)
+			{
+				startYear++;
+				iteratorForAnimal++;
+
+				if (iteratorForAnimal > 11)
+				{
+					iteratorForAnimal = 0;
+				}
+
+				if (iteratorForAnimal % 2 == 0)
+				{
+					iteratorForElement++;
+				}
+				if (iteratorForElement > 4)
+				{
+					iteratorForElement = 0;
+				}
+
+			}
+
+			Animal animal = (Animal)iteratorForAnimal;
+			Elements element = (Elements)iteratorForElement;
+
+			return $"{element} {animal}";
+		}
+	}
+}
+//https://www.codewars.com/kata/57a73e697cb1f31dd70000d2/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,17 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+				[Test]
+				public void ChineseZodiacTest()
+				{
+					Assert.AreEqual("Wood Snake", ChineseZodiacTask.ChineseZodiac(1965));
+					Assert.AreEqual("Earth Tiger", ChineseZodiacTask.ChineseZodiac(1938));
+					Assert.AreEqual("Earth Tiger", ChineseZodiacTask.ChineseZodiac(1998));
+					Assert.AreEqual("Fire Monkey", ChineseZodiacTask.ChineseZodiac(2016));
+					Assert.AreEqual("Wood Rat", ChineseZodiacTask.ChineseZodiac(1924));
+					Assert.AreEqual("Earth Monkey", ChineseZodiacTask.ChineseZodiac(1968));
+					Assert.AreEqual("Water Dog", ChineseZodiacTask.ChineseZodiac(2162));
+				}
 
 				[Test]
 				public void HarvestFestivalTest()
