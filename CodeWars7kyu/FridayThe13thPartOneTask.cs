59d90f518e2a8749b0c51fd4a3e2d6a1098121d9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-29 13:59:24 +0100

    [~] Task: Friday the 13th Part 1

---
+++ CodeWars7kyu/FridayThe13thPartOneTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FridayThe13thPartOneTask
+	{
+		public static string[] KillCount(Dictionary<string, int> counselors, int jason)
+		{
+			return counselors.Where(c => c.Value < jason).Select(c => c.Key).ToList().ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/5925acf31a9825d616000e74/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,16 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+				[Test]
+				public void FridayThe13thPartOneTest()
+				{
+					Dictionary<string, int> counselors = new Dictionary<string, int>
+	  {
+		{"Mike", 7},
+		{"Alysa", 3}
+	  };
+					Assert.AreEqual(new string[] { "Alysa" }, FridayThe13thPartOneTask.KillCount(counselors, 7));
+				}
 
 				[Test]
 				public void PatternCraftVisitorTask()
