266626073763a32bb6ae9305a89d9ef423fbe819
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-11-30 18:59:21 +0100

    [~] Task: Basics 04: Rotate Matrix

---
+++ CodeWars7kyu/RotateMatrixTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class RotateMatrixTask
+	{
+		public int[,] RotateMatrixLeft(int[,] matrix)
+		{
+			int rows = matrix.GetLength(0); 
+			int cols = matrix.GetLength(1); 
+
+			int[,] rotatedMatrix = new int[cols, rows];
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					rotatedMatrix[cols - j - 1, i] = matrix[i, j];
+				}
+			}
+
+			return rotatedMatrix;
+		}
+
+	}
+}
+//https://www.codewars.com/kata/56b5dd1702a30326ce000b02/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,22 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void RotateMatrixTest()
+				{
+					int[,] oldMatrix = new int[4, 4]
+		   {{-1,4,3,4},
+			 {5,6,7,8},
+			 {2,5,1,7},
+			 {3,14,-5,3}};
+					int[,] newMatrix = new int[4, 4]
+					{{4,8,7,3},
+			 {3,7,1,-5},
+			 {4,6,5,14},
+			 {-1,5,2,3}};
+					Assert.AreEqual(newMatrix, new RotateMatrixTask().RotateMatrixLeft(oldMatrix));
+				}
+
+				[Test]
 				public void HexadecimalNumberTest()
 				{
 					Assert.AreEqual(false, HexadecimalNumberTask.HexNumber(""));
