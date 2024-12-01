6b28c313653ac7a1ca6e04ec0d05c41421f0a182
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-01 19:48:12 +0100

    [~] Task: Temperature analysis I

---
+++ CodeWars7kyu/TemperatureAnalysisTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class TemperatureAnalysisTask
+	{
+		public static int? LowestTemperature(string t)
+		{
+			if (t is null || t == "")
+			{
+				return null;
+			}
+
+			List<int> temperature = new List<int>();
+			string[] parts = t.Split(' ');
+
+			foreach (var part in parts)
+			{
+				if (int.TryParse(part, out int temp))
+				{
+					temperature.Add(temp);
+				}
+			}
+
+			return temperature.Min();
+		}
+	}
+}
+//https://www.codewars.com/kata/588e0f11b7b4a5b373000041/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -20,6 +20,17 @@ namespace CodeWarsTest
         {
             public class Tests
 			{
+
+				[Test]
+				public void TemperatureAnalysisTest()
+				{
+					Assert.AreEqual(-8, TemperatureAnalysisTask.LowestTemperature("-1 50 -4 20 22 -7 0 10 -8"));
+					Assert.AreEqual(10, TemperatureAnalysisTask.LowestTemperature("10 50 12 20 22 13 20 100 18"));
+					Assert.AreEqual(-1, TemperatureAnalysisTask.LowestTemperature("-1 50 16 20 22 1 0 10 34"));
+					Assert.AreEqual(-2, TemperatureAnalysisTask.LowestTemperature("-2 3 -1 12 45 14"));
+					Assert.AreEqual(null, TemperatureAnalysisTask.LowestTemperature(""));
+				}
+
 				[Test]
 				public void RoundersTest()
 				{
