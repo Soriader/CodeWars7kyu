a13e9602637e672f65eb7e9074383d7dd0ebffd5
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-01 19:38:28 +0100

    [~] Task: Simple Fun #17: Rounders

---
+++ CodeWars7kyu/RoundersTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class RoundersTask
+	{
+		public int Rounders(int value)
+		{
+			int factor = 1; 
+
+			while (value >= 10)
+			{
+				int lastDigit = value % 10; 
+				value /= 10;
+
+				if (lastDigit >= 5)
+				{
+					value++; 
+				}
+
+				factor *= 10;
+			}
+
+			return value * factor;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/58846d50f54f021d90000012/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,26 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void RoundersTest()
+				{
+					var kata = new RoundersTask();
+
+					Assert.AreEqual(20, kata.Rounders(15));
+
+					Assert.AreEqual(1000, kata.Rounders(1234));
+
+					Assert.AreEqual(2000, kata.Rounders(1445));
+
+					Assert.AreEqual(10, kata.Rounders(14));
+
+					Assert.AreEqual(100, kata.Rounders(99));
+
+					Assert.AreEqual(10, kata.Rounders(10));
+
+
+				}
+
+				[Test]
 				public void HeavyMetalTest()
 				{
 					Assert.AreEqual("Ännöüncïng thë Mäcböök Äïr Güïtär", HeavyMetalTask.HeavyMetalUmlauts("Announcing the Macbook Air Guitar"));
