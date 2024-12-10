d5700eba9275fdfcf8e300242e7d579a5690740c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-10 16:14:06 +0100

    [~] Task: Simple Fun #179: Fraction

---
+++ CodeWars7kyu/FractionTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class FractionTask
+	{
+		public int Fraction(int a, int b)
+		{
+			int gcd = GCD(a, b);
+			int reducedNumerator = a / gcd;
+			int reducedDenominator = b / gcd;
+
+			return reducedNumerator + reducedDenominator;
+		}
+
+		private static int GCD(int x, int y)
+		{
+			while (y != 0)
+			{
+				int temp = y;
+				y = x % y;
+				x = temp;
+			}
+			return x;
+		}
+	}
+}
+//https://www.codewars.com/kata/58b8db810f40ea7788000126/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,25 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void FractionTest()
+				{
+					var kata = new FractionTask();
+
+					Assert.AreEqual(7, kata.Fraction(90, 120));
+
+					Assert.AreEqual(3, kata.Fraction(2, 4));
+
+					Assert.AreEqual(11, kata.Fraction(100, 1000));
+
+					Assert.AreEqual(6, kata.Fraction(3, 15));
+
+					Assert.AreEqual(157, kata.Fraction(114, 200));
+
+					Assert.AreEqual(121, kata.Fraction(3, 118));
+
+				}
+
+				[Test]
 				public void OddMarchBits8BitsTest()
 				{
 					var expected = new int[][]
