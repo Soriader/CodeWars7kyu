453419a37172ee1d241a06ff5a7d7d8ebb0e809f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-12 19:05:15 +0100

    [~] Task: String basics

---
+++ CodeWars7kyu/StringBasicsTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class StringBasicsTask
+	{
+		public static string[] GetUserIds(string s)
+		{
+			return s.ToLower().Replace("#", "").Split(',').Select(x => x.Trim()[3..]).ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/56326c13e63f90538d00004e/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void StringBasicsTest()
+				{
+					Assert.AreEqual(new string[] { "12345" }, StringBasicsTask.GetUserIds("uid12345"));
+					Assert.AreEqual(new string[] { "abc" }, StringBasicsTask.GetUserIds("   uidabc  "));
+					Assert.AreEqual(new string[] { "swagger" }, StringBasicsTask.GetUserIds("#uidswagger"));
+					Assert.AreEqual(new string[] { "one", "two" }, StringBasicsTask.GetUserIds("uidone, uidtwo"));
+					Assert.AreEqual(new string[] { "capslock" }, StringBasicsTask.GetUserIds("uidCAPSLOCK"));
+				}
+
+				[Test]
 				public void SumTimesTablesTest()
 				{
 					Assert.AreEqual(30, SumTimesTablesTask.SumTimesTable(new List<int> { 2, 3 }, 1, 3));
