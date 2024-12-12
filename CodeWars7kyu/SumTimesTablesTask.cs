faf17dc8b630ff439f1637458b0926e3408b8ca6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-12 18:39:25 +0100

    [~] Task: Sum Times Tables

---
+++ CodeWars7kyu/SumTimesTablesTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+	public class SumTimesTablesTask
+	{
+		public static long SumTimesTable(List<int> tables, long min, long max)
+		{
+			long result = 0;
+
+			foreach (var number in tables) 
+			{
+				for (long i = min; i <= max; i++)
+				{
+					result += number * i;
+				}
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/551e51155ed5ab41450006e1/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,14 @@ namespace CodeWarsTest
             public class Tests
 			{
 				[Test]
+				public void SumTimesTablesTest()
+				{
+					Assert.AreEqual(30, SumTimesTablesTask.SumTimesTable(new List<int> { 2, 3 }, 1, 3));
+					Assert.AreEqual(9, SumTimesTablesTask.SumTimesTable(new List<int> { 1, 3, 5 }, 1, 1));
+					Assert.AreEqual(495, SumTimesTablesTask.SumTimesTable(new List<int> { 1, 3, 5 }, 1, 10));
+				}
+
+				[Test]
 				public void FractionTest()
 				{
 					var kata = new FractionTask();
