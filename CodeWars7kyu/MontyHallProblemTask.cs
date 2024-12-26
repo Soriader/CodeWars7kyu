e134ce4d7b9b15d104d6e920ba05cfa743ce4ae0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-26 20:39:53 +0100

    [~] Task: Monty Hall Problem

---
+++ CodeWars7kyu/MontyHallProblemTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class MontyHallProblemTask
+    {
+        public static int MontyHall(int correctDoor, int[] participants)
+        {
+            int result = participants.Count(x => x == correctDoor);
+
+            return 100 - (int)Math.Round(100.0 * result / participants.Length);
+        }
+    }
+}
+//https://www.codewars.com/kata/54f9cba3c417224c63000872/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
 			{
                 [Test]
+                public void MontyHallProblemTest()
+                {
+                    Assert.AreEqual(71, MontyHallProblemTask.MontyHall(1, new int[] { 3, 3, 2, 3, 3, 2, 2, 3, 2, 2, 1, 1, 1, 1 }));
+                    Assert.AreEqual(64, MontyHallProblemTask.MontyHall(2, new int[] { 3, 3, 2, 3, 3, 2, 2, 3, 2, 2, 1, 1, 1, 1 }));
+                    Assert.AreEqual(64, MontyHallProblemTask.MontyHall(3, new int[] { 3, 3, 2, 3, 3, 2, 2, 3, 2, 2, 1, 1, 1, 1 }));
+
+                }
+
+
+                [Test]
                 public void LowercaseStringsInArrayTest()
                 {
                     Assert.AreEqual(new object[] { "red", "green" }, LowercaseStringsInArrayTask.ArrayLowerCase(new object[] { "Red", "Green" }));
