bc44eb0f99fbab7fc2d197b5db30c670e2a2c8c0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-28 17:43:37 +0100

    [~] Task: Red Knight

---
+++ CodeWars7kyu/RedKnightTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class RedKnightTask
+    {
+        public static (string color, long position) RedKnight(int knight, long pawn)
+        {
+            long knightFinalPosition = 2 * pawn;
+
+            string color = (knight + pawn) % 2 == 0 ? "White" : "Black";
+
+            return (color, knightFinalPosition);
+        }
+    }
+}
+//https://www.codewars.com/kata/5fc4349ddb878a0017838d0f/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,15 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void RedKnightTests()
+                {
+                    Assert.AreEqual(("White", 16), RedKnightTask.RedKnight(0, 8));
+                    Assert.AreEqual(("Black", 14), RedKnightTask.RedKnight(0, 7));
+                    Assert.AreEqual(("Black", 12), RedKnightTask.RedKnight(1, 6));
+                    Assert.AreEqual(("White", 10), RedKnightTask.RedKnight(1, 5));
+                }
+
+                [Test]
                 public void TheHiddenWordTest()
                 {
                     TheHiddenWordTask kata = new TheHiddenWordTask();
