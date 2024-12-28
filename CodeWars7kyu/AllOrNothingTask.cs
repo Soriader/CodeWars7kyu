a29bb8eb86d94ea34d94322e44abedcab08fd9b6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-28 17:55:12 +0100

    [~] Task: All or Nothing

---
+++ CodeWars7kyu/AllOrNothingTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class AllOrNothingTask
+    {
+        public static bool PossiblyPerfect(string[] key, string[] ans)
+        {
+            bool isCorrect = true;
+            bool isNotCorrect = true;
+
+            for (int i = 0; i < key.Length; i++) 
+            {
+                if (key[i] != ans[i] && key[i] != "_")
+                {
+                    isCorrect = false;
+                }
+
+                if(key[i] == ans[i] && key[i] != "_")
+                {
+                    isNotCorrect = false;
+                }
+            }
+
+            if (isCorrect || isNotCorrect)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/65112af7056ad6004b5672f8/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,26 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void AllOrNothingTest()
+                {
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "A", "_", "C", "_", "B" }, new[] { "A", "D", "C", "E", "B" }));
+                    Assert.AreEqual(false, AllOrNothingTask.PossiblyPerfect(new[] { "B", "_", "B" }, new[] { "B", "D", "C" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "T", "_", "F", "F", "F" }, new[] { "F", "F", "T", "T", "T" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "B", "A", "_", "_" }, new[] { "B", "A", "C", "C" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "A", "B", "A", "_" }, new[] { "B", "A", "C", "C" }));
+                    Assert.AreEqual(false, AllOrNothingTask.PossiblyPerfect(new[] { "A", "B", "C", "_" }, new[] { "B", "A", "C", "C" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "B", "_" }, new[] { "C", "A" }));
+                    Assert.AreEqual(false, AllOrNothingTask.PossiblyPerfect(new[] { "B", "A" }, new[] { "C", "A" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "B" }, new[] { "B" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "_", "T", "_", "_" }, new[] { "T", "F", "F", "F" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "_", "T", "_", "_" }, new[] { "T", "T", "F", "T" }));
+                    Assert.AreEqual(false, AllOrNothingTask.PossiblyPerfect(new[] { "_", "T", "T", "T" }, new[] { "T", "T", "F", "T" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "_", "T", "T", "T" }, new[] { "T", "T", "T", "T" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "_", "T", "T", "T" }, new[] { "F", "F", "F", "F" }));
+                    Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "_", "_", "_", "_" }, new[] { "F", "F", "F", "F" }));
+                }
+
+                [Test]
                 public void RedKnightTests()
                 {
                     Assert.AreEqual(("White", 16), RedKnightTask.RedKnight(0, 8));
