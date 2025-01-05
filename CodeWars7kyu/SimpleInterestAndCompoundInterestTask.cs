88bffcf8d8be709f548659c08c937de8be51661a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-05 14:19:21 +0100

    [~] Task: Simple Interest and Compound Interest

---
+++ CodeWars7kyu/SimpleInterestAndCompoundInterestTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class SimpleInterestAndCompoundInterestTask
+    {
+        public static int[] Interest(int p, double r, int n)
+        {
+            var result = new int[2];
+            var pFor0 = (p + (p*r*n));
+            result[0] = (int)Math.Round(pFor0);
+
+
+            int iterator = 0;
+            double pFor1 = p;
+
+            while (iterator < n) 
+            {
+                pFor1 += (pFor1 * r);
+                iterator++;
+            }
+
+            result[1] = (int)Math.Round(pFor1);
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/59cd0535328801336e000649/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,14 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+				public void SimpleInterestAndCompoundInterestTest()
+				{
+                    //Assert.AreEqual(new int[2] { 110, 110 }, SimpleInterestAndCompoundInterestTask.Interest(100, 0.1, 1));
+                    //Assert.AreEqual(new int[2] { 120, 121 }, SimpleInterestAndCompoundInterestTask.Interest(100, 0.1, 2));
+                    Assert.AreEqual(new int[2] { 200, 259 }, SimpleInterestAndCompoundInterestTask.Interest(100, 0.1, 10));
+                }
+
+                [Test]
                 public void AllOrNothingTest()
                 {
                     Assert.AreEqual(true, AllOrNothingTask.PossiblyPerfect(new[] { "A", "_", "C", "_", "B" }, new[] { "A", "D", "C", "E", "B" }));
