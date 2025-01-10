78ec6c7d0c8e024d3c53937b9194b6fe790c00ed
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-10 16:21:35 +0100

    [~] Task: Not all but sometimes all

---
+++ CodeWars7kyu/NotAllButSometimesAllTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class NotAllButSometimesAllTask
+    {
+        public static string Remove(string str, Dictionary<char, int> what)
+        {
+            var check = str.ToCharArray();
+            string result = "";
+
+            foreach (char c in check) 
+            {
+                if (what.ContainsKey(c) && what[c] > 0)
+                {
+                    what[c] -= 1;
+                }
+                else
+                {
+                    result += c;    
+                }
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/564ab935de55a747d7000040/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void NotAllButSometimesAllTest()
+                {
+                    Assert.That(NotAllButSometimesAllTask.Remove("this is a string", new Dictionary<char, int> { { 't', 1 }, { 'i', 2 } }), Is.EqualTo("hs s a string"));
+                    Assert.That(NotAllButSometimesAllTask.Remove("hello world", new Dictionary<char, int> { { 'x', 5 }, { 'i', 2 } }), Is.EqualTo("hello world"));
+                    Assert.That(NotAllButSometimesAllTask.Remove("apples and bananas", new Dictionary<char, int> { { 'a', 50 }, { 'n', 1 } }), Is.EqualTo("pples d bnns"));
+                    Assert.That(NotAllButSometimesAllTask.Remove("a", new Dictionary<char, int> { { 'a', 1 }, { 'n', 1 } }), Is.EqualTo(""));
+                    Assert.That(NotAllButSometimesAllTask.Remove("codewars", new Dictionary<char, int> { { 'c', 5 }, { 'o', 1 }, { 'd', 1 }, { 'e', 1 }, { 'w', 1 }, { 'z', 1 }, { 'a', 1 }, { 'r', 1 }, { 's', 1 } }), Is.EqualTo(""));
+                }
+
+                [Test]
                 public void YouGotChangeTest()
                 {
                     Assert.That(YouGotChangeTask.GiveChange(365), Is.EqualTo(new int[] { 0, 1, 1, 0, 1, 3 }));
