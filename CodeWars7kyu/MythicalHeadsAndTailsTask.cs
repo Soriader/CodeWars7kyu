41f52c36c8dbee871e0a365a92750e3a727d1f07
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-13 18:49:21 +0100

    [~] Task: Mythical Heads and Tails

---
+++ CodeWars7kyu/MythicalHeadsAndTailsTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class MythicalHeadsAndTailsTask
+    {
+        public static int[] Beasts(int heads, int tails)
+        {
+            int y = (heads - 2 * tails) / 3;
+
+            if ((heads - 2 * tails) % 3 != 0 || y < 0)
+            {
+                return null; 
+            }
+
+            int x = tails - y;
+
+            if (x < 0)
+            {
+                return null; 
+            }
+
+            return new int[] { x, y };
+        }
+    }
+}
+//https://www.codewars.com/kata/5751aa92f2dac7695d000fb0/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,16 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void MythicalHeadsAndTailsTest()
+                {
+                    Assert.That(MythicalHeadsAndTailsTask.Beasts(123, 39), Is.EqualTo(new int[] { 24, 15 }));
+                    Assert.That(MythicalHeadsAndTailsTask.Beasts(371, 88), Is.EqualTo(new int[] { 23, 65 }));
+                    Assert.That(MythicalHeadsAndTailsTask.Beasts(24, 12), Is.EqualTo(new int[] { 12, 0 }));
+                    Assert.That(MythicalHeadsAndTailsTask.Beasts(113, 37), Is.EqualTo(new int[] { 24, 13 }));
+                    Assert.That(MythicalHeadsAndTailsTask.Beasts(635, 181), Is.EqualTo(new int[] { 90, 91 }));
+                }
+
+                [Test]
                 public void TheRobberLanguageTest()
                 {
                     Assert.AreEqual("hohelollolo wowororloldod", TheRobberLanguageTask.RobberEncode("hello world"));
