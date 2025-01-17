c2b269f79f97a83c615ed89f8520755e15c2310f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-17 18:07:49 +0100

    [~] Task: Cogs 2

---
+++ CodeWars7kyu/CogsTwoTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class CogsTwoTask
+    {
+        public static double[] CogRpm(int[] cogs, int n)
+        {
+            if (cogs == null || cogs.Length == 0)
+            {
+                return null;
+            }
+
+            decimal rpm = 1.0m;
+
+            int numCogs = cogs.Length;
+
+            for (int i = n; i > 0; i--)
+            {
+                rpm *= -(decimal)cogs[i] / cogs[i - 1];
+            }
+            decimal firstCogRpm = rpm;
+
+            rpm = 1.0m;
+            for (int i = n; i < numCogs - 1; i++)
+            {
+                rpm *= -(decimal)cogs[i] / cogs[i + 1];
+            }
+            decimal lastCogRpm = rpm;
+
+            return new[] { (double)firstCogRpm, (double)lastCogRpm };
+        }
+    }
+}
+//https://www.codewars.com/kata/59e72bdcfc3c4974190000d9/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,17 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void CogsTwoTest()
+                {
+                    double delta = 1e-9;
+                    double[] expected = new[] { -1.0 / 2, -2.0 };
+                    var actual = CogsTwoTask.CogRpm(new[] { 100, 50, 25 }, 1);
+                    Assert.That(actual.Length, Is.EqualTo(2));
+                    Assert.That(actual[0], Is.EqualTo(expected[0]).Within(delta));
+                    Assert.That(actual[1], Is.EqualTo(expected[1]).Within(delta));
+                }
+
+                [Test]
                 public void CogsTest()
                 {
                     double expected = -4.0 / 3;
