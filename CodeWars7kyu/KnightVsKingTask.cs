b0a83b8c7e08c2597111298826d8152f13ee1010
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-21 21:57:32 +0100

    [~] Task: Knight vs King

---
+++ CodeWars7kyu/KnightVsKingTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class KnightVsKingTask
+    {
+        public static string KnightVsKing(object[] knightPosition, object[] kingPosition)
+        {
+            var x = (int)knightPosition[0] - (int)kingPosition[0];
+            var y = ((string)knightPosition[1])[0] - ((string)kingPosition[1])[0];
+            var d = (x * x) + (y * y);
+
+            if (d < 3)
+            { 
+                return "King"; 
+            }
+            else if (d == 5) 
+            { 
+                return "Knight"; 
+            }
+            return "None";
+        }
+
+    }
+}
+//https://www.codewars.com/kata/564e1d90c41a8423230000bc/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,22 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void KnightVsKingTest()
+                {
+					object[] kingPosition = { 2, "F" };
+					object[] knightPosition = { 6, "B" };
+					Assert.That(KnightVsKingTask.KnightVsKing(knightPosition, kingPosition), Is.EqualTo("None").IgnoreCase);
+
+					object[] kingPosition2 = { 4, "C" };
+					object[] knightPosition2 = { 6, "D" };
+					Assert.That(KnightVsKingTask.KnightVsKing(knightPosition2, kingPosition2), Is.EqualTo("Knight").IgnoreCase);
+
+					object[] kingPosition3 = { 7, "B" };
+                    object[] knightPosition3 = { 6, "C" };
+                    Assert.That(KnightVsKingTask.KnightVsKing(knightPosition3, kingPosition3), Is.EqualTo("King").IgnoreCase);
+                }
+
+                [Test]
                 public void CogsTwoTest()
                 {
                     double delta = 1e-9;
