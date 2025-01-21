c8e5ce36a4d7b0df1200210499c66157dbf03726
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-21 22:38:51 +0100

    [~] Task: Stone Pickaxe Crafting

---
+++ CodeWars7kyu/StonePickaxeCraftingTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars7kyu
+{
+    public class StonePickaxeCraftingTask
+    {
+        public static int StonePick(string[] arr)
+        {
+            int sticks = 0;
+            int cobblestones = 0;
+
+            foreach (var item in arr) 
+            {
+                if(item == "Sticks")
+                {
+                    sticks++;
+                }
+
+                if (item == "Cobblestone")
+                {
+                    cobblestones++;
+                }
+
+                if(item == "Wood")
+                {
+                    sticks += 4;
+                }
+            }
+
+            return Math.Min(sticks / 2, cobblestones / 3);
+        }
+
+    }
+}
+//https://www.codewars.com/kata/65c0161a2380ae78052e5731/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -21,6 +21,28 @@ namespace CodeWarsTest
             public class Tests
             {
                 [Test]
+                public void StonePickaxeCraftingTest()
+                {
+                    string[] stringToMinecraftArrayConverter(string s)
+                    {
+                        Dictionary<char, string> map = new Dictionary<char, string>()
+            { {'S', "Sticks"} , {'s', "Stone"}, {'W', "Wood" }, {'w', "Wool" },
+            {'C', "Cobblestone" }, {'D', "Dirt" }, {'d', "Diamond" } };
+                        return s.Select(x => map[x]).ToArray();
+                    }
+
+					var arr = stringToMinecraftArrayConverter("");
+					var actual = StonePickaxeCraftingTask.StonePick(arr);
+					Assert.That(actual, Is.EqualTo(0));
+
+					Assert.AreEqual(2, StonePickaxeCraftingTask.StonePick(stringToMinecraftArrayConverter("SSSSCCCCCC")));
+                    Assert.AreEqual(0, StonePickaxeCraftingTask.StonePick(stringToMinecraftArrayConverter("SSC")));
+                    Assert.AreEqual(0, StonePickaxeCraftingTask.StonePick(stringToMinecraftArrayConverter("SSSSwwwDDDDDD")));
+                    Assert.AreEqual(4, StonePickaxeCraftingTask.StonePick(stringToMinecraftArrayConverter("WWCCCCCCCCCCCC")));
+
+                }
+
+                [Test]
                 public void KnightVsKingTest()
                 {
 					object[] kingPosition = { 2, "F" };
