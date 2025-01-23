06c9461d5309dc260f7f84ad6d0557c5a8184e5c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-23 19:52:10 +0100

    [!] Rebuilded the test class

---
+++ CodeWarsTest/ReturnTheMissingElement.cs
@@ -0,0 +1,13 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class ReturnTheMissingElement
+{
+    [Test]
+    public void ReturnTheMissingElementTest()
+    {
+        Assert.AreEqual(8, ReturnTheMissingElementTask.GetMissingElement(new int[9] { 0, 5, 1, 3, 2, 9, 7, 6, 4 }));
+        Assert.AreEqual(3, ReturnTheMissingElementTask.GetMissingElement(new int[9] { 9, 2, 4, 5, 7, 0, 8, 6, 1 }));
+    }
+}
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -13,13 +13,7 @@ namespace CodeWarsTest
         {
 	        public class Tests
             {
-
-                [Test]
-                public void ReturnTheMissingElementTest()
-                {
-                    Assert.AreEqual(8, ReturnTheMissingElementTask.GetMissingElement(new int[9] { 0, 5, 1, 3, 2, 9, 7, 6, 4 }));
-                    Assert.AreEqual(3, ReturnTheMissingElementTask.GetMissingElement(new int[9] { 9, 2, 4, 5, 7, 0, 8, 6, 1 }));
-                }
+                private readonly ReturnTheMissingElement _returnTheMissingElement = new ReturnTheMissingElement();
 
                 [Test]
                 public void CheckThreeAndTwoTest()
