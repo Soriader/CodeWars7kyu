b4d24c4881e5bd9016885280ee37a7014197fdc6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-24 18:10:01 +0100

    [~] Task: Remove the noise from the string

---
+++ CodeWars7kyu/RemoveTheNoiseFromTheStringTask.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars7kyu;
+
+public class RemoveTheNoiseFromTheStringTask
+{
+    public static string RemoveNoise(string equation)
+    {
+        var exceptions = "%$&/#·@|º\\ª";
+        return new string(equation.Where(c => !exceptions.Contains(c) || c == ' ').ToArray());
+    }
+}
+//https://www.codewars.com/kata/5681cf0be812b41721000034/train/csharp
---
+++ CodeWarsTest/RemoveTheNoiseFromTheString.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class RemoveTheNoiseFromTheString
+{
+    [Test]
+    public void RemoveTheNoiseFromTheStringTest()
+    {
+        Assert.That(RemoveTheNoiseFromTheStringTask.RemoveNoise("%$&/#·@|º\\ª"), Is.EqualTo(""), "You failed the test for '%$&/#·@|º\\ª'. (Please note that in c# special characters are displayed as '?' in the error screen)");
+        Assert.That(RemoveTheNoiseFromTheStringTask.RemoveNoise("h%e&·%$·llo w&%or&$l·$%d"), Is.EqualTo("hello world"), "You failed the test for 'h%e&·%$·llo w&%or&$l·$%d'.");    
+        Assert.That(RemoveTheNoiseFromTheStringTask.RemoveNoise("he%$·ll@o c$&%odi%&ng for ev|#·ery&$$#$on%$·e"), Is.EqualTo("hello coding for everyone"), "You failed the test for 'he%$·ll@o c$&%odi%&ng for ev|#·ery&$$#$on%$·e'.");
+        Assert.That(RemoveTheNoiseFromTheStringTask.RemoveNoise("c|o@$%de%w@a·$r%s &rºocªks"), Is.EqualTo("codewars rocks"), "You failed the test for 'c|o@$%de%w@a·$r%s &rºocªks'.(Please note that in c# special characters are displayed as '?' in the error screen)");
+    }
+}
