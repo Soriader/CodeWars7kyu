41133d3b226f8dfa5f61d808b582d194e834c199
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-26 17:14:36 +0100

    [~] Task: Simple array product

---
+++ CodeWars7kyu/SimpleArrayProductTask.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeWars7kyu;
+
+public class SimpleArrayProductTask
+{
+    public static int SimpleArray(int[][] arr)
+    {
+        return GetMaxProduct(arr, 0, 1);
+    }
+    private static int GetMaxProduct(int[][] arr, int depth, int currentProduct)
+    {
+        if (depth == arr.Length)
+        {
+            return currentProduct;
+        }
+
+        int result = int.MinValue;
+
+        foreach (int num in arr[depth])
+        {
+            result = Math.Max(result, GetMaxProduct(arr, depth + 1, currentProduct * num));
+        }
+
+        return result;
+    }
+}
---
+++ CodeWarsTest/SimpleArrayProduct.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class SimpleArrayProduct
+{
+    [Test]
+    public void ExampleTests()
+    {
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { 1, 2 }, new int [] { 3, 4 } }), Is.EqualTo(8));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { 10, -15 }, new int [] { -1, -3 } }), Is.EqualTo(45));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { -1, 2, -3, 4 }, new int [] { 1, -2, 3, -4 } }), Is.EqualTo(12));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { -11, -6 }, new int[] { -20, -20 }, new int[] { 18, -4 }, new int[] { -20, 1 } }), Is.EqualTo(17600));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int[] { 14, 2 }, new int[] { 0, -16 }, new int[] { -12, -16 } }), Is.EqualTo(3584));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { -3, -4 }, new int [] { 1, 2, -3 } }), Is.EqualTo(12));
+        Assert.That(SimpleArrayProductTask.SimpleArray(new int [][] { new int [] { -2, -15, -12, -8, -16 }, new int [] { -4, -15, -7 }, new int [] { -10, -5 } }), Is.EqualTo(-40));
+    }
+}
