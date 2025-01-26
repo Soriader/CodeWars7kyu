e6787f89114235ad59e27e7f23dbc164b15bdb20
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-26 18:38:20 +0100

    [~] Task: Complete The Pattern #10 - Parallelogram

---
+++ CodeWars7kyu/ParallelogramTask.cs
@@ -0,0 +1,51 @@
+﻿
+namespace CodeWars7kyu;
+
+public class ParallelogramTask
+{
+    public static string Pattern(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException("Negative number");
+        }
+        
+        var spaceLengthOnLeft = n - 1;
+        int spaceLengthOnRight = 0;
+        int iterator = 0;
+        string row = "";
+        int number = 1;
+        string result = "";
+        
+        while (iterator < n)
+        {
+            row += number.ToString();
+            iterator++;
+            number++;
+
+            if (number == 10 && iterator != n)
+            {
+                number = 0;
+                row += number.ToString();
+                iterator++;
+                number++;
+            }
+        }
+
+        iterator = 0;
+
+        while (iterator < n)
+        {
+            result += string.Concat(Enumerable.Repeat(" ", spaceLengthOnLeft)) 
+                      + row 
+                      + string.Concat(Enumerable.Repeat(" ", spaceLengthOnRight)) 
+                      + "\n";
+            spaceLengthOnLeft--;
+            spaceLengthOnRight++;
+            iterator++;
+        }
+        
+        return result.TrimEnd('\n');
+    }
+}
+//https://www.codewars.com/kata/5581a7651185fe13190000ee/train/csharp
---
+++ CodeWarsTest/Parallelogram.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class Parallelogram
+{
+    [Test]
+    public void ParallelogramTest()
+    {
+        Assert.That(ParallelogramTask.Pattern(3), Is.EqualTo("  123\n 123 \n123  "));
+        Assert.That(ParallelogramTask.Pattern(5), Is.EqualTo("    12345\n   12345 \n  12345  \n 12345   \n12345    "));
+        Assert.That(ParallelogramTask.Pattern(8), Is.EqualTo("       12345678\n      12345678 \n     12345678  \n    12345678   \n   12345678    \n  12345678     \n 12345678      \n12345678       "));
+    }
+}
