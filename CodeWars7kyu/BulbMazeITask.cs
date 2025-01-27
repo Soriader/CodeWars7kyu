6cc2d23fab74518b4be3e81d4c819e8594ccf361
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-27 20:09:54 +0100

    [~] Task: Simple Fun #347: Bulb Maze I

---
+++ CodeWars7kyu/BulbMazeITask.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeWars7kyu;
+
+public class BulbMazeITask
+{
+    public static bool BulbMaze(string maze)
+    {
+        var secondCombination = maze.Replace('x', 'z').Replace('o', 'x').Replace('z', 'o');
+        
+        int iterator = 0;
+
+        while (iterator != maze.Length)
+        {
+            if (iterator % 2 == 0)
+            {
+                if (maze[iterator] == 'o')
+                {
+                    return false;
+                }  
+            }
+            else if (!(iterator % 2 == 0))
+            {
+                if (secondCombination[iterator] == 'o')
+                {
+                    return false;
+                }  
+            }
+            
+            iterator++;
+        }
+        
+        return true;
+    }
+}
+//https://www.codewars.com/kata/59c0b9d4cb7fb4dd41000962/train/csharp
---
+++ CodeWarsTest/BulbMazeI.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class BulbMazeI
+{
+    [TestCase("xo oxox", ExpectedResult = true)]
+    [TestCase("xo  oxox", ExpectedResult = false)]
+    [TestCase("xo ox xo", ExpectedResult = true)]
+    [TestCase("xooxxo", ExpectedResult = false)]
+    [TestCase("xxxxxx", ExpectedResult = false)]
+    [TestCase("oooooo", ExpectedResult = false)]
+    [TestCase("      ", ExpectedResult = true)]
+    [TestCase(" ox", ExpectedResult = true)]
+    [TestCase("xo", ExpectedResult = true)]
+    [TestCase("x", ExpectedResult = true)]
+    [TestCase("o", ExpectedResult = false)]
+    public bool Sample_Cases(string s) => BulbMazeITask.BulbMaze(s);
+}
