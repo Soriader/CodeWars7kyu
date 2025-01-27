d5efe08b3f4a96e8153253d6c722714972f8aaa1
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-27 19:07:39 +0100

    [~] Task: Print count and numbers

---
+++ CodeWars7kyu/PrintCountAndNumbersTask.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWars7kyu;
+
+public class PrintCountAndNumbersTask
+{
+    public static string CountMe(string data)
+    {
+        if (data.Any(ch => !char.IsDigit(ch)) || data == "")
+        {
+            return ""; 
+        }
+        string result = "";
+        char boxForNumber = data[0];
+        int iterator = 1;
+        for (int i = 1; i < data.Length; i++)
+        {
+            if (boxForNumber == data[i])
+            {
+                iterator++;
+            }
+            else
+            {
+                result += $"{iterator}{boxForNumber}";
+                boxForNumber = data[i];
+                iterator = 1;
+            }
+        }
+
+        result += $"{iterator}{boxForNumber}";
+
+        return result;
+    }
+}
+//https://www.codewars.com/kata/559af787b4b8eac78b000022/train/csharp
---
+++ CodeWarsTest/PrintCountAndNumbers.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars7kyu;
+
+namespace CodeWarsTest;
+
+public class PrintCountAndNumbers
+{
+    private static IEnumerable<TestCaseData> testCases
+    {
+        get
+        {
+            yield return new TestCaseData("1123").Returns("211213");
+            yield return new TestCaseData("1").Returns("11");
+            yield return new TestCaseData("a").Returns("");
+            yield return new TestCaseData("11").Returns("21");
+            yield return new TestCaseData("a123").Returns("");
+            yield return new TestCaseData("21").Returns("1211");
+            yield return new TestCaseData("1211").Returns("111221");
+            yield return new TestCaseData("12322212223443").Returns("111213321132132413");
+            yield return new TestCaseData("").Returns("");
+            yield return new TestCaseData("123a").Returns("");
+        }
+    }
+  
+    [Test, TestCaseSource("testCases")]
+    public string Test(string data) => PrintCountAndNumbersTask.CountMe(data);
+}
