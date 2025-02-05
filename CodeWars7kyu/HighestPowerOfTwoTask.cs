fb7d23f578d10daacf629c1650a0aece96ee65ab
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-05 19:11:33 +0100

    [~] Task: Highest power of 2 that evenly divides a number

---
+++ CodeWars7kyu/HighestPowerOfTwoTask.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeWars7kyu;
+
+public class HighestPowerOfTwoTask
+{
+    public static long Solution(long n)
+    {
+        int iterator = 0;
+        var boxForDividing = n;
+
+        while (boxForDividing > 0)
+        {
+            if (boxForDividing % 2 != 0)
+            {
+                break;
+            }
+            
+            boxForDividing = boxForDividing / 2;
+            iterator++; 
+        }
+        
+        return (long)Math.Pow(2, iterator);
+    }
+}
+//https://www.codewars.com/kata/58dabca7ce77a12dbd00000f/train/csharp
---
+++ CodeWarsTest/HighestPowerOfTwo.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class HighestPowerOfTwo
+{
+    [Test]
+    public void HighestPowerOfTwoTest()
+    {
+        Assert.That(HighestPowerOfTwoTask.Solution(123), Is.EqualTo(1));
+        Assert.That(HighestPowerOfTwoTask.Solution(64), Is.EqualTo(64));
+        Assert.That(HighestPowerOfTwoTask.Solution(132232), Is.EqualTo(8));
+        Assert.That(HighestPowerOfTwoTask.Solution(192), Is.EqualTo(64));
+    }
+}
