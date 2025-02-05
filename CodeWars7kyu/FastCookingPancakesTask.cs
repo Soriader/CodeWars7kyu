93e51314b1c4a3bc9aa0f5895171392958889fea
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-05 14:50:13 +0100

    [~] Task: Fast cooking pancakes

---
+++ CodeWars7kyu/FastCookingPancakesTask.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWars7kyu;
+
+public class FastCookingPancakesTask
+{
+    public static int CookPancakes(int n, int m) 
+    {
+        if (n <= m)
+        {
+            return 2;
+        }
+        
+        return (2 * n - 1) / m + 1;
+        
+    }
+}
+//https://www.codewars.com/kata/58552bdb68b034a1a80001fb/train/csharp
---
+++ CodeWarsTest/FastCookingPancakes.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class FastCookingPancakes
+{
+    static void Act(int expected, int n, int m)
+        => Assert.That(FastCookingPancakesTask.CookPancakes(n, m), Is.EqualTo(expected), $"Input -> n = {n}, m = {m}");
+    
+    [TestCase(2, 1, 2)]
+    [TestCase(2, 2, 2)]
+    [TestCase(3, 3, 2)]
+    [TestCase(4, 4, 2)]
+    [TestCase(3, 4, 3)]
+    [TestCase(2, 4, 4)]
+    [TestCase(2, 1, 4)]
+    [TestCase(2, 2, 3)]
+    [TestCase(3, 5, 4)]
+    [TestCase(4, 6, 3)]
+    [TestCase(3, 6, 5)]
+    [TestCase(6, 3, 1)]
+    public void FixedTests(int expected, int n, int m) => Act(expected, n, m);
+}
