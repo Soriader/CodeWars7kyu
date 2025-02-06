738115c4a63031676f0a5bbedad4ba7ca6cb133a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-06 19:49:38 +0100

    [~] Task: Hop Across

---
+++ CodeWars7kyu/HopAcrossTask.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars7kyu;
+
+public class HopAcrossTask
+{
+    public static int HopAcross(List<int> lst)
+    {
+        if (lst.Count == 1)
+        {
+            return 2;
+        }
+
+        var leftSide = lst.ToList(); 
+        leftSide.Reverse();
+
+        return HopHop(lst) + HopHop(leftSide);
+    }
+
+    private static int HopHop(List<int> river)
+    {
+        int jumps = 0;
+        int position = 0;
+
+        while (position < river.Count)
+        {
+            jumps++;
+            position += river[position];
+        }
+
+        return jumps;
+    }
+}
+//https://www.codewars.com/kata/5a60d519400f93fc450032e5/train/csharp
---
+++ CodeWarsTest/HopAcross.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class HopAcross
+{
+    [Test]
+    public void HopAcrossTest()
+    {
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {1}), Is.EqualTo(2));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {2}), Is.EqualTo(2));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {1,1}), Is.EqualTo(4));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {2,1}), Is.EqualTo(3));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {2,1,1}), Is.EqualTo(5));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {1,2,1,2}), Is.EqualTo(5));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {1,2,5,1}), Is.EqualTo(5));
+        Assert.That(HopAcrossTask.HopAcross(new List<int> {2,2,3,1,1,2,1}), Is.EqualTo(7));
+    }
+}
