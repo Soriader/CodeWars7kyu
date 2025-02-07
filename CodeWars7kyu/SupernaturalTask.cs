760d812a1de6fb8751d56dd2413a1a9c803a5be1
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-07 18:31:28 +0100

    [~] Task: Supernatural

---
+++ CodeWars7kyu/SupernaturalTask.cs
@@ -0,0 +1,86 @@
+﻿namespace CodeWars7kyu;
+
+public class SupernaturalTask
+{
+    public static string Bob(string monsterName)
+    {
+        
+        switch (monsterName)
+        {
+            case "werewolf":
+            case "shapeshifter":
+            {
+                return "Silver knife or bullet to the heart, idjits!";
+            }
+            case "vampire":
+            {
+                return "Behead it with a machete, idjits!";
+            }
+            case "wendigo":
+            {
+                return "Burn it to death, idjits!";
+            }
+            case "angel":
+            {
+                return "Use the angelic blade, idjits!";
+            }
+            case "demon":
+            {
+                return "Use Ruby's knife, or some Jesus-juice, idjits!";
+            }
+            case "ghost":
+            {
+                return "Salt and iron, and don't forget to burn the corpse, idjits!";
+            }
+            case "dragon":
+            {
+                return "You have to find the excalibur for that, idjits!";
+            }
+            case "djinn":
+            {
+                return "Stab it with silver knife dipped in a lamb's blood, idjits!";
+            }
+            case "pagan god":
+            {
+                return "It depends on which one it is, idjits!";
+            }
+            case "leviathan":
+            {
+                return "Use some Borax, then kill Dick, idjits!";
+            }
+            case "ghoul":
+            {
+                return "Behead it, idjits!";
+            }
+            case "jefferson starship":
+            {
+                return "Behead it with a silver blade, idjits!";
+            }
+            case "reaper":
+            {
+                return "If it's nasty, you should gank who controls it, idjits!";
+            }
+            case "rugaru":
+            {
+                return "Burn it alive, idjits!";
+            }
+            case "skinwalker":
+            {
+                return "A silver bullet will do it, idjits!";
+            }
+            case "phoenix":
+            {
+                return "Use the colt, idjits!";
+            }
+            case "witch":
+            {
+                return "They are humans, idjits!";
+            }
+            default:
+            {
+                return "I have friggin no idea yet, idjits!";
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/55c9a8cda33889d69e00008b/train/csharp
---
+++ CodeWarsTest/Supernatural.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class Supernatural
+{
+    [Test]
+    public static void SupernaturalTest()
+    {
+        Assert.That(SupernaturalTask.Bob("vampire"), Is.EqualTo("Behead it with a machete, idjits!"));
+        Assert.That(SupernaturalTask.Bob("pagan god"), Is.EqualTo("It depends on which one it is, idjits!"));
+        Assert.That(SupernaturalTask.Bob("werepuppy"), Is.EqualTo("I have friggin no idea yet, idjits!"));
+    }
+}
