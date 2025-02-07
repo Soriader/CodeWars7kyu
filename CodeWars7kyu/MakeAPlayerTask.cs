c492ce94eac4da4bf988b5f92c012e3f16ab0935
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-07 18:43:54 +0100

    [~] Task: A (no more) bugs trilogy: Episode 3 - Make a Player

---
+++ CodeWars7kyu/MakeAPlayerTask.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars7kyu;
+
+public class Player
+{
+    public string Name{get; set;}
+    public string Position{get; set;}
+    public int Age{get; set;}
+    public int Dribbling{get; set;}
+    public int Pass{get; set;}
+    public int Shoot{get; set;}
+
+    public Player(string name, string position, int age, int dribbling, int pass, int shoot)
+    {
+        Name = name;
+        Position = position;
+        Age = age;
+        Dribbling = dribbling;
+        Pass = pass;
+        Shoot = shoot;
+    }
+}
+//https://www.codewars.com/kata/5630c850ed4343c1d0000083/train/csharp
---
+++ CodeWarsTest/MakeAPlayer.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+
+public class MakeAPlayer
+{
+    [Test]
+    public void SampleTest()
+    {
+        Player player = new Player("Player", "Right Winger", 25, 75, 90, 65);
+        Assert.That(player.Name, Is.EqualTo("Player"));
+        Assert.That(player.Position, Is.EqualTo("Right Winger"));
+        Assert.That(player.Age, Is.EqualTo(25));
+        Assert.That(player.Dribbling, Is.EqualTo(75));
+        Assert.That(player.Pass, Is.EqualTo(90));
+        Assert.That(player.Shoot, Is.EqualTo(65));
+    }
+}
