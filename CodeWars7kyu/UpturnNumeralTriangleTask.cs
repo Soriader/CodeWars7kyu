1687cb30beeb80be3d8da16bbf7411cb279c26cc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-11 12:17:21 +0100

    [~] Task: Upturn Numeral Triangle

---
+++ CodeWars7kyu/UpturnNumeralTriangleTask.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars7kyu;
+
+public class UpturnNumeralTriangleTask
+{
+    public static string UpturnNumeralTriangle(int n)
+    {
+        int number = 1;
+        int iterator = n;
+        int space = 1;
+        string result = "";
+
+        while (iterator != 0)
+        {
+            result += string.Concat(Enumerable.Repeat(" ", space)) 
+                + string.Concat(Enumerable.Repeat(number + " ", iterator)).TrimEnd(' ')
+                + "\n";
+            iterator--;
+            number++;
+            space++;
+
+            if (number == 10)
+            {
+                number = 0;
+            }
+            
+            
+        }
+
+        return result.TrimEnd('\n');
+    }
+}
+//https://www.codewars.com/kata/564f3d49a06556d27c000077/train/csharp
---
+++ CodeWarsTest/UpturnNumeralTriangle.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class UpturnNumeralTriangle
+{
+    [Test, Order(1)]
+    public void Pattern7()
+    {
+        string expectetPattern = " 1 1 1 1 1 1 1\n  2 2 2 2 2 2\n   3 3 3 3 3\n    4 4 4 4\n     5 5 5\n      6 6\n       7";
+        Assert.That(UpturnNumeralTriangleTask.UpturnNumeralTriangle(7), Is.EqualTo(expectetPattern));
+    }
+    
+    [Test, Order(2)]
+    public void Pattern12()
+    {
+        string expectetPattern = " 1 1 1 1 1 1 1 1 1 1 1 1\n  2 2 2 2 2 2 2 2 2 2 2\n   3 3 3 3 3 3 3 3 3 3\n    4 4 4 4 4 4 4 4 4\n     5 5 5 5 5 5 5 5\n      6 6 6 6 6 6 6\n       7 7 7 7 7 7\n        8 8 8 8 8\n         9 9 9 9\n          0 0 0\n           1 1\n            2";
+        Assert.That(UpturnNumeralTriangleTask.UpturnNumeralTriangle(12), Is.EqualTo(expectetPattern));
+    }
+    
+    [Test, Order(3)]
+    public void Pattern21()
+    {
+        string expectetPattern = " 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2\n   3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3\n    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5\n      6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6\n       7 7 7 7 7 7 7 7 7 7 7 7 7 7 7\n        8 8 8 8 8 8 8 8 8 8 8 8 8 8\n         9 9 9 9 9 9 9 9 9 9 9 9 9\n          0 0 0 0 0 0 0 0 0 0 0 0\n           1 1 1 1 1 1 1 1 1 1 1\n            2 2 2 2 2 2 2 2 2 2\n             3 3 3 3 3 3 3 3 3\n              4 4 4 4 4 4 4 4\n               5 5 5 5 5 5 5\n                6 6 6 6 6 6\n                 7 7 7 7 7\n                  8 8 8 8\n                   9 9 9\n                    0 0\n                     1" ; 
+        Assert.That(UpturnNumeralTriangleTask.UpturnNumeralTriangle(21), Is.EqualTo(expectetPattern));
+    }
+}
