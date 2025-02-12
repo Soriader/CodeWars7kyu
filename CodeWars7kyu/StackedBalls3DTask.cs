10b9d8f9c346bb346f425d4b3e5f2b417db76743
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-12 10:19:49 +0100

    [~] Task: Stacked Balls - 3D (square base)

---
+++ CodeWars7kyu/StackedBalls3DTask.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWars7kyu;
+
+public class StackedBalls3DTask
+{
+    public static double StackHeight3d(int layers)
+    {
+        if (layers == 0)
+        {
+            return 0d;
+        }
+        
+        return 1.0 + (layers - 1) * (Math.Sqrt(2) / 2);
+    }
+}
+//https://www.codewars.com/kata/5bb493932ce53339dc0000c2/train/csharp
---
+++ CodeWarsTest/StackedBalls3D.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class StackedBalls3D
+{
+    private static double EPSILON = 0.001;
+
+    [Test, Order(1)]
+    public void Test1()
+    {
+        Assert.That(StackedBalls3DTask.StackHeight3d(1), Is.EqualTo(1.0).Within(EPSILON));
+    }
+
+    [Test, Order(2)]
+    public void Test2()
+    {
+        Assert.That(StackedBalls3DTask.StackHeight3d(2), Is.EqualTo(1.7071).Within(EPSILON));
+    }
+}
