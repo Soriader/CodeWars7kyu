aee7a4bced8f8e991b8d49bae850edbcb2be45ee
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-13 18:23:20 +0100

    [~] Task: Boxlines

---
+++ CodeWars7kyu/BoxlinesTask.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeWars7kyu;
+
+public class BoxlinesTask
+{
+    public static ulong F(ushort x, ushort y, ushort z)
+    {
+        return x * (y + 1UL) * (z + 1UL) +
+               y * (z + 1UL) * (x + 1UL) +
+               z * (x + 1UL) * (y + 1UL);
+    }
+}
+//https://www.codewars.com/kata/6129095b201d6b000e5a33f0/train/csharp
---
+++ CodeWarsTest/Boxlines.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class Boxlines
+{
+    private static void Act(ushort x, ushort y, ushort z, ulong expected)
+    {
+        var msg = $"Invalid answer for x: {x}, y: {y}, z: {z}";
+        var actual = BoxlinesTask.F(x, y, z);
+        Assert.That(actual, Is.EqualTo(expected), msg);
+    }
+      
+    [TestCase(2, 1, 1, 20UL)]
+    [TestCase(1, 2, 3, 46UL)]
+    [TestCase(2, 2, 2, 54UL)]
+    public void BoxlinesTest(int x, int y, int z, ulong expected)
+    {
+        Act((ushort)x, (ushort)y, (ushort)z, expected);
+    }
+}
