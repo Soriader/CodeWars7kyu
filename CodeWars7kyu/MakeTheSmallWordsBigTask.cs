8dc643ab59c193a5e72cf01d9a40a29805e79f74
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-13 18:54:08 +0100

    [~] Task: Make the small words big!

---
+++ CodeWars7kyu/MakeTheSmallWordsBigTask.cs
@@ -0,0 +1,54 @@
+﻿namespace CodeWars7kyu;
+
+public class MakeTheSmallWordsBigTask
+{
+    public static string SmallWordHelper(string sentence)
+    {
+        string result = "";
+        var vovels = "aeiouAEIOU";
+        
+        var check = sentence.Split(" ");
+
+        foreach (string word in check)
+        {
+            if (word.Length <= 3)
+            {
+                result += word.ToUpper();
+            }
+
+            if (word.Length > 3)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (vovels.Contains(word[i].ToString()))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        result += word[i];
+                    }
+                }
+            }
+
+            result += " ";
+        }
+        
+        result = result.TrimEnd(' ');
+
+        for (int i = sentence.Length - 1; i >= 0; i--)
+        {
+            if (sentence[i] == ' ')
+            {
+                result += " ";
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
+//https://www.codewars.com/kata/57b4dd38d2a31c75f7000299/train/csharp
---
+++ CodeWarsTest/MakeTheSmallWordsBig.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class MakeTheSmallWordsBig
+{
+    [Test]
+    public void MakeTheSmallWordsBigTest()
+    {
+        Assert.That(MakeTheSmallWordsBigTask.SmallWordHelper("The quick brown fox jumps over the lazy dog"), Is.EqualTo("THE qck brwn FOX jmps vr THE lzy DOG"));
+        Assert.That(MakeTheSmallWordsBigTask.SmallWordHelper("I'm just a small word from a small word family"), Is.EqualTo("I'M jst A smll wrd frm A smll wrd fmly"));   
+    }
+}
