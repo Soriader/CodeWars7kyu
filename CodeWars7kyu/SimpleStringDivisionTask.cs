5dfa1724d0003806d229c2ef7d86056e60ad5f8c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-23 19:09:32 +0100

    [~] Task: Simple string division

---
+++ CodeWars7kyu/SimpleStringDivisionTask.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars7kyu;
+
+public class SimpleStringDivisionTask
+{
+    public static long Solve(string st, int k)
+    {
+        List<long> partitions = new List<long>();
+        GeneratePartitions(st, k, 0, new List<string>(), partitions);
+        return partitions.Max();
+    }
+
+    private static void GeneratePartitions(string st, int k, int index, List<string> currentParts, List<long> partitions)
+    {
+        if (k == 0)
+        {
+            string lastPart = st.Substring(index);
+            currentParts.Add(lastPart);
+            partitions.Add(currentParts.Select(long.Parse).Max());
+            currentParts.RemoveAt(currentParts.Count - 1);
+            return;
+        }
+
+        for (int i = index + 1; i <= st.Length - k; i++)
+        {
+            string part = st.Substring(index, i - index);
+            currentParts.Add(part);
+            GeneratePartitions(st, k - 1, i, currentParts, partitions);
+            currentParts.RemoveAt(currentParts.Count - 1);
+        }
+    }
+}
+//https://www.codewars.com/kata/5b83c1c44a6acac33400009a/train/csharp
---
+++ CodeWarsTest/SimpleStringDivision.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class SimpleStringDivision
+{
+    [Test]
+    public void SimpleStringDivisionTest()
+    {
+        Assert.That(SimpleStringDivisionTask.Solve("123", 1), Is.EqualTo(23));
+        Assert.That(SimpleStringDivisionTask.Solve("1234", 1), Is.EqualTo(234));     
+        Assert.That(SimpleStringDivisionTask.Solve("1234", 2), Is.EqualTo(34));
+        Assert.That(SimpleStringDivisionTask.Solve("1234", 3), Is.EqualTo(4)); 
+   
+    }
+}
