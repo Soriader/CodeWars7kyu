063e347ef4015fb60171c8ea0d11a411a82e2576
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-26 13:29:19 +0100

    [~] Task: Array comparator

---
+++ CodeWars7kyu/ArrayComparatorTask.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars7kyu;
+
+public class ArrayComparatorTask
+{
+    public static int MatchArrays(int[] v, int[] r)
+    {
+        int result = 0;
+        
+        foreach (int element in v)
+        {
+            if (r.Contains(element))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
+//https://www.codewars.com/kata/561046a9f629a8aac000001d/train/csharp
---
+++ CodeWarsTest/ArrayComparator.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class ArrayComparator
+{
+    [Test]
+    public void ArrayComparatorTest()
+    {
+        Assert.That(ArrayComparatorTask.MatchArrays(new int[] {1,2,3,4,5}, new int[] {2, 3, 4, 5, 6}), Is.EqualTo(4));
+        Assert.That(ArrayComparatorTask.MatchArrays(new int[] {1,2,3,4,5}, new int[] {5, 4}), Is.EqualTo(2));
+        Assert.That(ArrayComparatorTask.MatchArrays(new int[] {0, -1, 1, 4}, new int[] {-1}), Is.EqualTo(1));
+    }
+}
