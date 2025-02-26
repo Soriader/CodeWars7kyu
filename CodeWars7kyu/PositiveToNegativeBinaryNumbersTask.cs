eb69bc2591138d055499a6ca67e62ddd3b849ca7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-26 13:23:12 +0100

    [~] Task: Positive to negative binary numbers

---
+++ CodeWars7kyu/PositiveToNegativeBinaryNumbersTask.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeWars7kyu;
+
+public class PositiveToNegativeBinaryNumbersTask
+{
+    public static int[] Positive2Negative(int[] binaryArray)
+    {
+        var invertedArray = new int[binaryArray.Length];
+        
+        for (int i = 0; i < binaryArray.Length; i++)
+        {
+            invertedArray[i] = binaryArray[i] == 0 ? 1 : 0;
+        }
+
+        int carry = 1;
+        var resultArray = new int[binaryArray.Length];
+        for (int i = binaryArray.Length - 1; i >= 0; i--)
+        {
+            int sum = invertedArray[i] + carry;
+            if (sum == 2)
+            {
+                resultArray[i] = 0;
+                carry = 1;
+            }
+            else
+            {
+                resultArray[i] = sum;
+                carry = 0;
+            }
+        }
+
+        return resultArray;
+    }
+}
+//https://www.codewars.com/kata/5becace7063291bebf0001d5/train/csharp
---
+++ CodeWarsTest/PositiveToNegativeBinaryNumbers.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class PositiveToNegativeBinaryNumbers
+{
+    int[] Test1 = new int[] {0,0,0,0};
+    int[] Test2 = new int[] {0,0,1,0};
+    int[] Test3 = new int[] {0,0,1,1};
+    int[] Test4 = new int[] {0,1,0,0};
+    int[] Test5 = new int[] {0, 1, 0, 0, 1};
+
+
+    [Test]
+    public void PositiveToNegativeBinaryNumbersTest()
+    {
+        Assert.That(PositiveToNegativeBinaryNumbersTask.Positive2Negative(Test1), Is.EqualTo(Test1));
+        Assert.That(PositiveToNegativeBinaryNumbersTask.Positive2Negative(Test2), Is.EqualTo(new int[] {1,1,1,0}));
+        Assert.That(PositiveToNegativeBinaryNumbersTask.Positive2Negative(Test3), Is.EqualTo(new int[] {1,1,0,1}));
+        Assert.That(PositiveToNegativeBinaryNumbersTask.Positive2Negative(Test4), Is.EqualTo(new int[] {1,1,0,0}));
+        Assert.That(PositiveToNegativeBinaryNumbersTask.Positive2Negative(Test5), Is.EqualTo(new int[] {1, 0, 1, 1, 1}));
+    }
+}
