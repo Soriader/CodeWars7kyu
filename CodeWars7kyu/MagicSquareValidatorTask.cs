58959724e8a73d1e3f9cb92f80785e078be171af
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-27 19:25:47 +0100

    [~] Task: Magic Square Validator

---
+++ CodeWars7kyu/MagicSquareValidatorTask.cs
@@ -0,0 +1,41 @@
+﻿namespace CodeWars7kyu;
+
+public class MagicSquareValidatorTask
+{
+    public static bool IsMagical(int[] square)
+    {
+        if (square.Length != 9)
+        {
+            return false;
+        }
+
+        if (square.Distinct().Count() != 9)
+        {
+            return false;
+        }
+
+        if (square[0] + square[1] + square[2] != 15 ||
+            square[3] + square[4] + square[5] != 15 ||
+            square[6] + square[7] + square[8] != 15)
+        {
+            return false;
+        }
+
+        if (square[0] + square[3] + square[6] != 15 ||
+            square[1] + square[4] + square[7] != 15 ||
+            square[2] + square[5] + square[8] != 15)
+        {
+            return false;
+        }
+
+        if (square[0] + square[4] + square[8] != 15 ||
+            square[2] + square[4] + square[6] != 15)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
+//https://www.codewars.com/kata/57be6a612eaf7cc3af000178/train/csharp
---
+++ CodeWarsTest/MagicSquareValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class MagicSquareValidator
+{
+    [Test]
+    public void MagicSquareValidatorTest()
+    {
+        Assert.That(MagicSquareValidatorTask.IsMagical(new int[] {4, 9, 2, 3, 5, 7, 8, 1, 6}), Is.True, "This is a valid magic square.");
+        Assert.That(MagicSquareValidatorTask.IsMagical(new int[] {4, 9, 2, 3, 5, 7, 8, 6, 1}), Is.False, "Some column(s) and diagonal(s) don't sum to 15");
+        Assert.That(MagicSquareValidatorTask.IsMagical(new int[] {4, 5, 2, 3, 9, 7, 8, 1, 6}), Is.False, "Some row(s) don't sum to 15");
+    }
+}
