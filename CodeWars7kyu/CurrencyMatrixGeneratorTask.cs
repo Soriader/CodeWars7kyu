22bcc3a5b0f42614223aeb17cd8a1ff9a66f6e7a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-27 18:45:44 +0100

    [~] Task: Currency Matrix Generator

---
+++ CodeWars7kyu/CurrencyMatrixGeneratorTask.cs
@@ -0,0 +1,42 @@
+﻿namespace CodeWars7kyu;
+
+public class CurrencyMatrixGeneratorTask
+{
+    public static string[] GenerateCurrencyMatrix(string currency)
+    {
+        Dictionary<string, int> currencyForex = new Dictionary<string, int>
+        {
+            { "EUR", 1 },
+            { "GBP", 2 },
+            { "AUD", 3 },
+            { "NZD", 4 },
+            { "USD", 5 },
+            { "CAD", 6 },
+            { "CHF", 7 },
+            { "JPY", 8 }
+        };
+
+        var valueOfCurrency = currencyForex[currency];
+
+        var result = new List<string>();
+        
+        foreach (var currencyPair in currencyForex)
+        {
+            if (currencyPair.Key == currency)
+            {
+                continue;
+            }
+            if (currencyPair.Value > valueOfCurrency) 
+            {
+                result.Add($"{currency}{currencyPair.Key}");
+            }
+            else
+            {
+                result.Add($"{currencyPair.Key}{currency}");
+            }
+        }
+        
+        return result.ToArray();
+    }
+}
+//https://www.codewars.com/kata/57bec3bc5640aa5f3f00003e/train/csharp
---
+++ CodeWarsTest/CurrencyMatrixGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class CurrencyMatrixGenerator
+{
+    [Test]
+    public void CurrencyMatrixGeneratorTest()
+    {
+        var expected = string.Join(",", new string[] { "EURGBP", "EURAUD", "EURNZD", "EURUSD", "EURCAD", "EURCHF", "EURJPY" });
+        Assert.That(string.Join(",", CurrencyMatrixGeneratorTask.GenerateCurrencyMatrix("EUR")), Is.EqualTo(expected));
+      
+        expected = string.Join(",", new string[] { "EURGBP", "GBPAUD", "GBPNZD", "GBPUSD", "GBPCAD", "GBPCHF", "GBPJPY" });
+        Assert.That(string.Join(",", CurrencyMatrixGeneratorTask.GenerateCurrencyMatrix("GBP")), Is.EqualTo(expected));
+      
+        expected = string.Join(",", new string[] { "EURCHF", "GBPCHF", "AUDCHF", "NZDCHF", "USDCHF", "CADCHF", "CHFJPY" });
+        Assert.That(string.Join(",", CurrencyMatrixGeneratorTask.GenerateCurrencyMatrix("CHF")), Is.EqualTo(expected));      
+    }
+}
