40d32b886fc93a11ad36ddb22056087f283a169c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-28 17:07:11 +0100

    [~] Task: Time Degrees

---
+++ CodeWars7kyu/TimeDegreesTask.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeWars7kyu;
+
+public class TimeDegreesTask
+{
+    public static string ClockDegree(string s)
+    {
+        if (string.IsNullOrEmpty(s) || s.Length != 5 || s[2] != ':')
+        {
+            return "Check your time !";
+        }
+
+        string[] parts = s.Split(':');
+        if (parts.Length != 2)
+        {
+            return "Check your time !";
+        }
+
+        if (!int.TryParse(parts[0], out int hours) || !int.TryParse(parts[1], out int minutes))
+        {
+            return "Check your time !";
+        }
+        
+        if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59)
+        {
+            return "Check your time !";
+        }
+
+        hours = hours % 12;
+
+        int hourAngle = hours * 30;
+
+        int minuteAngle = minutes * 6;
+
+        hourAngle = (int)Math.Round((double)hourAngle);
+        minuteAngle = (int)Math.Round((double)minuteAngle);
+
+        hourAngle = hourAngle % 360;
+        minuteAngle = minuteAngle % 360;
+
+        if (hourAngle == 0) hourAngle = 360;
+        if (minuteAngle == 0) minuteAngle = 360;
+
+        return $"{hourAngle}:{minuteAngle}";
+    }
+}
+//https://www.codewars.com/kata/5782a87d9fb2a5e623000158/train/csharp
---
+++ CodeWarsTest/TimeDegrees.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class TimeDegrees
+{
+    [Test]
+    public void TimeDegreesTest()
+    {
+        Assert.That(TimeDegreesTask.ClockDegree("01:01"), Is.EqualTo("30:6"));
+        Assert.That(TimeDegreesTask.ClockDegree("00:00"), Is.EqualTo("360:360"));
+        Assert.That(TimeDegreesTask.ClockDegree("01:03"), Is.EqualTo("30:18"));
+        Assert.That(TimeDegreesTask.ClockDegree("01:30"), Is.EqualTo("30:180"));
+        Assert.That(TimeDegreesTask.ClockDegree("12:05"), Is.EqualTo("360:30"));
+        Assert.That(TimeDegreesTask.ClockDegree("26:78"), Is.EqualTo("Check your time !"));
+        Assert.That(TimeDegreesTask.ClockDegree("23:60"), Is.EqualTo("Check your time !"));
+        Assert.That(TimeDegreesTask.ClockDegree("24:60"), Is.EqualTo("Check your time !"));
+        Assert.That(TimeDegreesTask.ClockDegree("16:25"), Is.EqualTo("120:150"));
+        Assert.That(TimeDegreesTask.ClockDegree("17:09"), Is.EqualTo("150:54"));
+        Assert.That(TimeDegreesTask.ClockDegree("19:00"), Is.EqualTo("210:360"));
+        Assert.That(TimeDegreesTask.ClockDegree("20:34"), Is.EqualTo("240:204"));
+        Assert.That(TimeDegreesTask.ClockDegree("23:20"), Is.EqualTo("330:120"));
+        Assert.That(TimeDegreesTask.ClockDegree("24:00"), Is.EqualTo("Check your time !"));
+        Assert.That(TimeDegreesTask.ClockDegree("-09:00"), Is.EqualTo("Check your time !"));
+    }
+}
