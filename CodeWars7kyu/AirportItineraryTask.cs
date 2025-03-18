d161b87de3c1e056dba0389aa1574524422e0f2b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-18 18:39:33 +0100

    [~] Task: Airport itinerary

---
+++ CodeWars7kyu/AirportItineraryTask.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWars7kyu;
+
+public class AirportItineraryTask
+{
+    public static string Itinerary(Route[] travel)
+    { 
+        if (travel == null || travel.Length == 0)
+        {
+            return "";
+        }
+
+        var airports = new List<string>();
+        
+        foreach (var segment in travel)
+        {
+            if (!airports.Contains(segment.In))
+            {
+                airports.Add(segment.In);
+            }
+            airports.Add(segment.Out);
+        }
+
+        return string.Join("-", airports);
+    }
+}
+
+public class Route
+{
+    public string In;
+    public string Out;
+}
+
+//https://www.codewars.com/kata/57a72cb872292dc43100000c/train/csharp
---
+++ CodeWarsTest/AirportItinerary.cs
@@ -0,0 +1,60 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class AirportItinerary
+{
+    [Test, Order(1)]
+    public void SimpleTest()
+    {
+        var travel = new [] 
+        { 
+            new Route { In = "TRN", Out = "FCO"}
+        };
+        Assert.That(AirportItineraryTask.Itinerary(travel), Is.EqualTo("TRN-FCO"));
+    }
+    
+    [Test, Order(2)]
+    public void MultipleTest()
+    {
+        var travel = new [] 
+        { 
+            new Route { In = "TRN", Out = "FCO"}, 
+            new Route { In = "CIA", Out = "JFK"} 
+        };
+        Assert.That(AirportItineraryTask.Itinerary(travel), Is.EqualTo("TRN-FCO-CIA-JFK"));
+    }
+    
+    [Test, Order(3)]
+    public void SameAirportsAreSkippedTest()
+    {
+        var travel = new [] 
+        { 
+            new Route { In = "TRN", Out = "FCO"}, 
+            new Route { In = "FCO", Out = "JFK"} 
+        };
+        Assert.That(AirportItineraryTask.Itinerary(travel), Is.EqualTo("TRN-FCO-JFK"));
+    }
+
+    [Test, Order(4)]
+    public void RoundTripAreConservedTest()
+    {
+        var travel = new [] 
+        { 
+            new Route { In = "TRN", Out = "FCO"}, 
+            new Route { In = "CIA", Out = "TRN"} 
+        };
+        Assert.That(AirportItineraryTask.Itinerary(travel), Is.EqualTo("TRN-FCO-CIA-TRN"));
+    }
+    
+    [Test, Order(5)]
+    public void RoundTripAreConservedWithSharedAirportsTest()
+    {
+        var travel = new [] 
+        { 
+            new Route { In = "TRN", Out = "FCO"}, 
+            new Route { In = "FCO", Out = "TRN"} 
+        };
+        Assert.That(AirportItineraryTask.Itinerary(travel), Is.EqualTo("TRN-FCO-TRN"));
+    }    
+}
