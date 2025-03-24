c2e9eb3a7a10b09ee47a2daecc11855a8478af84
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-24 11:38:14 +0100

    [~] Task: Array of twins

---
+++ CodeWars7kyu/ArrayOfTwinsTask.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars7kyu;
+
+public class ArrayOfTwinsTask
+{
+    public static bool Twins(int[] arr)
+    {
+        var boxForResult = new Dictionary<int, int>();
+
+        foreach (var box in arr)
+        {
+            if (boxForResult.ContainsKey(box))
+            {
+                boxForResult[box]++;
+            }
+            else
+            {
+                boxForResult.Add(box, 1);
+            }
+        }
+
+        if (boxForResult.Values.All(value => value == 2))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
+//https://www.codewars.com/kata/58fdc60ed94f25a863000092/train/csharp
---
+++ CodeWarsTest/ArrayOfTwins.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class ArrayOfTwins
+{
+    private static IEnumerable<TestCaseData> testCases
+    {
+        get
+        {
+            yield return new TestCaseData(new[] {new int[] {1, 2, 3, 1, 2, 3}}).Returns(true);
+            yield return new TestCaseData(new[] {new int[] {33, 5, 17, 5}}).Returns(false);
+            yield return new TestCaseData(new[] {new int[] {2, 16, 2, 17, 2, 16}}).Returns(false);
+        }
+    }
+  
+    [Test, TestCaseSource("testCases")]
+    public bool Test(int[] arr) =>
+        ArrayOfTwinsTask.Twins(arr);
+}
