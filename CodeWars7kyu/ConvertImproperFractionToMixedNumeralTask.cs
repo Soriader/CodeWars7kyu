8d1fb6f6b24d2917e77944e645ef847b3fb0add6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-31 21:06:06 +0200

    [~] Task: Convert Improper Fraction to Mixed Numeral

---
+++ CodeWars7kyu/ConvertImproperFractionToMixedNumeralTask.cs
@@ -0,0 +1,42 @@
+﻿namespace CodeWars7kyu;
+
+public class ConvertImproperFractionToMixedNumeralTask
+{
+    public string ConvertToMixedNumeral(string frac)
+    {
+        var numbers = frac.Split('/');
+        var numerator = int.Parse(numbers[0]);
+        var denominator = int.Parse(numbers[1]);
+    
+        if (Math.Abs(numerator) < denominator)
+        {
+            return frac; 
+        }
+    
+        int wholeNumber = numerator / denominator;
+        int remainder = numerator % denominator;
+    
+        if (remainder == 0)
+        {
+            return wholeNumber.ToString();
+        }
+    
+        if (Math.Sign(remainder) != Math.Sign(wholeNumber) && wholeNumber != 0)
+        {
+            remainder = Math.Abs(remainder);
+            if (wholeNumber > 0)
+            {
+                wholeNumber--;
+                remainder = denominator - remainder;
+            }
+            else
+            {
+                wholeNumber++;
+                remainder = denominator - remainder;
+            }
+        }
+    
+        return $"{wholeNumber} {Math.Abs(remainder)}/{denominator}";
+    }
+}
+//https://www.codewars.com/kata/574e4175ff5b0a554a00000b/train/csharp
---
+++ CodeWarsTest/ConvertImproperFractionToMixedNumeral.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class ConvertImproperFractionToMixedNumeral
+{
+    [Test]
+    public void ConvertImproperFractionToMixedNumeralTest()
+    {
+        ConvertImproperFractionToMixedNumeralTask ConvertImproperFractionToMixedNumeralTask = new ConvertImproperFractionToMixedNumeralTask();
+        Assert.That(ConvertImproperFractionToMixedNumeralTask.ConvertToMixedNumeral("6/2"), Is.EqualTo("3"));
+        Assert.That(ConvertImproperFractionToMixedNumeralTask.ConvertToMixedNumeral("74/3"), Is.EqualTo("24 2/3"));
+        Assert.That(ConvertImproperFractionToMixedNumeralTask.ConvertToMixedNumeral("-504/26"), Is.EqualTo("-19 10/26"));
+        Assert.That(ConvertImproperFractionToMixedNumeralTask.ConvertToMixedNumeral("9/18"), Is.EqualTo("9/18"));
+    }
+}
