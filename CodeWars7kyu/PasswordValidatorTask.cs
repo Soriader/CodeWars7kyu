3bc8203176f5563250353fc8e555ceb68169b997
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-08 17:50:04 +0200

    [~] Task: Password validator

---
+++ CodeWars7kyu/PasswordValidatorTask.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWars7kyu;
+
+public class PasswordValidatorTask
+{
+    public static bool Password(string st)
+    {
+        if (st.Length < 8 
+            ||!st.ToCharArray().Any(c => char.IsLower(c))
+            ||!st.ToCharArray().Any(c => char.IsUpper(c))
+            ||!st.ToCharArray().Any(c => char.IsDigit(c)))
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
+//https://www.codewars.com/kata/56a921fa8c5167d8e7000053/train/csharp
