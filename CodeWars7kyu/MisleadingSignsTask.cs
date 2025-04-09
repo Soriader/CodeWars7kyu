6395afdd531438e875b09cc4f288d57bd593df1c
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-09 13:55:00 +0200

    [~] Task: Misleading Signs

---
+++ CodeWars7kyu/MisleadingSignsTask.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWars7kyu;
+
+public class MisleadingSignsTask
+{
+    public static bool Gaslighting(string shirtWord, string yourWord, char[] friendsLetters)
+    {
+        if (shirtWord == yourWord)
+        {
+            return false;
+        }
+        
+
+        for (int i = 0; i < shirtWord.Length; i++)
+        {
+            if (shirtWord[i] != yourWord[i] 
+                && (friendsLetters.Contains(shirtWord[i]) 
+                    || friendsLetters.Contains(yourWord[i])))
+            {
+                return true;
+            }
+        }
+        
+        return false;     
+    }
+}
+//https://www.codewars.com/kata/65dd5b414ccda60a4be32c2a/train/csharp
---
+++ CodeWarsTest/MisleadingSigns.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars7kyu;
+
+public class MisleadingSigns
+{
+        [TestCase("snack", "snake", new char[] {'c'}, ExpectedResult = true)]
+        [TestCase("snack", "snack", new char[] {'s', 'n', 'a', 'c', 'k'}, ExpectedResult = false)]
+        [TestCase("snake", "snack", new char[] { 'c' }, ExpectedResult = true)]
+        [TestCase("ping", "pong", new char[] { 'p', 'n', 'g' }, ExpectedResult = false)]
+        [TestCase("ping", "pong", new char[] { 'i' }, ExpectedResult = true)]
+        public bool Sample_Cases(string s, string y, char[] f) => MisleadingSignsTask.Gaslighting(s, y, f);
+}
