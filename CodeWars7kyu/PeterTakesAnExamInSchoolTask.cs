247d2bc95bb94bf0e1867654a63910d6f5542362
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-18 15:16:54 +0200

    [~] Task: Peter takes an exam in school and added gitignore

---
+++ CodeWars7kyu/PeterTakesAnExamInSchoolTask.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars7kyu;
+
+public class PeterTakesAnExamInSchoolTask
+{
+    
+    public static List<string> PassedStudentList(List<Student> gradeList, int acceptanceGrade)
+    {
+        var result = new List<string>();
+
+        foreach (var grade in gradeList)
+        {
+            if (grade.Grade >= acceptanceGrade)
+            {
+                result.Add(grade.Name);
+            }
+        }
+        
+        return result.OrderBy(x => x).ToList();
+    }
+
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public double Grade { get; set; }
+}
+
+//https://www.codewars.com/kata/564b0f24c685f1befa000081/train/csharp
